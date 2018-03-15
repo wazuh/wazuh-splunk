9712206f93ed23bb55279865e4fa4558452b552c
manuasir <manu.asi2@gmail.com>
2018-03-15 18:26:04 +0100

    New directive that allows to inject JS into ui-view frames

---
+++ wazuh/appserver/static/js/directives/loadScript.js
@@ -0,0 +1,19 @@
+(function (ng) {
+    'use strict';
+    var app = ng.module('ngLoadScript', []);
+  
+    app.directive('script', function() {
+      return {
+        restrict: 'E',
+        scope: true,
+        link: function(scope, elem, attr) {
+          if (attr.type === 'text/javascript-lazy') {
+            var code = elem.text();
+            var f = new Function(code);
+            f();
+          }
+        }
+      };
+    });
+  
+  }(angular));
