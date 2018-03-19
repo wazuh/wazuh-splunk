1cd728de811f1c117fc9155e9d57547053b7d008
manuasir <manu.asi2@gmail.com>
2018-03-19 18:45:41 +0100

    Refactoring backend. Adding new routes and naming them properly

--- wazuh/appserver/controllers/manager.py
+++ wazuh/appserver/controllers/manager.py
@@ -13,20 +13,20 @@ def setup_logger(level):
     """
     Setup a logger for the REST handler.
     """
-    logger = logging.getLogger('splunk.appserver.%s.controllers.my_script' % _APPNAME)
+    logger = logging.getLogger('splunk.appserver.%s.controllers.manager' % _APPNAME)
     logger.propagate = False  # Prevent the log messages from being duplicated in the python.log file
     logger.setLevel(level)
-    file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'my_script_controller.log']), maxBytes=25000000, backupCount=5)
+    file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'manager_controller.log']), maxBytes=25000000, backupCount=5)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     return logger
 logger = setup_logger(logging.DEBUG)
 print 'OK'
-class my_script(controllers.BaseController):
+class manager(controllers.BaseController):
     # /custom/MyAppName/my_script/my_endpoint
     @expose_page(must_login=False, methods=['GET'])
-    def my_endpoint(self, **kwargs):
+    def info(self, **kwargs):
         opt_username = 'foo'
         opt_password = 'bar'
         opt_base_url = 'http://192.168.0.157:55000'
@@ -34,5 +34,17 @@ class my_script(controllers.BaseController):
         verify = False
         request = requests.get(opt_base_url + '/manager/info', auth=auth, verify=verify)
         manager_info = json.loads(request.text)['data']
-        miJson = json.dumps(manager_info)
-        return miJson
+        myJson = json.dumps(manager_info)
+        return myJson
+
+    @expose_page(must_login=False, methods=['GET'])
+    def logs(self, **kwargs):
+        opt_username = 'foo'
+        opt_password = 'bar'
+        opt_base_url = 'http://192.168.0.157:55000'
+        auth = requests.auth.HTTPBasicAuth(opt_username, opt_password)
+        verify = False
+        request = requests.get(opt_base_url + '/manager/logs', auth=auth, verify=verify)
+        manager_info = json.loads(request.text)['data']['items']
+        myJson = json.dumps(manager_info)
+        return myJson
