7525fa61a0a5ea27a028bd061a19cd030c58c951
manuasir <manu.asi2@gmail.com>
2018-03-22 15:36:00 +0100

    Splunk indexes are not needed anymore for any Ruleset or Decoders functionality, datasets are already fetched on demand from Wazuh API

--- SplunkAppForWazuh/appserver/controllers/manager.py
+++ SplunkAppForWazuh/appserver/controllers/manager.py
@@ -68,5 +68,33 @@ class manager(controllers.BaseController):
                     data[key] = row[key]
             # data = json.dumps(data)
             results.append(data)
-        
+        return json.dumps(results)
+
+     # /custom/wazuh/manager/decoders
+    @expose_page(must_login=False, methods=['GET'])
+    def decoders(self, **kwargs):
+        opt_username = 'foo'
+        opt_password = 'bar'
+        opt_base_url = 'http://192.168.0.157:55000'
+        auth = requests.auth.HTTPBasicAuth(opt_username, opt_password)
+        verify = False
+        request = requests.get(opt_base_url + '/decoders?limit=1', auth=auth, verify=verify)
+        decoders_qty = json.loads(request.text)["data"]["totalItems"]
+        request = requests.get(opt_base_url + '/decoders?offset=0&limit=' + str(decoders_qty), auth=auth, verify=verify)
+        decoders = json.loads(request.text)["data"]["items"]
+        results = []
+        for row in decoders:
+            data = {}
+            for key in row:
+                if isinstance(row[key], dict):
+                    for detail in row[key]:
+                        data[key + "-" + detail] = row[key][detail]
+                elif isinstance(row[key], list):
+                    count = 0
+                    for detail in row[key]:
+                        data[str(key) + "-" + str(count)] = detail
+                        count += 1
+                else:
+                    data[key] = row[key]
+            results.append(data)
         return json.dumps(results)
