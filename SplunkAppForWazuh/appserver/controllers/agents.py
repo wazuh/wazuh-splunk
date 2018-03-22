afd2006c4199f1eedc17376f8ebc354ac1060e16
manuasir <manu.asi2@gmail.com>
2018-03-22 18:24:33 +0100

    Agent status backend endpoint

--- SplunkAppForWazuh/appserver/controllers/agents.py
+++ SplunkAppForWazuh/appserver/controllers/agents.py
@@ -13,10 +13,10 @@ def setup_logger(level):
     """
     Setup a logger for the REST handler.
     """
-    logger = logging.getLogger('splunk.appserver.%s.controllers.manager' % _APPNAME)
+    logger = logging.getLogger('splunk.appserver.%s.controllers.agents' % _APPNAME)
     logger.propagate = False  # Prevent the log messages from being duplicated in the python.log file
     logger.setLevel(level)
-    file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'manager.log']), maxBytes=25000000, backupCount=5)
+    file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'agents.log']), maxBytes=25000000, backupCount=5)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
@@ -37,5 +37,6 @@ class agents(controllers.BaseController):
         data = {}
         for key in agent_summary:
             data['agent_summary_' + key.lower().replace(' ', '')] = agent_summary[key]
+        data = [data]
         result = json.dumps(data)
-        return data
+        return result
