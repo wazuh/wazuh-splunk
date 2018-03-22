0660b46459720fab410bd362b44eb4f0f54bfe17
manuasir <manu.asi2@gmail.com>
2018-03-22 14:45:02 +0100

    Fetching ruleset data from API on demand and avoiding indexation closes #8

---
+++ TA-wazuh-api-connector/bin/input_module_wazuh_api_info_basic.py
@@ -0,0 +1,37 @@
+import json
+import requests
+
+def validate_input(helper, definition):
+    pass
+
+def collect_events(helper, ew):
+    opt_username = helper.get_arg('username')
+    opt_password = helper.get_arg('password')
+    opt_base_url = helper.get_arg('base_url')
+
+    auth = requests.auth.HTTPBasicAuth(opt_username, opt_password)
+    verify = False
+
+    request = requests.get(opt_base_url + '/manager/info', auth=auth, verify=verify)
+    manager_info = json.loads(request.text)['data']
+
+    request = requests.get(opt_base_url + '/manager/status', auth=auth, verify=verify)
+    manager_status = json.loads(request.text)['data']
+
+    request = requests.get(opt_base_url + '/agents/summary', auth=auth, verify=verify)
+    agent_summary = json.loads(request.text)['data']
+
+    data = {}
+    for key in manager_info:
+        data['manager-info_' + key.lower()] = manager_info[key]
+
+    for key in manager_status:
+        data['manager-status_' + key.lower()] = manager_status[key]
+
+    for key in agent_summary:
+        data['agent_summary_' + key.lower().replace(' ', '')] = agent_summary[key]
+
+
+    data = json.dumps(data)
+    event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=data)
+    ew.write_event(event)
