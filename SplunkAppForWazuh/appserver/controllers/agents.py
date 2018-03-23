7e666a6d7f73d8858554234dd373a75b09739cbd
manuasir <manu.asi2@gmail.com>
2018-03-23 18:12:36 +0100

    Agent list backend

--- SplunkAppForWazuh/appserver/controllers/agents.py
+++ SplunkAppForWazuh/appserver/controllers/agents.py
@@ -40,9 +40,37 @@ class agents(controllers.BaseController):
         result = json.dumps(data)
         return result
 
-    # /custom/wazuh/agents/agents
     @expose_page(must_login=False, methods=['GET'])
-    def agents(self, **kwargs):
+    def agents_info(self, **kwargs):
+        opt_username = 'foo'
+        opt_password = 'bar'
+        opt_base_url = 'http://192.168.0.157:55000'
+        auth = requests.auth.HTTPBasicAuth(opt_username, opt_password)
+        verify = False
+        request = requests.get(opt_base_url + '/agents?limit=0', auth=auth, verify=verify)
+        agents_qty = json.loads(request.text)["data"]["totalItems"]
+
+        request = requests.get(opt_base_url + '/agents?offset=0&limit=' + str(agents_qty), auth=auth, verify=verify)
+        agents = json.loads(request.text)["data"]["items"]
+        results = []
+        for row in agents:
+            data = {}
+            request = requests.get(opt_base_url + '/agents/' + row["id"], auth=auth, verify=verify)
+            agent_info = json.loads(request.text)["data"]
+
+            keys = ["id", "status", "name", "ip", "dateAdd", "version", "os_family", "lastKeepAlive", "os"]
+
+            data = {}
+            for key in keys:
+                if key in agent_info:
+                    data[key] = agent_info[key]
+
+            results.append(data)
+        return json.dumps(agents)
+        
+    # /custom/wazuh/agents/agentschecks
+    @expose_page(must_login=False, methods=['GET'])
+    def agents_checks(self, **kwargs):
         opt_username = 'foo'
         opt_password = 'bar'
         opt_base_url = 'http://192.168.0.157:55000'
