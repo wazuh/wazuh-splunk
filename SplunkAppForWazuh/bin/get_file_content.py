17ee7e478db2c2d0b90b1a1499caa2ef17f12399
manuasir <manu.asi2@gmail.com>
2018-04-03 18:40:01 +0200

    Implemented controllers for interacting with back-end

---
+++ SplunkAppForWazuh/bin/get_file_content.py
@@ -0,0 +1,22 @@
+#!/opt/splunk/bin/python
+############################################################
+#
+# GET /agents/files
+#
+############################################################
+import sys
+import splunk.Intersplunk as si
+import requests
+import json
+
+try:
+    results = []
+    request = requests.get("http://192.168.0.159:8000/en-US/custom/wazuh/agents/filescontent?id="+sys.argv[1]+'&filename='+sys.argv[2])
+    data = json.loads(request.text)
+except Exception as err:
+        import traceback
+        print err
+        stack = traceback.format_exc()
+        data = si.generateErrorResults("Error : Traceback: " + str(stack))
+
+si.outputResults(data)
