50446aa388cbbdc5659a1322e3adfe65fae20c1a
manuasir <manu.asi2@gmail.com>
2018-04-10 18:25:20 +0200

    Logs already working only with data provided via inputs

--- wazuh/bin/get_manager_logs.py
+++ wazuh/bin/get_manager_logs.py
@@ -11,17 +11,6 @@ import json
 
 try:
     request = requests.get('http://'+sys.argv[1]+":"+sys.argv[2]+"/en-US/custom/wazuh/manager/logs?ip="+sys.argv[3]+"&port="+sys.argv[4]+"&user="+sys.argv[5]+"&pass="+sys.argv[6])
-    print "cero " + str(sys.argv)
-
-    print "ip " + sys.argv[1]
-    print "port " + sys.argv[2]
-    print "api " + sys.argv[3]
-    print "api port " + sys.argv[4]
-
-    print "api user " + sys.argv[5]
-
-    print "api password " + sys.argv[6]
-
     data = json.loads(request.text)
 except Exception as err:
         import traceback
