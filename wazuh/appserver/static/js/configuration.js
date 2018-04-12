cd9af38e55db8e0e60e8dde74a4d55c0460732d7
manuasir <manu.asi2@gmail.com>
2018-04-12 19:50:11 +0200

    Refactoring frontend logic for manager configuration tab

---
+++ wazuh/appserver/static/js/configuration.js
@@ -0,0 +1,221 @@
+
+    require([
+      "splunkjs/mvc",
+      "splunkjs/mvc/utils",
+      "splunkjs/mvc/tokenutils",
+      "underscore",
+      "jquery",
+      "splunkjs/mvc/simplexml",
+      "splunkjs/mvc/layoutview",
+      "splunkjs/mvc/simplexml/dashboardview",
+      "splunkjs/mvc/simplexml/dashboard/panelref",
+      "splunkjs/mvc/simplexml/element/chart",
+      "splunkjs/mvc/simplexml/element/event",
+      "splunkjs/mvc/simplexml/element/html",
+      "splunkjs/mvc/simplexml/element/list",
+      "splunkjs/mvc/simplexml/element/map",
+      "splunkjs/mvc/simplexml/element/single",
+      "splunkjs/mvc/simplexml/element/table",
+      "splunkjs/mvc/simplexml/element/visualization",
+      "splunkjs/mvc/simpleform/formutils",
+      "splunkjs/mvc/simplexml/eventhandler",
+      "splunkjs/mvc/simplexml/searcheventhandler",
+      "splunkjs/mvc/simpleform/input/dropdown",
+      "splunkjs/mvc/simpleform/input/radiogroup",
+      "splunkjs/mvc/simpleform/input/linklist",
+      "splunkjs/mvc/simpleform/input/multiselect",
+      "splunkjs/mvc/simpleform/input/checkboxgroup",
+      "splunkjs/mvc/simpleform/input/text",
+      "splunkjs/mvc/simpleform/input/timerange",
+      "splunkjs/mvc/simpleform/input/submit",
+      "splunkjs/mvc/searchmanager",
+      "splunkjs/mvc/savedsearchmanager",
+      "splunkjs/mvc/postprocessmanager",
+      "splunkjs/mvc/simplexml/urltokenmodel"
+      // Add comma-separated libraries and modules manually here, for example:
+      // ..."splunkjs/mvc/simplexml/urltokenmodel",
+      // "splunkjs/mvc/tokenforwarder"
+    ],
+      function (
+        mvc,
+        utils,
+        TokenUtils,
+        _,
+        $,
+        DashboardController,
+        LayoutView,
+        Dashboard,
+        PanelRef,
+        ChartElement,
+        EventElement,
+        HtmlElement,
+        ListElement,
+        MapElement,
+        SingleElement,
+        TableElement,
+        VisualizationElement,
+        FormUtils,
+        EventHandler,
+        SearchEventHandler,
+        DropdownInput,
+        RadioGroupInput,
+        LinkListInput,
+        MultiSelectInput,
+        CheckboxGroupInput,
+        TextInput,
+        TimeRangeInput,
+        SubmitButton,
+        SearchManager,
+        SavedSearchManager,
+        PostProcessManager,
+        UrlTokenModel
+
+        // Add comma-separated parameter names here, for example: 
+        // ...UrlTokenModel, 
+        // TokenForwarder
+      ) {
+
+        var pageLoading = true;
+
+
+        // 
+        // TOKENS
+        //
+
+        // Create token namespaces
+        var urlTokenModel = new UrlTokenModel();
+        mvc.Components.registerInstance('url', urlTokenModel);
+        var defaultTokenModel = mvc.Components.getInstance('default', { create: true });
+        var submittedTokenModel = mvc.Components.getInstance('submitted', { create: true });
+        var service = mvc.createService({ owner: "nobody" });
+
+        urlTokenModel.on('url:navigate', function () {
+          defaultTokenModel.set(urlTokenModel.toJSON());
+          if (!_.isEmpty(urlTokenModel.toJSON()) && !_.all(urlTokenModel.toJSON(), _.isUndefined)) {
+            submitTokens();
+          } else {
+            submittedTokenModel.clear();
+          }
+        });
+
+        // Initialize tokens
+        defaultTokenModel.set(urlTokenModel.toJSON());
+
+        function submitTokens() {
+          // Copy the contents of the defaultTokenModel to the submittedTokenModel and urlTokenModel
+          FormUtils.submitForm({ replaceState: pageLoading });
+        }
+
+        function setToken(name, value) {
+          defaultTokenModel.set(name, value);
+          submittedTokenModel.set(name, value);
+        }
+
+        function unsetToken(name) {
+          defaultTokenModel.unset(name);
+          submittedTokenModel.unset(name);
+        }
+
+        $(document).ready(function () {
+          service.request(
+            "storage/collections/data/credentials/",
+            "GET",
+            null,
+            null,
+            null,
+            { "Content-Type": "application/json" }, null
+          ).done(function (data) {
+            var parsedData = JSON.parse(data);
+            setToken('baseip', parsedData[0].baseip);
+            setToken('baseport', parsedData[0].baseport);
+            setToken('ipapi', parsedData[0].ipapi);
+            setToken('portapi', parsedData[0].portapi);
+            setToken('userapi', parsedData[0].userapi);
+            setToken('passwordapi', parsedData[0].passapi);
+            var tokens = mvc.Components.get("default");
+            var ipBase = tokens.get("baseip");
+            var ipApi = tokens.get("ipapi");
+            var portApi = tokens.get("portapi");
+            var passApi = tokens.get("passwordapi");
+            var userApi = tokens.get("userapi");
+            var portBase = tokens.get("baseport");
+            var endPoint = 'http://' + ipBase + ':' + portBase + '/custom/wazuh/manager/configuration?ip=' + ipApi + '&port=' + portApi + '&user=' + userApi + '&pass=' + passApi;
+            $.get(endPoint, function (data) {
+              var jsonObj = JSON.parse(data);
+              console.log(jsonObj);
+              //var jsonPretty = JSON.stringify(jsonObj[0].data, null, '\t');
+              $('#jsonOutput').text(jsonObj.global.jsonout_output);
+              $('#logAlertLevel').text(jsonObj.alerts.log_alert_level);
+            }).done(function () {
+              console.log("request success");
+            }).fail(function () {
+              console.log("error");
+            }).always(function () {
+              console.log("finished");
+            });
+          });
+
+
+        })
+
+
+        //
+        // SEARCH MANAGERS
+        //
+
+        //
+        // SPLUNK LAYOUT
+        //
+
+        $('header').remove();
+        new LayoutView({ "hideFooter": false, "hideSplunkBar": false, "hideAppBar": false, "hideChrome": false })
+          .render()
+          .getContainerElement()
+          .appendChild($('.dashboard-body')[0]);
+
+        //
+        // DASHBOARD EDITOR
+        //
+
+        new Dashboard({
+          id: 'dashboard',
+          el: $('.dashboard-body'),
+          showTitle: true,
+          editable: true
+        }, { tokens: true }).render();
+
+
+        //
+        // VIEWS: VISUALIZATION ELEMENTS
+        //
+
+
+        //
+        // VIEWS: FORM INPUTS
+        //
+
+        DashboardController.onReady(function () {
+          if (!submittedTokenModel.has('earliest') && !submittedTokenModel.has('latest')) {
+            submittedTokenModel.set({ earliest: '0', latest: '' });
+          }
+        });
+
+        // Initialize time tokens to default
+        if (!defaultTokenModel.has('earliest') && !defaultTokenModel.has('latest')) {
+          defaultTokenModel.set({ earliest: '0', latest: '' });
+        }
+
+        if (!_.isEmpty(urlTokenModel.toJSON())) {
+          submitTokens();
+        }
+
+
+        //
+        // DASHBOARD READY
+        //
+
+        DashboardController.ready();
+        pageLoading = false;
+
+      }
+    );
