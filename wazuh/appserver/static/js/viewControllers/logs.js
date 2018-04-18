efa90b459baaecc64f08157696033fe011527a40
manuasir <manu.asi2@gmail.com>
2018-04-18 17:51:40 +0200

    Migrating Manager logs table to new dynamic table

--- wazuh/appserver/static/js/viewControllers/logs.js
+++ wazuh/appserver/static/js/viewControllers/logs.js
@@ -13,101 +13,26 @@
 
 require([
   "splunkjs/mvc",
-  "splunkjs/mvc/utils",
-  "splunkjs/mvc/tokenutils",
-  "underscore",
   "jquery",
-  "splunkjs/mvc/simplexml",
   "splunkjs/mvc/layoutview",
-  "splunkjs/mvc/simplexml/dashboardview",
-  "splunkjs/mvc/simplexml/dashboard/panelref",
-  "splunkjs/mvc/simplexml/element/chart",
-  "splunkjs/mvc/simplexml/element/event",
-  "splunkjs/mvc/simplexml/element/html",
-  "splunkjs/mvc/simplexml/element/list",
-  "splunkjs/mvc/simplexml/element/map",
-  "splunkjs/mvc/simplexml/element/single",
-  "splunkjs/mvc/simplexml/element/table",
-  "splunkjs/mvc/simplexml/element/visualization",
-  "splunkjs/mvc/simpleform/formutils",
-  "splunkjs/mvc/simplexml/eventhandler",
-  "splunkjs/mvc/simplexml/searcheventhandler",
-  "splunkjs/mvc/simpleform/input/dropdown",
-  "splunkjs/mvc/simpleform/input/radiogroup",
-  "splunkjs/mvc/simpleform/input/linklist",
-  "splunkjs/mvc/simpleform/input/multiselect",
-  "splunkjs/mvc/simpleform/input/checkboxgroup",
-  "splunkjs/mvc/simpleform/input/text",
-  "splunkjs/mvc/simpleform/input/timerange",
-  "splunkjs/mvc/simpleform/input/submit",
-  "splunkjs/mvc/searchmanager",
-  "splunkjs/mvc/savedsearchmanager",
-  "splunkjs/mvc/postprocessmanager",
   "splunkjs/mvc/simplexml/urltokenmodel",
   "/static/app/wazuh/js/customViews/tableView.js"
-  // Add comma-separated libraries and modules manually here, for example:
-  // ..."splunkjs/mvc/simplexml/urltokenmodel",
-  // "splunkjs/mvc/tokenforwarder"
 ],
   function (
     mvc,
-    utils,
-    TokenUtils,
-    _,
     $,
-    DashboardController,
     LayoutView,
-    Dashboard,
-    PanelRef,
-    ChartElement,
-    EventElement,
-    HtmlElement,
-    ListElement,
-    MapElement,
-    SingleElement,
-    TableElement,
-    VisualizationElement,
-    FormUtils,
-    EventHandler,
-    SearchEventHandler,
-    DropdownInput,
-    RadioGroupInput,
-    LinkListInput,
-    MultiSelectInput,
-    CheckboxGroupInput,
-    TextInput,
-    TimeRangeInput,
-    SubmitButton,
-    SearchManager,
-    SavedSearchManager,
-    PostProcessManager,
     UrlTokenModel,
     tableView
 
-    // Add comma-separated parameter names here, for example: 
-    // ...UrlTokenModel, 
-    // TokenForwarder
   ) {
 
-    var pageLoading = true;
+    var urlTokenModel = new UrlTokenModel()
+    mvc.Components.registerInstance('url', urlTokenModel)
+    var defaultTokenModel = mvc.Components.getInstance('default', { create: true })
+    var service = mvc.createService({ owner: "nobody" })
 
-
-    // 
-    // TOKENS
-    //
-
-    // Create token namespaces
-    var urlTokenModel = new UrlTokenModel();
-    mvc.Components.registerInstance('url', urlTokenModel);
-    var defaultTokenModel = mvc.Components.getInstance('default', { create: true });
-    var submittedTokenModel = mvc.Components.getInstance('submitted', { create: true });
-    var service = mvc.createService({ owner: "nobody" });
-
-
-    //
-    // SEARCH MANAGERS
-    //
-    $(document).ready(function () {
+    $(document).ready(() => {
       service.request(
         "storage/collections/data/credentials/",
         "GET",
@@ -115,43 +40,32 @@ require([
         null,
         null,
         { "Content-Type": "application/json" }, null
-      ).done(function (data) {
-        var parsedData = JSON.parse(data);
+      ).done((data) => {
 
-        const miclase = new tableView()
-        miclase.generateTable($('#miid'),'http://'+parsedData[0].baseip+':'+parsedData[0].baseport+'/custom/wazuh/manager/logs?ip='+ parsedData[0].ipapi+'&port='+parsedData[0].ipapi+'&user='+ parsedData[0].userapi+'&pass='+ parsedData[0].passapi,10)
-    
-      });
+        // Inject DataTable
+        const parsedData = JSON.parse(data)
+        console.log(parsedData)
+        const opts = {
+          pages: 10,
+          processing: true,
+          serverSide: true,
+          filterVisible: false,
+          columns: [
+            { "data": "timestamp", 'orderable': true },
+            { "data": "tag", 'orderable': true },
+            { "data": "description", 'orderable': true },
+            { "data": "level", 'orderable': true }
+          ]
+        }
+        const table = new tableView($('#miid'), 'http://192.168.0.159:8000/custom/wazuh/manager/logs?ip=192.168.0.130&port=55000&user=foo&pass=bar', opts)
+        // table.search($('#tag'))
+      })
     })
 
     $('header').remove();
     new LayoutView({ "hideFooter": false, "hideChrome": false, "hideSplunkBar": false, "hideAppBar": false })
       .render()
       .getContainerElement()
-      .appendChild($('.dashboard-body')[0]);
-
-    //
-    // DASHBOARD EDITOR
-    //
-
-    new Dashboard({
-      id: 'dashboard',
-      el: $('.dashboard-body'),
-      showTitle: true,
-      editable: true
-    }, { tokens: true }).render();
-
-
-    // Initialize time tokens to default
-    if (!defaultTokenModel.has('earliest') && !defaultTokenModel.has('latest')) {
-      defaultTokenModel.set({ earliest: '0', latest: '' });
-    }
-
-    // DASHBOARD READY
-    //
-
-    DashboardController.ready();
-    pageLoading = false;
+      .appendChild($('.dashboard-body')[0])
   }
-);
-// ]]>
+)
