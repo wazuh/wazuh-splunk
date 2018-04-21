7b8442e36fb9470868455146f8d1a73f0d04acff
manuasir <manu.asi2@gmail.com>
2018-04-21 21:55:48 +0200

    Improving manager configuration tab, deleting unused event listeners and token models

--- wazuh/appserver/static/js/viewControllers/configuration.js
+++ wazuh/appserver/static/js/viewControllers/configuration.js
@@ -1,121 +1,19 @@
 
 require([
   "splunkjs/mvc",
-  "splunkjs/mvc/utils",
-  "splunkjs/mvc/tokenutils",
   "underscore",
   "jquery",
-  "splunkjs/mvc/simplexml",
-  "splunkjs/mvc/layoutview",
-  "splunkjs/mvc/simplexml/dashboardview",
-  "splunkjs/mvc/simplexml/dashboard/panelref",
-  "splunkjs/mvc/simplexml/element/chart",
-  "splunkjs/mvc/simplexml/element/event",
-  "splunkjs/mvc/simplexml/element/html",
-  "splunkjs/mvc/simplexml/element/list",
-  "splunkjs/mvc/simplexml/element/map",
-  "splunkjs/mvc/simplexml/element/single",
-  "splunkjs/mvc/simplexml/element/table",
-  "splunkjs/mvc/simplexml/element/visualization",
-  "splunkjs/mvc/simpleform/formutils",
-  "splunkjs/mvc/simplexml/eventhandler",
-  "splunkjs/mvc/simplexml/searcheventhandler",
-  "splunkjs/mvc/simpleform/input/dropdown",
-  "splunkjs/mvc/simpleform/input/radiogroup",
-  "splunkjs/mvc/simpleform/input/linklist",
-  "splunkjs/mvc/simpleform/input/multiselect",
-  "splunkjs/mvc/simpleform/input/checkboxgroup",
-  "splunkjs/mvc/simpleform/input/text",
-  "splunkjs/mvc/simpleform/input/timerange",
-  "splunkjs/mvc/simpleform/input/submit",
-  "splunkjs/mvc/searchmanager",
-  "splunkjs/mvc/savedsearchmanager",
-  "splunkjs/mvc/postprocessmanager",
-  "splunkjs/mvc/simplexml/urltokenmodel"
-  // Add comma-separated libraries and modules manually here, for example:
-  // ..."splunkjs/mvc/simplexml/urltokenmodel",
-  // "splunkjs/mvc/tokenforwarder"
+  "splunkjs/mvc/layoutview"
 ],
   function (
     mvc,
-    utils,
-    TokenUtils,
     _,
     $,
-    DashboardController,
-    LayoutView,
-    Dashboard,
-    PanelRef,
-    ChartElement,
-    EventElement,
-    HtmlElement,
-    ListElement,
-    MapElement,
-    SingleElement,
-    TableElement,
-    VisualizationElement,
-    FormUtils,
-    EventHandler,
-    SearchEventHandler,
-    DropdownInput,
-    RadioGroupInput,
-    LinkListInput,
-    MultiSelectInput,
-    CheckboxGroupInput,
-    TextInput,
-    TimeRangeInput,
-    SubmitButton,
-    SearchManager,
-    SavedSearchManager,
-    PostProcessManager,
-    UrlTokenModel
+    LayoutView
 
-    // Add comma-separated parameter names here, for example: 
-    // ...UrlTokenModel, 
-    // TokenForwarder
   ) {
 
-    var pageLoading = true;
-
-
-    // 
-    // TOKENS
-    //
-
-    // Create token namespaces
-    var urlTokenModel = new UrlTokenModel();
-    mvc.Components.registerInstance('url', urlTokenModel);
-    var defaultTokenModel = mvc.Components.getInstance('default', { create: true });
-    var submittedTokenModel = mvc.Components.getInstance('submitted', { create: true });
-    var service = mvc.createService({ owner: "nobody" });
-
-    urlTokenModel.on('url:navigate', function () {
-      defaultTokenModel.set(urlTokenModel.toJSON());
-      if (!_.isEmpty(urlTokenModel.toJSON()) && !_.all(urlTokenModel.toJSON(), _.isUndefined)) {
-        submitTokens();
-      } else {
-        submittedTokenModel.clear();
-      }
-    });
-
-    // Initialize tokens
-    defaultTokenModel.set(urlTokenModel.toJSON());
-
-    function submitTokens() {
-      // Copy the contents of the defaultTokenModel to the submittedTokenModel and urlTokenModel
-      FormUtils.submitForm({ replaceState: pageLoading });
-    }
-
-    function setToken(name, value) {
-      defaultTokenModel.set(name, value);
-      submittedTokenModel.set(name, value);
-    }
-
-    function unsetToken(name) {
-      defaultTokenModel.unset(name);
-      submittedTokenModel.unset(name);
-    }
-
+    const service = mvc.createService({ owner: "nobody" });
     $(document).ready(function () {
       service.request(
         "storage/collections/data/credentials/",
@@ -125,21 +23,11 @@ require([
         null,
         { "Content-Type": "application/json" }, null
       ).done(function (data) {
-        var parsedData = JSON.parse(data);
-        setToken('baseip', parsedData[0].baseip);
-        setToken('baseport', parsedData[0].baseport);
-        setToken('ipapi', parsedData[0].ipapi);
-        setToken('portapi', parsedData[0].portapi);
-        setToken('userapi', parsedData[0].userapi);
-        setToken('passwordapi', parsedData[0].passapi);
-        var tokens = mvc.Components.get("default");
-        var ipBase = tokens.get("baseip");
-        var ipApi = tokens.get("ipapi");
-        var portApi = tokens.get("portapi");
-        var passApi = tokens.get("passwordapi");
-        var userApi = tokens.get("userapi");
-        var portBase = tokens.get("baseport");
-        var endPoint = 'http://' + ipBase + ':' + portBase + '/custom/wazuh/manager/configuration?ip=' + ipApi + '&port=' + portApi + '&user=' + userApi + '&pass=' + passApi;
+        const jsonData = JSON.parse(data)
+        const url = window.location.href
+        const arr = url.split("/")
+        const baseUrl = arr[0] + "//" + arr[2]
+        const endPoint = baseUrl + '/custom/wazuh/manager/configuration?ip=' + jsonData[0].ipapi + '&port=' + jsonData[0].portapi + '&user=' + jsonData[0].userapi + '&pass=' + jsonData[0].passapi
         $.get(endPoint, function (data) {
           var jsonObj = JSON.parse(data);
           console.log(jsonObj);
@@ -348,63 +236,11 @@ require([
     })
 
 
-    //
-    // SEARCH MANAGERS
-    //
-
-    //
-    // SPLUNK LAYOUT
-    //
-
     $('header').remove();
     new LayoutView({ "hideFooter": false, "hideSplunkBar": false, "hideAppBar": false, "hideChrome": false })
       .render()
       .getContainerElement()
       .appendChild($('.dashboard-body')[0]);
-
-    //
-    // DASHBOARD EDITOR
-    //
-
-    new Dashboard({
-      id: 'dashboard',
-      el: $('.dashboard-body'),
-      showTitle: true,
-      editable: true
-    }, { tokens: true }).render();
-
-
-    //
-    // VIEWS: VISUALIZATION ELEMENTS
-    //
-
-
-    //
-    // VIEWS: FORM INPUTS
-    //
-
-    DashboardController.onReady(function () {
-      if (!submittedTokenModel.has('earliest') && !submittedTokenModel.has('latest')) {
-        submittedTokenModel.set({ earliest: '0', latest: '' });
-      }
-    });
-
-    // Initialize time tokens to default
-    if (!defaultTokenModel.has('earliest') && !defaultTokenModel.has('latest')) {
-      defaultTokenModel.set({ earliest: '0', latest: '' });
-    }
-
-    if (!_.isEmpty(urlTokenModel.toJSON())) {
-      submitTokens();
-    }
-
-
-    //
-    // DASHBOARD READY
-    //
-
-    DashboardController.ready();
-    pageLoading = false;
 
   }
 );
