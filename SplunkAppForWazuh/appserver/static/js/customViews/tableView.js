b6fdda5f383fdd357e432640181f20b0edb01865
manuasir <manu.asi2@gmail.com>
2018-05-18 18:59:58 +0200

    Throwing errors in dynamic tables

--- SplunkAppForWazuh/appserver/static/js/customViews/tableView.js
+++ SplunkAppForWazuh/appserver/static/js/customViews/tableView.js
@@ -24,6 +24,7 @@ define(function (require, exports, module) {
     constructor() {
       this.$el = ""
       this.table = ""
+      $.fn.dataTable.ext.errMode = 'throw'
       // this.$el.DataTable({"retrieve": true}) 
     }
 
@@ -36,53 +37,67 @@ define(function (require, exports, module) {
      * @param {Object} opt: options
      */
     build(urlArg, opt) {
-
-      this.table = this.$el.DataTable({
-        "ordering": opt.ordering || true,
-        "retrieve": opt.retrieve || true,
-        "orderMulti": true,
-        "paging": true,
-        "processing": opt.processing || true,
-        "serverSide": opt.serverSide || true,
-        "pageLength": opt.pages || 10,
-        "ajax": {
-          url: urlArg,
-          type: opt.method || 'get',
-          dataFilter: (data) => {
-            let json = jQuery.parseJSON(data)
-            json.recordsTotal = json.data.totalItems
-            json.recordsFiltered = json.data.totalItems
-            json.data = json.data.items
-            return JSON.stringify(json) // return JSON string
+      try {
+        this.table = this.$el.DataTable({
+          "ordering": opt.ordering || true,
+          "retrieve": opt.retrieve || true,
+          "orderMulti": true,
+          "paging": true,
+          "processing": opt.processing || true,
+          "serverSide": opt.serverSide || true,
+          "pageLength": opt.pages || 10,
+          "ajax": {
+            url: urlArg,
+            type: opt.method || 'get',
+            dataFilter: (data) => {
+              let json = jQuery.parseJSON(data)
+              json.recordsTotal = json.data.totalItems
+              json.recordsFiltered = json.data.totalItems
+              json.data = json.data.items
+              return JSON.stringify(json) // return JSON string
+            },
           },
-        },
-        // "bFilter": opt.filterVisible || false,
-        // 'sDom': '<"top"i>rt<"bottom"flp><"clear">',
-        "columns": opt.columns
-      })
-      this.table.draw()
+          // "bFilter": opt.filterVisible || false,
+          // 'sDom': '<"top"i>rt<"bottom"flp><"clear">',
+          "columns": opt.columns,
+          "error": (xhr,error,thrown) => {
+            return Promise.reject(error)
+          }
+        })
+        this.table.draw()
+      } catch (err) {
+        return Promise.reject(err)
+      }
     }
 
     search($el) {
-      this.table.columns().every(function () {
-        const that = this;
-        $el.on('keyup change', function () {
-          if (that.search() !== this.value) {
-            that
-              .search(this.value)
-          }
+      try {
+        this.table.columns().every(function () {
+          const that = this;
+          $el.on('keyup change', function () {
+            if (that.search() !== this.value) {
+              that
+                .search(this.value)
+            }
+          })
         })
-      })
+      } catch (err) {
+        return Promise.reject(err)
+      }
     }
 
     /**
      * Click: perform a click in a row
      */
     click(cb) {
-      const myThis = this;
-      this.$el.on('click', 'tr', function() {
-        cb(myThis.table.row(this).data())
-      })
+      try {
+        const myThis = this;
+        this.$el.on('click', 'tr', function () {
+          cb(myThis.table.row(this).data())
+        })
+      } catch (err) {
+        return Promise.reject(err)
+      }
     }
   }
 
