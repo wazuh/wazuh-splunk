01b5eb537721889e105fd26af7feeb6e21b6d4de
manuasir <manu.asi2@gmail.com>
2018-06-15 15:57:25 +0200

    Getting most active useer from index and showing it in agent summary dashboard

--- SplunkAppForWazuh/appserver/static/js/viewControllers/agent_summary.js
+++ SplunkAppForWazuh/appserver/static/js/viewControllers/agent_summary.js
@@ -17,8 +17,16 @@ require([
   "/static/app/SplunkAppForWazuh/js/services/credentialService.js",
   "/static/app/SplunkAppForWazuh/js/services/apiService.js",
   "/static/app/SplunkAppForWazuh/js/directives/toaster.js",
-  "/static/app/SplunkAppForWazuh/js/directives/selectedCredentialsDirective.js"
-
+  "/static/app/SplunkAppForWazuh/js/directives/selectedCredentialsDirective.js",
+  'splunkjs/mvc',
+  "splunkjs/mvc/searchmanager",
+  "splunkjs/mvc/simplexml/element/table",
+  "splunkjs/mvc/utils",
+  "splunkjs/mvc/tokenutils",
+  "splunkjs/mvc/simplexml",
+  "splunkjs/mvc/layoutview",
+  "splunkjs/mvc/simplexml/eventhandler",
+  "splunkjs/mvc/simplexml/searcheventhandler"
 ],
   function (
     $,
@@ -27,13 +35,86 @@ require([
     CredentialService,
     ApiService,
     Toast,
-    SelectedCredentials
-
+    SelectedCredentials,
+    mvc,
+    SearchManager,
+    TableElement,
+    utils,
+    TokenUtils,
+    DashboardController,
+    LayoutView,
+    EventHandler,
+    SearchEventHandler
   ) {
 
-    const errorToast = new Toast('error', 'toast-bottom-right', 'Error at loading agent list', 1000, 250, 250)
 
-    CredentialService.checkSelectedApiConnection().then(({api}) => {
+
+    CredentialService.checkSelectedApiConnection().then(({ api, selectedIndex }) => {
+
+      const defaultTokenModel = mvc.Components.getInstance('default', { create: true })
+      const submittedTokenModel = mvc.Components.getInstance('submitted', { create: true })
+
+      const errorToast = new Toast('error', 'toast-bottom-right', 'Error at loading agent list', 1000, 250, 250)
+
+      const customErrorToast = (msg) => {
+        return new Toast('error', 'toast-bottom-right', msg, 1000, 250, 250)
+      }
+
+      const submitTokens = () => {
+        // Copy the contents of the defaultTokenModel to the submittedTokenModel and urlTokenModel
+        FormUtils.submitForm({ replaceState: pageLoading })
+      }
+
+      const setToken = (name, value) => {
+        defaultTokenModel.set(name, value)
+        submittedTokenModel.set(name, value)
+      }
+
+      const unsetToken = name => {
+        defaultTokenModel.unset(name)
+        submittedTokenModel.unset(name)
+      }
+
+      setToken('tokHTML', '--')
+      // Listen for a change to the token tokHTML value
+
+      const searchTopAgent = new SearchManager({
+        "id": "searchTopAgent",
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=wazuh | top agent.name",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": true
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+
+      new SearchEventHandler({
+        managerid: "searchTopAgent",
+        event: "done",
+        conditions: [
+          {
+            attr: "any",
+            value: "*",
+            actions: [
+              { "type": "set", "token": "tokHTML", "value": "$result.agent.name$" },
+            ]
+          }
+        ]
+      })
+
+      submittedTokenModel.on("change:tokHTML", function (model, tokHTML, options) {
+        var tokHTMLJS = submittedTokenModel.get("tokHTML");
+        if (tokHTMLJS !== undefined) {
+          $("#higherActivity").html(tokHTMLJS);
+        }
+      })
 
       /**
        * Initializes agent table and data from API
@@ -42,14 +123,16 @@ require([
         try {
 
           const data = await Promise.all([
-            ApiService.get('/agents/summary?ip='+api.url+'&port='+api.portapi+'&pass='+api.passapi+'&user='+api.userapi)
+            ApiService.get('/agents/summary?ip=' + api.url + '&port=' + api.portapi + '&pass=' + api.passapi + '&user=' + api.userapi),
+            ApiService.get('/agents/agents?ip=' + api.url + '&port=' + api.portapi + '&pass=' + api.passapi + '&user=' + api.userapi)
           ])
           $('#activeUsers').text(data[0][0].agent_summary_active)
           $('#neverConnected').text(data[0][0].agent_summary_neverconnected)
           $('#disconnectedUsers').text(data[0][0].agent_summary_disconnected)
-          $('#agentsCoverage').text(round((data[0][0].agent_summary_active / data[0][0].agent_summary_total * 100)))
+          $('#agentsCoverage').text(Math.round((data[0][0].agent_summary_active / data[0][0].agent_summary_total * 100)))
+          $('#lastRegisteredAgent').text((data[1].data.items[0].name))
 
-          SelectedCredentials.render($('#selectedCredentials'),api.filter[1])
+          SelectedCredentials.render($('#selectedCredentials'), api.filter[1])
           const baseUrl = ApiService.getBaseUrl()
           const urlData = {
             baseUrl: baseUrl,
@@ -61,7 +144,7 @@ require([
           const table = new agentsTable($('#row1'))
           table.build(urlData)
         } catch (err) {
-          errorToast.show()
+          customErrorToast(err).show()
         }
       }
 
