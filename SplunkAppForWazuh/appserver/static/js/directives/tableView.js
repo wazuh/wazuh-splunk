0b97e8c9b14822c57e505deabff17edb3a6b4197
manuasir <manu.asi2@gmail.com>
2018-06-18 12:27:37 +0200

    Deleting default filters from table

--- SplunkAppForWazuh/appserver/static/js/directives/tableView.js
+++ SplunkAppForWazuh/appserver/static/js/directives/tableView.js
@@ -63,7 +63,7 @@ define(function (require, exports, module) {
               return JSON.stringify(json) // return JSON string
             },
           },
-           "dom": '<"top"f>rt<"bottom"ipl>',
+           "dom": '<"top">rt<"bottom"ip>',
           // "bFilter": opt.filterVisible || false,
           // 'sDom': '<"top"i>rt<"bottom"flp><"clear">',
           "columns": opt.columns,
