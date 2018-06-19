b5202d231192f1f8cfdb8d36b7c3e1ff2942e8bf
manuasir <manu.asi2@gmail.com>
2018-06-20 00:35:46 +0200

    First fix approach

--- TA-wazuh-api-connector/bin/input_module_wazuh_api_agents.py
+++ TA-wazuh-api-connector/bin/input_module_wazuh_api_agents.py
@@ -15,31 +15,19 @@ def collect_events(helper, ew):
     verify = False
 
     request = requests.get(opt_base_url + '/agents?limit=0', auth=auth, verify=verify)
-    agents_qty = json.loads(request.text)["data"]["totalItems"]
+    total_agents = json.loads(request.text)["data"]["totalItems"]
+    limit = 10
+    offset = 0
 
-    request = requests.get(opt_base_url + '/agents?offset=0&limit=' + str(agents_qty), auth=auth, verify=verify)
+    request = requests.get(opt_base_url + '/agents?offset='+str(offset)+'&limit=' + str(limit), auth=auth, verify=verify)
     agents = json.loads(request.text)["data"]["items"]
-
-    for row in agents:
-        data = {}
-        request = requests.get(opt_base_url + '/agents/' + row["id"], auth=auth, verify=verify)
-        agent_info = json.loads(request.text)["data"]
-
-        request = requests.get(opt_base_url + '/rootcheck/' + row["id"] + '/last_scan', auth=auth, verify=verify)
-        rootcheck_lastscan = json.loads(request.text)["data"]["start"]
-
-        request = requests.get(opt_base_url + '/syscheck/' + row["id"] + '/last_scan', auth=auth, verify=verify)
-        syscheck_lastscan = json.loads(request.text)["data"]["start"]
-
-        keys = ["id", "status", "name", "ip", "dateAdd", "version", "os_family", "lastKeepAlive", "os"]
-
-        data = {}
-        for key in keys:
-            if key in agent_info:
-                data[key] = agent_info[key]
-
-        data["last_rootcheck"] = rootcheck_lastscan
-        data["las_syscheck"] = syscheck_lastscan
-
-        event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=json.dumps(data))
+    for agent in agents:
+        event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=json.dumps(agent))
         ew.write_event(event)
+    while (offset <= total_agents-limit):
+        offset+=limit
+        request = requests.get(opt_base_url + '/agents?offset='+str(offset)+'&limit=' + str(limit), auth=auth, verify=verify)
+        agents = json.loads(request.text)["data"]["items"]
+        for agent in agents:
+            event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=json.dumps(agent))
+            ew.write_event(event)
