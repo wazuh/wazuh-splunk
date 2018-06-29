8d4ef4c03938844a7117227a65eee2d6816dfc75
manuasir <manu.asi2@gmail.com>
2018-06-29 16:51:04 +0200

    Fixing agents group configuration visualization

--- SplunkAppForWazuh/appserver/static/js/viewControllers/settings.js
+++ SplunkAppForWazuh/appserver/static/js/viewControllers/settings.js
@@ -382,10 +382,7 @@ require([
         await autoSelectApi()
         await loadAboutContent()
         const { api } = await CredentialService.checkSelectedApiConnection()
-        const parentElement = $('img[src="/en-US/splunkd/__raw/servicesNS/admin/SplunkAppForWazuh/static/appLogo.png"]').parent().parent().parent().parent()
-        const logo = $('img[src="/en-US/splunkd/__raw/servicesNS/admin/SplunkAppForWazuh/static/appLogo.png"]')
-        $(parentElement).prepend('<p>TESTING</p>')
-        SelectedCredentials.render(element, api.filter[1])
+        SelectedCredentials.render($('#selectedCredentials'), api.filter[1])
         await drawApiList()
         successConnectionToast.show()
       } catch (err) {
