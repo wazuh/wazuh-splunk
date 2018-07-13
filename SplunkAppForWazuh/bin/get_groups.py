8890f794ca0866b1d267b7801069075ecba57bb2
manuasir <manu.asi2@gmail.com>
2018-07-13 17:21:53 +0200

    Added binary scripts for stream commands

---
+++ SplunkAppForWazuh/bin/get_groups.py
@@ -0,0 +1,21 @@
+#!/opt/splunk/bin/python
+############################################################
+#
+# GET /manager/groups
+#
+############################################################
+import sys
+import splunk.Intersplunk as si
+import requests
+import json
+
+try:
+    request = requests.get(sys.argv[1]+":"+sys.argv[2]+"/en-US/custom/SplunkAppForWazuh/manager/groups?ip="+sys.argv[3]+"&port="+sys.argv[4]+"&user="+sys.argv[5]+"&pass="+sys.argv[6])
+    data = json.loads(request.text)
+except Exception as err:
+        import traceback
+        print err
+        stack = traceback.format_exc()
+        data = si.generateErrorResults("Error : Traceback: " + str(stack))
+
+si.outputResults(data)
---
+++ SplunkAppForWazuh/bin/get_manager_status.py
@@ -0,0 +1,22 @@
+#!/opt/splunk/bin/python
+############################################################
+#
+# GET /manager/status
+#
+############################################################
+import sys
+import splunk.Intersplunk as si
+import requests
+import json
+
+try:
+
+    request = requests.get(str(sys.argv[1])+"/en-US/custom/SplunkAppForWazuh/manager/status?ip="+sys.argv[2]+"&port="+sys.argv[3]+"&user="+sys.argv[4]+"&pass="+sys.argv[5],verify=False)
+    data = json.loads(request.text)
+except Exception as err:
+        import traceback
+        print err
+        stack = traceback.format_exc()
+        data = si.generateErrorResults("Error : Traceback: " + str(stack))
+
+si.outputResults(data)
