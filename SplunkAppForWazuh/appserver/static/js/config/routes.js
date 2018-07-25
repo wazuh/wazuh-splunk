a68da4bd253211873f7976202be49bab17368134
manuasir <manu.asi2@gmail.com>
2018-07-25 11:42:58 +0200

    Setting up overview audit state

--- SplunkAppForWazuh/appserver/static/js/config/routes.js
+++ SplunkAppForWazuh/appserver/static/js/config/routes.js
@@ -1,14 +1,14 @@
 define(['./module'], function (module) {
-  'use strict';
+  'use strict'
   module.config(['$mdIconProvider', '$locationProvider', '$stateProvider', '$mdThemingProvider', function ($mdIconProvider, $locationProvider, $stateProvider, $mdThemingProvider, $navigationService) {
-    $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('blue');
+    $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('blue')
     $locationProvider.html5Mode({
       'enabled': true,
       'requirebase': false,
       'rewriteLinks': false
     })
     $stateProvider
-      .state('overview', { templateUrl: 'static/app/SplunkAppForWazuh/views/overview/overview-welcome.html', onEnter: ($navigationService) => { $navigationService.storeRoute('overview'); } })
+      .state('overview', { templateUrl: 'static/app/SplunkAppForWazuh/views/overview/overview-welcome.html', onEnter: ($navigationService) => { $navigationService.storeRoute('overview') } })
       // Overview - General
       .state('general', {
         templateUrl: 'static/app/SplunkAppForWazuh/views/overview/overview-general.html',
@@ -37,11 +37,11 @@ define(['./module'], function (module) {
         controllerAs: 'oac',
       })
       // Manager
-      .state('manager', { templateUrl: 'static/app/SplunkAppForWazuh/views/manager.html', onEnter: ($navigationService) => { $navigationService.storeRoute('manager'); } })
+      .state('manager', { templateUrl: 'static/app/SplunkAppForWazuh/views/manager.html', onEnter: ($navigationService) => { $navigationService.storeRoute('manager') } })
       // settings
-      .state('settings', { abstract: true, templateUrl: 'static/app/SplunkAppForWazuh/views/settings/settings.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.api'); } })
+      .state('settings', { abstract: true, templateUrl: 'static/app/SplunkAppForWazuh/views/settings/settings.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.api') } })
       // settings -> about
-      .state('settings.about', { templateUrl: '/static/app/SplunkAppForWazuh/views/settings/about.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.about'); } })
+      .state('settings.about', { templateUrl: '/static/app/SplunkAppForWazuh/views/settings/about.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.about') } })
       // settings -> api
       .state('settings.api', {
         templateUrl: '/static/app/SplunkAppForWazuh/views/settings/api.html',
@@ -52,13 +52,13 @@ define(['./module'], function (module) {
           apiList: ['$credentialService', ($credentialService) => {
             return $credentialService.getApiList()
               .then(function (response) {
-                return response;
+                return response
               }, function (response) {
-                return response;
+                return response
               })
           }]
         }
       })
-      .state('settings.index', { templateUrl: '/static/app/SplunkAppForWazuh/views/settings/index.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.index'); } })
-  }]);
-});
+      .state('settings.index', { templateUrl: '/static/app/SplunkAppForWazuh/views/settings/index.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.index') } })
+  }])
+})
