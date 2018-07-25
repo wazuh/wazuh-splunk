31c31f0f3c81bba3f9f55e8e74884e2d7a0bd811
manuasir <manu.asi2@gmail.com>
2018-07-25 11:42:36 +0200

    Registered audit controller module

--- SplunkAppForWazuh/appserver/static/js/controllers/index.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/index.js
@@ -2,9 +2,10 @@ define([
   './overview/overviewGeneralCtrl',
   './overview/overviewFimCtrl',
   './overview/overviewPolicyMonitoringCtrl',
+  './overview/overviewAuditCtrl',
   './management/managerCtrl',
   './settings/settingsCtrl',
   './settings/settingsIndexCtrl',
   './settings/settingsApiCtrl',
   './main/mainCtrl'
-], function () {});
+], function () {})
