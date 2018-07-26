4e1aba5805ccbcbe05421530444cbd2192cbd538
manuasir <manu.asi2@gmail.com>
2018-07-26 19:24:24 +0200

    Added Overview vulnerabilities first approach

---
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/overviewVulnerabilitiesCtrl.js
@@ -0,0 +1,486 @@
+define([
+  '../module',
+  "splunkjs/mvc",
+  "splunkjs/mvc/utils",
+  "splunkjs/mvc/tokenutils",
+  "underscore",
+  "jquery",
+  "splunkjs/mvc/simplexml",
+  "splunkjs/mvc/simplexml/dashboardview",
+  "splunkjs/mvc/simplexml/element/chart",
+  "splunkjs/mvc/simplexml/element/table",
+  "splunkjs/mvc/simpleform/formutils",
+  "splunkjs/mvc/simplexml/searcheventhandler",
+  "splunkjs/mvc/simpleform/input/timerange",
+  "splunkjs/mvc/searchmanager",
+  "splunkjs/mvc/simplexml/urltokenmodel"
+], function (controllers,
+  mvc,
+  utils,
+  TokenUtils,
+  _,
+  $,
+  DashboardController,
+  Dashboard,
+  ChartElement,
+  TableElement,
+  FormUtils,
+  SearchEventHandler,
+  TimeRangeInput,
+  SearchManager,
+  UrlTokenModel) {
+
+    'use strict'
+
+    controllers.controller('overviewVulnerabilitiesCtrl', function ($scope, $currentApiIndexService) {
+      const vm = this
+      const epoch = (new Date).getTime()
+      let pageLoading = false
+      // Create token namespaces
+      const urlTokenModel = new UrlTokenModel({ id: 'tokenModel' + epoch })
+      mvc.Components.registerInstance('url' + epoch, urlTokenModel)
+      const defaultTokenModel = mvc.Components.getInstance('default', { create: true })
+      const submittedTokenModel = mvc.Components.getInstance('submitted', { create: true })
+      const selectedIndex = $currentApiIndexService.getIndex()
+
+      const filter = $currentApiIndexService.getFilter()
+      const nameFilter = filter[0] + '=' + filter[1]
+
+      urlTokenModel.on('url:navigate', function () {
+        defaultTokenModel.set(urlTokenModel.toJSON())
+        if (!_.isEmpty(urlTokenModel.toJSON()) && !_.all(urlTokenModel.toJSON(), _.isUndefined)) {
+          submitTokens()
+        } else {
+          submittedTokenModel.clear()
+        }
+      })
+
+      // Initialize tokens
+      defaultTokenModel.set(urlTokenModel.toJSON())
+
+      const submitTokens = () => {
+        FormUtils.submitForm({ replaceState: pageLoading })
+      }
+
+      let criticalSeveritySearch = ''
+      let highSeveritySeach = ''
+      let mediumSeveritySearch = ''
+      let lowSeveritySearch = ''
+      let alertsSeverityOverTime = ''
+      let topAgentsSeverity = ''
+      let affectedPackages = ''
+      let alertsSummary = ''
+      let element1 = ''
+      let element2 = ''
+      let element3 = ''
+      let element4 = ''
+
+      /**
+       * When controller is destroyed
+       */
+      $scope.$on('$destroy', () => {
+        criticalSeveritySearch = null
+        highSeveritySeach = null
+        mediumSeveritySearch = null
+        lowSeveritySearch = null
+        alertsSeverityOverTime = null
+        topAgentsSeverity = null
+        affectedPackages = null
+        alertsSummary = null
+        element1 = null
+        element2 = null
+        element3 = null
+        element4 = null
+      })
+
+      // Listen for a change to the token tokenTotalAlerts value
+      criticalSeveritySearch = new SearchManager({
+        "id": "criticalSeveritySearch" + epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" rule.id=80790 | stats count",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": true
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+
+      new SearchEventHandler({
+        managerid: "criticalSeveritySearch" + epoch,
+        event: "done",
+        conditions: [
+          {
+            attr: "any",
+            value: "*",
+            actions: [
+              { "type": "set", "token": "filesAddedToken", "value": "$result.count$" },
+            ]
+          }
+        ]
+      })
+
+      submittedTokenModel.on("change:filesAddedToken", (model, filesAddedToken, options) => {
+        const filesAddedTokenJS = submittedTokenModel.get("filesAddedToken")
+        if (typeof filesAddedTokenJS !== 'undefined' && filesAddedTokenJS !== 'undefined') {
+          vm.newFiles = filesAddedTokenJS
+          if (!$scope.$$phase) $scope.$digest()
+        }
+      })
+
+      // Listen for a change to the token tokenTotalAlerts value
+      highSeveritySeach = new SearchManager({
+        "id": "highSeveritySeach" + epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" rule.id=80784 | stats count",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": true
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+      new SearchEventHandler({
+        managerid: "highSeveritySeach" + epoch,
+        event: "done",
+        conditions: [
+          {
+            attr: "any",
+            value: "*",
+            actions: [
+              { "type": "set", "token": "readFilesToken", "value": "$result.count$" },
+            ]
+          }
+        ]
+      })
+
+      submittedTokenModel.on("change:readFilesToken", (model, readFilesToken, options) => {
+        const readFilesTokenJS = submittedTokenModel.get("readFilesToken")
+        if (typeof readFilesTokenJS !== 'undefined' && readFilesTokenJS !== 'undefined') {
+          vm.readFiles = readFilesTokenJS
+          if (!$scope.$$phase) $scope.$digest()
+        }
+      })
+
+      // Listen for a change to the token tokenTotalAlerts value
+      mediumSeveritySearch = new SearchManager({
+        "id": "mediumSeveritySearch" + epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" rule.id=80781 | stats count",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": true
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+      new SearchEventHandler({
+        managerid: "mediumSeveritySearch" + epoch,
+        event: "done",
+        conditions: [
+          {
+            attr: "any",
+            value: "*",
+            actions: [
+              { "type": "set", "token": "filesModifiedToken", "value": "$result.count$" },
+            ]
+          }
+        ]
+      })
+
+      submittedTokenModel.on("change:filesModifiedToken", (model, filesModifiedToken, options) => {
+        const filesDeletedTokenJS = submittedTokenModel.get("filesModifiedToken")
+        if (typeof filesModifiedTokenJS !== 'undefined' && filesModifiedTokenJS !== 'undefined') {
+          vm.filesModifiedToken = filesModifiedToken
+          if (!$scope.$$phase) $scope.$digest()
+        }
+      })
+
+      // Listen for a change to the token tokenTotalAlerts value
+      lowSeveritySearch = new SearchManager({
+        "id": "lowSeveritySearch" + epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" rule.id=80791 | stats count",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": true
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+      new SearchEventHandler({
+        managerid: "filesDeletedSearch" + epoch,
+        event: "done",
+        conditions: [
+          {
+            attr: "any",
+            value: "*",
+            actions: [
+              { "type": "set", "token": "filesDeletedToken", "value": "$result.count$" },
+            ]
+          }
+        ]
+      })
+
+      submittedTokenModel.on("change:filesDeletedToken", (model, filesDeletedToken, options) => {
+        const filesDeletedTokenJS = submittedTokenModel.get("filesDeletedToken")
+        if (typeof filesDeletedTokenJS !== 'undefined' && filesDeletedTokenJS !== 'undefined') {
+          vm.filesDeleted = filesDeletedTokenJS
+          if (!$scope.$$phase) $scope.$digest()
+        }
+      })
+
+      alertsSeverityOverTime = new SearchManager({
+        "id": "alertsSeverityOverTime"+epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" | top rule.alertsSeverityOverTime",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": false
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+      topAgentsSeverity = new SearchManager({
+        "id": "topAgentsSeverity"+epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" agent.name=* | top agent.name",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": false
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+      affectedPackages = new SearchManager({
+        "id": "affectedPackages"+epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" audit.directory.name=* | top audit.directory.name",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": false
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+      alertsSummary = new SearchManager({
+        "id": "alertsSummary"+epoch,
+        "cancelOnUnload": true,
+        "sample_ratio": 1,
+        "earliest_time": "$when.earliest$",
+        "status_buckets": 0,
+        "search": "index=" + selectedIndex + " " + nameFilter + " sourcetype=wazuh rule.alertsSeverityOverTime=\"audit\" audit.file.name=* | top audit.file.name",
+        "latest_time": "$when.latest$",
+        "app": utils.getCurrentApp(),
+        "auto_cancel": 90,
+        "preview": true,
+        "tokenDependencies": {
+        },
+        "runWhenTimeIsUndefined": false
+      }, { tokens: true, tokenNamespace: "submitted" })
+
+
+      element1 = new ChartElement({
+        "id": "element6"+epoch,
+        "charting.axisY2.scale": "inherit",
+        "trellis.size": "medium",
+        "charting.chart.stackMode": "default",
+        "resizable": true,
+        "charting.layout.splitSeries.allowIndependentYRanges": "0",
+        "charting.drilldown": "none",
+        "charting.chart.nullValueMode": "gaps",
+        "charting.axisTitleY2.visibility": "visible",
+        "charting.chart": "pie",
+        "trellis.scales.shared": "1",
+        "charting.layout.splitSeries": "0",
+        "charting.chart.style": "shiny",
+        "charting.legend.labelStyle.overflowMode": "ellipsisMiddle",
+        "charting.axisTitleX.visibility": "visible",
+        "charting.axisTitleY.visibility": "visible",
+        "charting.axisX.scale": "linear",
+        "charting.chart.bubbleMinimumSize": "10",
+        "charting.axisLabelsX.majorLabelStyle.overflowMode": "ellipsisNone",
+        "charting.axisY2.enabled": "0",
+        "trellis.enabled": "0",
+        "charting.legend.placement": "right",
+        "charting.chart.bubbleSizeBy": "area",
+        "charting.chart.bubbleMaximumSize": "50",
+        "charting.axisLabelsX.majorLabelStyle.rotation": "0",
+        "charting.axisY.scale": "linear",
+        "charting.chart.showDataLabels": "none",
+        "charting.chart.sliceCollapsingThreshold": "0.01",
+        "managerid": "alertsSeverityOverTime"+epoch,
+        "el": $('#element6')
+      }, { tokens: true, tokenNamespace: "submitted" }).render()
+
+
+      element2 = new ChartElement({
+        "id": "element7"+epoch,
+        "charting.axisY2.scale": "inherit",
+        "trellis.size": "medium",
+        "charting.chart.stackMode": "default",
+        "resizable": true,
+        "charting.layout.splitSeries.allowIndependentYRanges": "0",
+        "charting.drilldown": "none",
+        "charting.chart.nullValueMode": "gaps",
+        "charting.axisTitleY2.visibility": "visible",
+        "charting.chart": "column",
+        "trellis.scales.shared": "1",
+        "charting.layout.splitSeries": "0",
+        "charting.chart.style": "shiny",
+        "charting.legend.labelStyle.overflowMode": "ellipsisMiddle",
+        "charting.axisTitleX.visibility": "visible",
+        "charting.axisTitleY.visibility": "visible",
+        "charting.axisX.scale": "linear",
+        "charting.chart.bubbleMinimumSize": "10",
+        "charting.axisLabelsX.majorLabelStyle.overflowMode": "ellipsisNone",
+        "charting.axisY2.enabled": "0",
+        "trellis.enabled": "0",
+        "charting.legend.placement": "none",
+        "charting.chart.bubbleSizeBy": "area",
+        "charting.chart.bubbleMaximumSize": "50",
+        "charting.axisLabelsX.majorLabelStyle.rotation": "0",
+        "charting.axisY.scale": "linear",
+        "charting.chart.showDataLabels": "none",
+        "charting.chart.sliceCollapsingThreshold": "0.01",
+        "managerid": "topAgentsSeverity"+epoch,
+        "el": $('#element7')
+      }, { tokens: true, tokenNamespace: "submitted" }).render()
+
+
+      element3 = new ChartElement({
+        "id": "element8"+epoch,
+        "charting.axisY2.scale": "inherit",
+        "trellis.size": "medium",
+        "charting.chart.stackMode": "default",
+        "resizable": true,
+        "charting.layout.splitSeries.allowIndependentYRanges": "0",
+        "charting.drilldown": "none",
+        "charting.chart.nullValueMode": "gaps",
+        "charting.axisTitleY2.visibility": "visible",
+        "charting.chart": "pie",
+        "trellis.scales.shared": "1",
+        "charting.layout.splitSeries": "0",
+        "charting.chart.style": "shiny",
+        "charting.legend.labelStyle.overflowMode": "ellipsisMiddle",
+        "charting.axisTitleX.visibility": "visible",
+        "charting.axisTitleY.visibility": "visible",
+        "charting.axisX.scale": "linear",
+        "charting.chart.bubbleMinimumSize": "10",
+        "charting.axisLabelsX.majorLabelStyle.overflowMode": "ellipsisNone",
+        "charting.axisY2.enabled": "0",
+        "trellis.enabled": "0",
+        "charting.legend.placement": "right",
+        "charting.chart.bubbleSizeBy": "area",
+        "charting.chart.bubbleMaximumSize": "50",
+        "charting.axisLabelsX.majorLabelStyle.rotation": "0",
+        "charting.axisY.scale": "linear",
+        "charting.chart.showDataLabels": "none",
+        "charting.chart.sliceCollapsingThreshold": "0.01",
+        "managerid": "affectedPackages"+epoch,
+        "el": $('#element8')
+      }, { tokens: true, tokenNamespace: "submitted" }).render()
+
+
+      element4 = new ChartElement({
+        "id": "element9"+epoch,
+        "charting.axisY2.scale": "inherit",
+        "trellis.size": "medium",
+        "charting.chart.stackMode": "default",
+        "resizable": true,
+        "charting.layout.splitSeries.allowIndependentYRanges": "0",
+        "charting.drilldown": "none",
+        "charting.chart.nullValueMode": "gaps",
+        "charting.axisTitleY2.visibility": "visible",
+        "charting.chart": "pie",
+        "trellis.scales.shared": "1",
+        "charting.layout.splitSeries": "0",
+        "charting.chart.style": "shiny",
+        "charting.legend.labelStyle.overflowMode": "ellipsisMiddle",
+        "charting.axisTitleX.visibility": "visible",
+        "charting.axisTitleY.visibility": "visible",
+        "charting.axisX.scale": "linear",
+        "charting.chart.bubbleMinimumSize": "10",
+        "charting.axisLabelsX.majorLabelStyle.overflowMode": "ellipsisNone",
+        "charting.axisY2.enabled": "0",
+        "trellis.enabled": "0",
+        "charting.legend.placement": "right",
+        "charting.chart.bubbleSizeBy": "area",
+        "charting.chart.bubbleMaximumSize": "50",
+        "charting.axisLabelsX.majorLabelStyle.rotation": "0",
+        "charting.axisY.scale": "linear",
+        "charting.chart.showDataLabels": "none",
+        "charting.chart.sliceCollapsingThreshold": "0.01",
+        "managerid": "alertsSummary"+epoch,
+        "el": $('#element9')
+      }, { tokens: true, tokenNamespace: "submitted" }).render()
+
+      //
+      // VIEWS: FORM INPUTS
+      //
+
+      const input1 = new TimeRangeInput({
+        "id": "input1"+epoch,
+        "searchWhenChanged": true,
+        "default": { "latest_time": "now", "earliest_time": "-24h@h" },
+        "earliest_time": "$form.when.earliest$",
+        "latest_time": "$form.when.latest$",
+        "el": $('#input1')
+      }, { tokens: true }).render()
+
+      input1.on("change", (newValue) => {
+        FormUtils.handleValueChange(input1)
+      })
+
+      DashboardController.onReady(() => {
+        if (!submittedTokenModel.has('earliest') && !submittedTokenModel.has('latest')) {
+          submittedTokenModel.set({ earliest: '0', latest: '' })
+        }
+      })
+
+      // Initialize time tokens to default
+      if (!defaultTokenModel.has('earliest') && !defaultTokenModel.has('latest')) {
+        defaultTokenModel.set({ earliest: '0', latest: '' })
+      }
+
+      submitTokens()
+
+      DashboardController.ready()
+
+    })
+  })
