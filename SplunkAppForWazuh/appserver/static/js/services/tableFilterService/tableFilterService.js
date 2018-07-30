7e285dd5f936977eca7658567537aa1e9ee4ca5c
manuasir <manu.asi2@gmail.com>
2018-07-30 11:50:05 +0200

    Added table filter service for table directive

--- SplunkAppForWazuh/appserver/static/js/services/tableFilterService/tableFilterService.js
+++ SplunkAppForWazuh/appserver/static/js/services/tableFilterService/tableFilterService.js
@@ -1,10 +1,10 @@
 define(['../module'], function (module) {
   'use strict'
 
-  module.service('$tableFilterService', function ($state) {
+  module.service('$tableFilterService', function () {
     const filters = [];
     return {
-      set: array => { if (Array.isArray(array)) { filters.length = 0; filters.push(...array); } },
+      set: array => { if (Array.isArray(array)) { filters.length = 0; filters.push(...array) } },
       get: () => filters
     }
   })
