e074cfdbb6b8ae7d77fc94f9153b03b23f40200b
manuasir <manu.asi2@gmail.com>
2018-07-30 15:43:32 +0200

    Completing data service for tables

--- SplunkAppForWazuh/appserver/static/js/services/dataService/dataService.js
+++ SplunkAppForWazuh/appserver/static/js/services/dataService/dataService.js
@@ -12,94 +12,90 @@
 
 define(['../module', 'splunkjs/mvc'], function (module) {
   'use strict'
-  module.service('$dataService', function () {
+  module.service('$dataService', function ($apiService) {
     return class DataFactory {
-      constructor(httpClient, path, implicitFilter) {
-        this.implicitFilter = implicitFilter || false;
-        this.httpClient = httpClient;
-        this.items = [];
-        this.path = path;
-        this.filters = [];
-        this.sortValue = false;
-        this.sortDir = false;
-        this.sortValue = false;
+      constructor(path) {
+        this.items = []
+        this.path = path
+        this.filters = []
+        this.sortValue = false
+        this.sortDir = false
+        this.sortValue = false
       }
 
       addSorting(value) {
-        this.sortValue = value;
-        this.sortDir = !this.sortDir;
+        this.sortValue = value
+        this.sortDir = !this.sortDir
       }
 
       removeFilters() {
-        this.filters = [];
-        if (this.implicitFilter) this.filters.push(...this.implicitFilter);
+        this.filters = []
       }
 
       serializeFilters(parameters) {
         if (this.sortValue) {
-          parameters.sort = this.sortDir ? '-' + this.sortValue : this.sortValue;
+          parameters.sort = this.sortDir ? '-' + this.sortValue : this.sortValue
         }
 
         for (const filter of this.filters) {
-          if (filter.value !== '') parameters[filter.name] = filter.value;
+          if (filter.value !== '') parameters[filter.name] = filter.value
         }
       }
 
       addFilter(filterName, value) {
-        this.filters = this.filters.filter(filter => filter.name !== filterName);
+        this.filters = this.filters.filter(filter => filter.name !== filterName)
 
         if (typeof value !== 'undefined') {
           this.filters.push({
             name: filterName,
             value: value
-          });
+          })
         }
       }
 
       async fetch(options = {}) {
         try {
-          const start = new Date();
+          const start = new Date()
 
           // If offset is not given, it means we need to start again
-          if (!options.offset) this.items = [];
-          const offset = options.offset || 0;
-          const limit = options.limit || 500;
-          const parameters = { limit, offset };
+          if (!options.offset) this.items = []
+          const offset = options.offset || 0
+          const limit = options.limit || 500
+          const parameters = { limit, offset }
 
-          this.serializeFilters(parameters);
+          this.serializeFilters(parameters)
 
           // Fetch next <limit> items
-          const firstPage = await this.httpClient.request('GET', this.path, parameters);
-          this.items = this.items.filter(item => !!item);
-          this.items.push(...firstPage.data.data.items);
+          const firstPage = await $apiService.get(this.path)
+          this.items = this.items.filter(item => !!item)
+          this.items.push(...firstPage.data.data.items)
 
-          const totalItems = firstPage.data.data.totalItems;
+          const totalItems = firstPage.data.data.totalItems
 
           const remaining = this.items.length === totalItems ? 0 :
-            totalItems - this.items.length;
+            totalItems - this.items.length
 
           // Ignore manager as an agent, once the team solves this issue, review this line
-          if (this.path === '/agents') this.items = this.items.filter(item => item.id !== '000');
+          if (this.path === '/agents') this.items = this.items.filter(item => item.id !== '000')
 
-          if (remaining > 0) this.items.push(...Array(remaining).fill(null));
+          if (remaining > 0) this.items.push(...Array(remaining).fill(null))
 
-          const end = new Date();
-          const elapsed = (end - start) / 1000;
+          const end = new Date()
+          const elapsed = (end - start) / 1000
 
-          return { items: this.items, time: elapsed };
+          return { items: this.items, time: elapsed }
 
         } catch (error) {
-          return Promise.reject(error);
+          return Promise.reject(error)
         }
       }
 
       reset() {
-        this.items = [];
-        this.filters = [];
-        if (this.implicitFilter) this.filters.push(...this.implicitFilter);
-        this.sortValue = false;
-        this.sortDir = false;
-        this.sortValue = false;
+        this.items = []
+        this.filters = []
+        this.sortValue = false
+        this.sortDir = false
+        this.sortValue = false
       }
     }
   })
