3341f955ac2b352811da2d75db2ef1ae8eddb674
manuasir <manu.asi2@gmail.com>
2018-08-03 18:32:21 +0200

    Registered directive into interface module

--- SplunkAppForWazuh/appserver/static/js/directives/index.js
+++ SplunkAppForWazuh/appserver/static/js/directives/index.js
@@ -2,5 +2,6 @@ define([
   './wz-menu/wz-menu',
   './wz-table/wz-table',
   './wz-enter/wz-enter',
-  './wz-dynamic/wz-dynamic'
+  './wz-dynamic/wz-dynamic',
+  './wz-welcome-card/wz-welcome-card'
 ], function () {})
