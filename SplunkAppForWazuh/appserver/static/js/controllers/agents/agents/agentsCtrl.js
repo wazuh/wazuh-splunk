054e5d17a95dcf3a4f2e431c7c3f6597b8a373ac
manuasir <manu.asi2@gmail.com>
2018-08-06 17:09:26 +0200

    Fixed filters

--- SplunkAppForWazuh/appserver/static/js/controllers/agents/agents/agentsCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/agents/agentsCtrl.js
@@ -10,131 +10,127 @@
  * Find more information about this on the LICENSE file.
  */
 
-define(['../../module', 
-'splunkjs/mvc',
-'splunkjs/mvc/simplexml/searcheventhandler',
-'splunkjs/mvc/searchmanager',
-"splunkjs/mvc/utils"], function (controllers, mvc, SearchEventHandler, SearchManager,utils) {
+define([
+  '../../module',
+  'splunkjs/mvc',
+  'splunkjs/mvc/simplexml/searcheventhandler',
+  'splunkjs/mvc/searchmanager',
+  "splunkjs/mvc/utils"], function (controllers, mvc, SearchEventHandler, SearchManager, utils) {
 
-  'use strict'
+    'use strict'
 
-  controllers.controller('agentsCtrl', function ($scope, $currentApiIndexService, data) {
-    const vm = this
-    const submittedTokenModel = mvc.Components.getInstance('submitted', { create: true })
+    controllers.controller('agentsCtrl', function ($scope, $currentApiIndexService, data) {
+      const vm = this
+      const submittedTokenModel = mvc.Components.getInstance('submitted', { create: true })
 
-    vm.search = term => {
-      $scope.$broadcast('wazuhSearch', { term })
-    }
-
-    vm.filter = filter => {
-      $scope.$broadcast('wazuhFilter', { filter })
-    }
-
-    vm.isClusterEnabled = $currentApiIndexService.getClusterInfo() && $currentApiIndexService.getClusterInfo().status === 'enabled'
-    vm.loading = true
-
-    vm.status = 'all'
-    vm.osPlatform = 'all'
-    vm.version = 'all'
-    vm.osPlatforms = []
-    vm.versions = []
-    vm.groups = []
-    vm.nodes = []
-    vm.node_name = 'all'
-    vm.mostActiveAgent = {
-      name: '',
-      id: ''
-    }
-
-    const load = async () => {
-      try {
-        const clusterInfo = $currentApiIndexService.getClusterInfo()
-        const firstUrlParam = clusterInfo.status === 'enabled' ? 'cluster' : 'manager'
-        const epoch = (new Date).getTime()
-        
-        console.log('data ', data)
-        // const unique = data[0].data.result
-        const selectedIndex = $currentApiIndexService.getIndex()
-        const filter = $currentApiIndexService.getFilter()
-        const nameFilter = filter[0] + '=' + filter[1]
-        const searchTopAgent = new SearchManager({
-          "id": `searchTopAgent${epoch}`,
-          "cancelOnUnload": true,
-          "sample_ratio": 1,
-          "earliest_time": "$when.earliest$",
-          "status_buckets": 0,
-          "search": "index=" + selectedIndex + " " + nameFilter + "| top agent.name",
-          "latest_time": "$when.latest$",
-          "app": utils.getCurrentApp(),
-          "auto_cancel": 90,
-          "preview": true,
-          "tokenDependencies": {
-          },
-          "runWhenTimeIsUndefined": true
-        }, { tokens: true, tokenNamespace: "submitted" })
-
-        new SearchEventHandler({
-          managerid: `searchTopAgent${epoch}`,
-          event: "done",
-          conditions: [
-            {
-              attr: "any",
-              value: "*",
-              actions: [
-                { "type": "set", "token": "activeAgentToken", "value": "$result.agent.name$" },
-              ]
-            }
-          ]
-        })
-
-        submittedTokenModel.on("change:activeAgentToken", function (model, activeAgentToken, options) {
-          const activeAgentTokenJS = submittedTokenModel.get("activeAgentToken");
-          if (activeAgentTokenJS !== undefined) {
-            vm.mostActiveAgent = `${activeAgentTokenJS}`
-            if (!$scope.$$phase) $scope.$digest()
-          }
-        })
-        const summary = data[0].data.data
-        const lastAgent = data[1].data.data.items[0]
-        // vm.groups = unique.groups
-        // vm.nodes = unique.nodes
-        // vm.versions = unique.versions
-        // vm.osPlatforms = unique.osPlatforms
-        vm.lastAgent = lastAgent
-        vm.agentsCountActive = summary.Active
-        vm.agentsCountDisconnected = summary.Disconnected
-        vm.agentsCountNeverConnected = summary['Never connected']
-        vm.agentsCountTotal = summary.Total 
-        vm.agentsCoverity = vm.agentsCountTotal ? (vm.agentsCountActive / vm.agentsCountTotal) * 100 : 0
-
-
-        // if (data[1].data.data === '') {
-        //   vm.mostActiveAgent.name = $currentApiIndexService.getClusterInfo().manager
-        //   vm.mostActiveAgent.id = '000'
-        // } else {
-        //   vm.mostActiveAgent.name = data[1].data.data
-
-        //   if (info.data.data === '' && vm.mostActiveAgent.name !== '') {
-        //     vm.mostActiveAgent.id = '000'
-        //   } else {
-        //     vm.mostActiveAgent.id = info.data.data
-        //   }
-        // }
-
-        vm.loading = false
-        if (!$scope.$$phase) $scope.$digest()
-        return
-      } catch (error) {
-        console.error('Agents Preview', error)
+      vm.search = term => {
+        $scope.$broadcast('wazuhSearch', { term })
       }
-      return
-    }
 
-    vm.showAgent = agent => {
-    }
+      vm.filter = filter => {
+        $scope.$broadcast('wazuhFilter', { filter })
+      }
 
-    //Load
-    load()
+      vm.isClusterEnabled = $currentApiIndexService.getClusterInfo() && $currentApiIndexService.getClusterInfo().status === 'enabled'
+      vm.loading = true
 
+      vm.status = 'all'
+      vm.osPlatform = 'all'
+      vm.version = 'all'
+      vm.osPlatforms = []
+      vm.versions = []
+      vm.groups = []
+      vm.nodes = []
+      vm.node_name = 'all'
+      vm.mostActiveAgent = {
+        name: '',
+        id: ''
+      }
+
+      const load = async () => {
+        try {
+          const clusterInfo = $currentApiIndexService.getClusterInfo()
+          const firstUrlParam = clusterInfo.status === 'enabled' ? 'cluster' : 'manager'
+          const epoch = (new Date).getTime()
+
+          console.log('data ', data)
+          // const unique = data[0].data.result
+          const selectedIndex = $currentApiIndexService.getIndex()
+          const filter = $currentApiIndexService.getFilter()
+          const nameFilter = filter[0] + '=' + filter[1]
+          const searchTopAgent = new SearchManager({
+            "id": `searchTopAgent${epoch}`,
+            "cancelOnUnload": true,
+            "sample_ratio": 1,
+            "earliest_time": "$when.earliest$",
+            "status_buckets": 0,
+            "search": "index=" + selectedIndex + " " + nameFilter + "| top agent.name",
+            "latest_time": "$when.latest$",
+            "app": utils.getCurrentApp(),
+            "auto_cancel": 90,
+            "preview": true,
+            "tokenDependencies": {
+            },
+            "runWhenTimeIsUndefined": true
+          }, { tokens: true, tokenNamespace: "submitted" })
+
+          const handlerTopAgent = new SearchEventHandler({
+            managerid: `searchTopAgent${epoch}`,
+            event: "done",
+            conditions: [
+              {
+                attr: "any",
+                value: "*",
+                actions: [
+                  { "type": "set", "token": "activeAgentToken", "value": "$result.agent.name$" },
+                ]
+              }
+            ]
+          })
+
+          submittedTokenModel.on("change:activeAgentToken", function (model, activeAgentToken, options) {
+            const activeAgentTokenJS = submittedTokenModel.get("activeAgentToken");
+            if (activeAgentTokenJS !== undefined) {
+              vm.mostActiveAgent = `${activeAgentTokenJS}`
+              if (!$scope.$$phase) $scope.$digest()
+            }
+          })
+
+          const summary = data[0].data.data
+          const lastAgent = data[1].data.data.items[0]
+          // vm.groups = unique.groups
+          // vm.nodes = unique.nodes
+          // vm.versions = unique.versions
+          // vm.osPlatforms = unique.osPlatforms
+
+          // Building operating system filter
+          const rawPlatforms = data[2].data.data.items.map(agent => !!agent.os)
+          vm.osPlatforms = [... new Set(rawPlatforms.filter(one => !!one))]
+
+          // Building version filter
+          const rawVersions = data[2].data.data.items.map(one => one.version);
+          vm.versions = [... new Set(rawVersions.filter(one => !!one))]
+
+          vm.lastAgent = lastAgent
+          vm.agentsCountActive = summary.Active
+          vm.agentsCountDisconnected = summary.Disconnected
+          vm.agentsCountNeverConnected = summary['Never connected']
+          vm.agentsCountTotal = summary.Total
+          vm.agentsCoverity = vm.agentsCountTotal ? (vm.agentsCountActive / vm.agentsCountTotal) * 100 : 0
+
+          vm.loading = false
+          if (!$scope.$$phase) $scope.$digest()
+          return
+        } catch (error) {
+          console.error('Agents Preview', error)
+        }
+        return
+      }
+
+      vm.showAgent = agent => {
+      }
+
+      //Load
+      load()
+    })
   })
-})
