583f13b555602bfca6fd79735227111d9239f71c
manuasir <manu.asi2@gmail.com>
2018-08-06 18:08:05 +0200

    First HTML and controller agent overview section

---
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/overview/agentsOverviewCtrl.js
@@ -0,0 +1,21 @@
+/*
+ * Wazuh app - Agents controller
+ * Copyright (C) 2018 Wazuh, Inc.
+ *
+ * This program is free software you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+define(['../../module'], function (controllers) {
+
+  'use strict'
+
+  controllers.controller('agentsOverviewCtrl', function ($scope, $currentApiIndexService, data) {
+    const vm = this
+
+  })
+})
