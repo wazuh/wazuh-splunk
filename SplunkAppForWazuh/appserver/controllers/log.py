958f8f32ac9880d7dc7b826c7a99c87455ff8751
manuasir <manu.asi2@gmail.com>
2018-09-10 10:57:02 +0200

    Added new tailer Python modules

--- SplunkAppForWazuh/appserver/controllers/log.py
+++ SplunkAppForWazuh/appserver/controllers/log.py
@@ -14,7 +14,7 @@ import sys
 import os
 from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "."))
-from tail import tail
+import tailer
 
 _APPNAME = 'SplunkAppForWazuh'
 
@@ -24,34 +24,35 @@ class log():
         """
         Setup a logger for the REST handler.
         """
-        self.logger = logging.getLogger(
-            'splunk.appserver.%s.controllers.logs' % _APPNAME)
+        # self.logger = logging.getLogger(
+        #     'splunk.appserver.%s.controllers.logs' % _APPNAME)
         try:
-            # Prevent the log messages from being duplicated in the python.log file
-            self.logger.propagate = False
-            self.logger.setLevel(logging.DEBUG)
-            self.file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(
-                ['var', 'log', 'splunk', 'SplunkAppForWazuh.log']), maxBytes=100000000, backupCount=0)
-            self.formatter = logging.Formatter(
-                '{ date: "%(asctime)s" , level: "%(levelname)s" , message: "%(message)s" }')
-            self.file_handler.setFormatter(self.formatter)
-            self.logger.addHandler(self.file_handler)
+            print('loaded')
+            # # Prevent the log messages from being duplicated in the python.log file
+            # self.logger.propagate = False
+            # self.logger.setLevel(logging.DEBUG)
+            # self.file_handler = logging.handlers.RotatingFileHandler(make_splunkhome_path(
+            #     ['var', 'log', 'splunk', 'SplunkAppForWazuh.log']), maxBytes=100000000, backupCount=0)
+            # self.formatter = logging.Formatter(
+            #     '{ date: "%(asctime)s" , level: "%(levelname)s" , message: "%(message)s" }')
+            # self.file_handler.setFormatter(self.formatter)
+            # self.logger.addHandler(self.file_handler)
         except Exception as e:
-            self.error('[log.py][constructor] %s' % (e))
+            # self.error('[log.py][constructor] %s' % (e))
             raise e
 
-    def error(self, msg):
-        self.logger.error(msg)
+    # def error(self, msg):
+    #     self.logger.error(msg)
 
-    def info(self, msg):
-        self.logger.info(msg)
+    # def info(self, msg):
+    #     self.logger.info(msg)
 
-    def get_last_log_lines(self, lines):
-        try:
-            current_tail = tail(
-                lines, "/opt/splunk/var/log/splunk/SplunkAppForWazuh.log")
-            result = current_tail.make_tail()
-        except Exception as e:
-            self.error('[log.py][get_last_log_lines] %s' % (e))
-            raise e
-        return result
+    # def get_last_log_lines(self, lines):
+    #     try:
+    #         current_tail = tail(
+    #             lines, "/opt/splunk/var/log/splunk/SplunkAppForWazuh.log")
+    #         result = current_tail.make_tail()
+    #     except Exception as e:
+    #         self.error('[log.py][get_last_log_lines] %s' % (e))
+    #         raise e
+    #     return result
