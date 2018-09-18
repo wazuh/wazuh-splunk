cb8633e66559e2e4f19b1a4a104e278c8601c879
manuasir <manu.asi2@gmail.com>
2018-09-18 17:11:47 +0200

    First wazuh monitoring dashboard

--- SplunkAppForWazuh/appserver/static/js/controllers/management/monitoring/monitoringCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/management/monitoring/monitoringCtrl.js
@@ -1,10 +1,29 @@
-define(['../module'], function (controllers) {
+define(['../../module'], function (controllers) {
 
     'use strict'
   
     controllers.controller('monitoringCtrl', function ($scope, monitoringInfo) {
       const vm = this
       console.log('monitoring info ',monitoringInfo)
+      const nodesCount = data[0].data.data.totalItems
+      vm.nodesCount = nodesCount
+
+      const configuration = data[1]
+      vm.configuration = configuration.data.data
+
+      const version = data[2]
+      vm.version = version.data.data
+
+      const agents = data[3]
+      vm.agentsCount = agents.data.data.totalItems - 1
+
+      const health = data[4]
+      vm.healthCheck = health.data.data
+
+      const nodes = data[0].data.data
+
+      nodes.name = vm.configuration.name
+      nodes.master_node = vm.configuration.node_name
   
     })
   })
