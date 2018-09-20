6cbdc8929bad5ef802834de2e9686e04d0284651
manuasir <manu.asi2@gmail.com>
2018-09-20 13:33:39 +0200

    Added searches and vis

--- SplunkAppForWazuh/appserver/static/js/config/routes.js
+++ SplunkAppForWazuh/appserver/static/js/config/routes.js
@@ -585,7 +585,7 @@ define(['./module'], function (module) {
       .state('aws', {
         templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/aws/aws.html',
         onEnter: ($navigationService) => { $navigationService.storeRoute('aws') },
-        controller: 'awsController',
+        controller: 'awsCtrl',
         controllerAs: 'aws'
       })
 
