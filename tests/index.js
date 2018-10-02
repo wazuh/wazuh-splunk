4897333f2ca7c2b47eb9dcd2a3b0c1790d4f7c41
Adri Valle <adri9valle@gmail.com>
2018-10-02 17:46:55 +0200

    Firsts checks for the manager.api

--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,97 @@
 const chai = require('chai')
 const needle = require('needle')
 
-let api_id = null
-let api_port = null
-let api_url = null
-let api_user = null
-let api_pass = null
+chai.should()
 
-describe('wazuh-api', () => {
-  before(async () => {
-    
-    })
+const s_url = 'http://localhost:8000/en-US/custom/SplunkAppForWazuh/manager'
+
+describe('manager-api', () => {
+  it('GET /check_connection', async () => {
+    try {
+      const res = await needle(`get`, `${s_url}/check_connection?user=foo&pass=bar&ip=http://localhost&port=55000`)
+      const resBodyJson = JSON.parse(res.body)
+      //Check status
+      res.statusCode.should.be.a('number') 
+      res.statusCode.should.equal(200)
+      //Check body
+      resBodyJson.should.be.a('object')    
+      resBodyJson.error.should.be.a('number')
+      resBodyJson.error.should.equal(0)
+      resBodyJson.data.should.be.a('string')
+    }catch(error){
+      console.log('eeee ',error)
+      const resErrMsg = JSON.parse(error.message)
+      console.log("error", resErrMsg)
+      //TODO check the status code that the error returns
+      //resErrMsg.should.be.a('object')
+      //error.status.should.equal('400')
+    }
   })
+  
+  it('GET /get_apis', async () => {
+    try {
+      const res = await needle(`get`, `${s_url}/get_apis`)
+      const resBodyJson = JSON.parse(res.body)
+      //Check status
+      res.statusCode.should.be.a('number')
+      res.statusCode.should.equal(200)
+      resBodyJson.should.be.a('array')
+      //Check body
+      resBodyJson.forEach(api => {
+        api.filterName.should.be.a('string')
+        api.passapi.should.be.a('string')
+        api.portapi.should.be.a('string')
+        api.filterType.should.be.a('string')
+        api.id.should.be.a('string')
+        api.url.should.be.a('string')
+        api.userapi.should.be.a('string')
+      })   
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  })
+  
+  it('GET /get_api with correct id', async () => {
+    try {
+      const res = await needle(`get`, `${s_url}/get_api?id=d0b6a9ce-341c-4758-9590-4caeb44d7163`)
+      const resBodyJson = JSON.parse(res.body)
+      //Check status
+      res.statusCode.should.be.a('number')
+      res.statusCode.should.equal(200)
+      //Check body
+      resBodyJson.should.be.a('array')
+      chai.expect(resBodyJson).to.have.lengthOf(1);
+      resBodyJson[0].managerName.should.be.a('string')
+      resBodyJson[0].filterType.should.be.a('string')
+      resBodyJson[0].portapi.should.be.a('string')
+      resBodyJson[0].passapi.should.be.a('string')
+      resBodyJson[0].userapi.should.be.a('string')
+      resBodyJson[0].filterName.should.be.a('string')
+      resBodyJson[0].id.should.be.a('string')
+      resBodyJson[0].url.should.be.a('string')        
+    } catch (error) { 
+      error = error.actual
+      return Promise.reject(error)
+    }
+  })
+  
+  it('GET /get_api with unexisted id', async () => {
+    try {
+      const res = await needle(`get`, `${s_url}/get_api?id=id_not_valid`)
+      const resBodyJson = JSON.parse(res.body)
+      //Check status
+      res.statusCode.should.be.a('number')
+      res.statusCode.should.equal(200)
+      //Check body
+      resBodyJson.should.be.a('array')
+      chai.expect(resBodyJson).to.have.lengthOf(0);      
+    } catch (error) { 
+      error = error.actual
+      return Promise.reject(error)
+    }
+  })
+
+  //TODO check /get_api without id param
+  
+})
+
