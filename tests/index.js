921ae4469333eb6bd15412a9c76ae9c95a336836
Adri Valle <adri9valle@gmail.com>
2018-10-03 11:37:03 +0200

    Added chai-match to match regex for the api id

--- tests/index.js
+++ tests/index.js
@@ -2,13 +2,28 @@ const chai = require('chai')
 const needle = require('needle')
 
 chai.should()
+chai.use(require('chai-match'));
 
 const s_url = 'http://localhost:8000/en-US/custom/SplunkAppForWazuh/manager'
 
 describe('manager-api', () => {
-  it('GET /check_connection', async () => {
+  it('GET /check_connection => "SUCCESFULLY"', async () => {
+    const res = await needle(`get`, `${s_url}/check_connection?user=foo&pass=bar&ip=http://localhost&port=55000`)
+    const resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number') 
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('object')    
+    resBodyJson.error.should.be.a('number')
+    resBodyJson.error.should.equal(0)
+    resBodyJson.data.should.be.a('string')
+  })
+  
+  /*
+  it('GET /check_connection error', async () => {
     try {
-      const res = await needle(`get`, `${s_url}/check_connection?user=foo&pass=bar&ip=http://localhost&port=55000`)
+      const res = await needle(`get`, `${s_url}/check_connection?user=foot&pass=bart&ip=http://localhost&port=44111`)
       const resBodyJson = JSON.parse(res.body)
       //Check status
       res.statusCode.should.be.a('number') 
@@ -19,97 +34,116 @@ describe('manager-api', () => {
       resBodyJson.error.should.equal(0)
       resBodyJson.data.should.be.a('string')
     }catch(error){
-      console.log('eeee ',error)
-      const resErrMsg = JSON.parse(error.message)
-      console.log("error", resErrMsg)
-      //TODO check the status code that the error returns
-      //resErrMsg.should.be.a('object')
-      //error.status.should.equal('400')
-    }
+      return Promise.reject(error)
+    }//TODO get error 400 in check connection and process it
+  })
+  */
+  it('GET /get_apis => "SUCCESFULLY"', async () => {
+    const res = await needle(`get`, `${s_url}/get_apis`)
+    const resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('array')
+    resBodyJson.forEach(api => {
+      api.passapi.should.be.a('string')
+      api.portapi.should.be.a('string')
+      api.id.should.be.a('string')
+      chai.expect(api.id).to.match(/^\w+\-\w+\-\w+\-\w+\-\w+$/)
+      api.url.should.be.a('string')
+      api.userapi.should.be.a('string')
+    })   
   })
   
-  it('GET /get_apis', async () => {
-    try {
-      const res = await needle(`get`, `${s_url}/get_apis`)
-      const resBodyJson = JSON.parse(res.body)
-      //Check status
-      res.statusCode.should.be.a('number')
-      res.statusCode.should.equal(200)
-      resBodyJson.should.be.a('array')
-      //Check body
-      resBodyJson.forEach(api => {
-        api.filterName.should.be.a('string')
-        api.passapi.should.be.a('string')
-        api.portapi.should.be.a('string')
-        api.filterType.should.be.a('string')
-        api.id.should.be.a('string')
-        api.url.should.be.a('string')
-        api.userapi.should.be.a('string')
-      })   
-    } catch (error) {
-      return Promise.reject(error)
-    }
+  it('GET /get_api => "CORRECT ID"', async () => {
+    const res = await needle(`get`, `${s_url}/get_api?id=94af3c9c-9c72-4a10-a5c7-574dab714874`)
+    const resBodyJson = JSON.parse(res.body) 
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('array')
+    chai.expect(resBodyJson).to.have.lengthOf(1);
+    resBodyJson[0].portapi.should.be.a('string')
+    resBodyJson[0].passapi.should.be.a('string')
+    resBodyJson[0].userapi.should.be.a('string')
+    resBodyJson[0].id.should.be.a('string')
+    chai.expect(resBodyJson[0].id).to.match(/^\w+\-\w+\-\w+\-\w+\-\w+$/)
+    resBodyJson[0].url.should.be.a('string')        
   })
   
-  it('GET /get_api with correct id', async () => {
-    try {
-      const res = await needle(`get`, `${s_url}/get_api?id=d0b6a9ce-341c-4758-9590-4caeb44d7163`)
-      const resBodyJson = JSON.parse(res.body)
-      //Check status
-      res.statusCode.should.be.a('number')
-      res.statusCode.should.equal(200)
-      //Check body
-      resBodyJson.should.be.a('array')
-      chai.expect(resBodyJson).to.have.lengthOf(1);
-      resBodyJson[0].managerName.should.be.a('string')
-      resBodyJson[0].filterType.should.be.a('string')
-      resBodyJson[0].portapi.should.be.a('string')
-      resBodyJson[0].passapi.should.be.a('string')
-      resBodyJson[0].userapi.should.be.a('string')
-      resBodyJson[0].filterName.should.be.a('string')
-      resBodyJson[0].id.should.be.a('string')
-      resBodyJson[0].url.should.be.a('string')        
-    } catch (error) { 
-      error = error.actual
-      return Promise.reject(error)
-    }
+  it('GET /get_api => "WRONG ID"', async () => {
+    const res = await needle(`get`, `${s_url}/get_api?id=id_not_valid`)
+    const resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('array')
+    chai.expect(resBodyJson).to.have.lengthOf(0);      
   })
   
-  it('GET /get_api with unexisted id', async () => {
-    try {
-      const res = await needle(`get`, `${s_url}/get_api?id=id_not_valid`)
-      const resBodyJson = JSON.parse(res.body)
-      //Check status
-      res.statusCode.should.be.a('number')
-      res.statusCode.should.equal(200)
-      //Check body
-      resBodyJson.should.be.a('array')
-      chai.expect(resBodyJson).to.have.lengthOf(0);      
-    } catch (error) { 
-      error = error.actual
-      return Promise.reject(error)
-    }
+  it('GET /get_api => "WITHOUT ID"', async () => {
+    const res = await needle(`get`, `${s_url}/get_api`)
+    const resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    resBodyJson.should.be.a('object')
+    resBodyJson.error.should.be.a('string')
+    resBodyJson.error.should.equal('Missing ID.')
   })
 
-  it('GET /get_api without id param', async () => {
-    try {
-      const res = await needle(`get`, `${s_url}/get_api`)
-      const resBodyJson = JSON.parse(res.body)
-      //Check status
-      res.statusCode.should.be.a('number')
-      res.statusCode.should.equal(200)
-      //Force the error using lengthOf(1) because don´t recieve any api
-      chai.expect(resBodyJson).to.have.lengthOf(1); 
-    } catch (error) { 
-      error = error.actual
-      error.error.should.be.a('string')
-      error.error.should.equal('Missing ID.')
+  it ('POST /add_api => "SUCCESFULLY"', async () => {
+    const payload = {
+      'payload[url]': 'http://127.0.0.1',
+      'payload[portapi]': '55000',
+      'payload[userapi]': 'foo',
+      'payload[passapi]': 'bar'
     }
+    const res = await needle(`post`, `${s_url}/add_api`, payload, {})
+    resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('object')
+    resBodyJson.result.should.be.a('string')
+    chai.expect(resBodyJson.result).to.match(/^\w+\-\w+\-\w+\-\w+\-\w+$/)
   })
   
+  it ('POST /add_api => "INVALID NUMBER OF ARGUMENTS"', async () => {
+    const payload = {
+      'payload[url]': 'http://127.0.0.1',
+      'payload[portapi]': '55000'
+    }
+    const res = await needle(`post`, `${s_url}/add_api`, payload, {})
+    resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('object')
+    resBodyJson.error.should.be.a('string')
+    resBodyJson.error.should.equal('Invalid number of arguments')
+  })
+
+  /*it ('POST /add_api => "ERROR"', async () => {
+    const payload = {
+      'payload[url]': 'http://error.com',
+      'payload[portapi]': '55010',
+      'payload[userapi]': 'testing',
+      'payload[passapi]': 'a_error'
+    }
+    const res = await needle(`post`, `${s_url}/add_api`, payload, {})
+    resBodyJson = JSON.parse(res.body)
+    //Check status
+    res.statusCode.should.be.a('number')
+    res.statusCode.should.equal(200)
+    //Check body
+    resBodyJson.should.be.a('object')
+    resBodyJson.error.should.be.a('string')
+  })*/
+
 })
-
-
-
-
-//- Test unitarios de splunk con mocha y chai
