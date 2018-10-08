cfac35f90c8d169da6097f5e606afe9b91c1bf79
manuasir <manu.asi2@gmail.com>
2018-10-08 16:12:04 +0200

    Added new viz to vulnerabilities

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/overviewVulnerabilitiesCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/overviewVulnerabilitiesCtrl.js
@@ -558,37 +558,13 @@ define([
       }, { tokens: true, tokenNamespace: "submitted" }).render()
 
       commonCves = new ChartElement({
-        "id": "commonCves" + epoch,
-        "charting.axisY2.scale": "inherit",
-        "trellis.size": "medium",
-        "charting.chart.stackMode": "default",
-        "resizable": true,
-        "charting.layout.splitSeries.allowIndependentYRanges": "0",
+        "id": `commonCves${epoch}`,
         "charting.drilldown": "none",
-        "charting.chart.nullValueMode": "gaps",
-        "charting.axisTitleY2.visibility": "visible",
-        "charting.chart": "pie",
-        "trellis.scales.shared": "1",
-        "charting.layout.splitSeries": "0",
-        "charting.chart.style": "shiny",
-        "charting.legend.labelStyle.overflowMode": "ellipsisMiddle",
-        "charting.axisTitleX.visibility": "visible",
-        "charting.axisTitleY.visibility": "visible",
-        "charting.axisX.scale": "linear",
-        "charting.chart.bubbleMinimumSize": "10",
-        "charting.axisLabelsX.majorLabelStyle.overflowMode": "ellipsisNone",
-        "charting.axisY2.enabled": "0",
-        "trellis.enabled": "0",
-        "charting.legend.placement": "right",
-        "charting.chart.bubbleSizeBy": "area",
-        "charting.chart.bubbleMaximumSize": "50",
-        "charting.axisLabelsX.majorLabelStyle.rotation": "0",
-        "charting.axisY.scale": "linear",
-        "charting.chart.showDataLabels": "none",
-        "charting.chart.sliceCollapsingThreshold": "0.01",
-        "managerid": "commonCvesSearch" + epoch,
+        "charting.chart": "bar",
+        "resizable": true,
+        "managerid": `commonCvesSearch${epoch}`,
         "el": $('#commonCves')
-      }, { tokens: true, tokenNamespace: "submitted" }).render()
+    }, {tokens: true, tokenNamespace: "submitted"}).render();
       
       
       element4 = new TableElement({
