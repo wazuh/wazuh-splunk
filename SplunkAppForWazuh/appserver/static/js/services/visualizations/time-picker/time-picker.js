532a4d628d007da0fb58ff3925adc77384fb3169
manuasir <manu.asi2@gmail.com>
2018-10-17 19:09:41 +0200

    New time picker module as singleton

---
+++ SplunkAppForWazuh/appserver/static/js/services/visualizations/time-picker/time-picker.js
@@ -0,0 +1,46 @@
+define([
+  "splunkjs/mvc",
+  "splunkjs/mvc/simpleform/input/timerange"
+  
+], function (
+  mvc,
+  TimeRangeInput
+  
+  ) {
+    'use strict'
+    
+    return class TimePicker {
+      
+      /**
+      * Generates a new visualization
+      * @param {Object} element 
+      * @param {String} id 
+      */
+      constructor(element) {
+        this.input = new TimeRangeInput({
+          "id": `timePicker`,
+          "default": { "latest_time": "now", "earliest_time": "-24h@h" },
+          "searchWhenChanged": true,
+          "earliest_time": "$form.when.earliest$",
+          "latest_time": "$form.when.latest$",
+          "el": $(`${element}`)
+        }, { tokens: true }).render()
+        
+      }
+      
+      /**
+       * Gets the TimeRangeInput
+       */
+      get() {
+        return this.input
+      }
+
+      /**
+       * Destroys the TimeRangeInput instance
+       */
+      destroy(){
+        mvc.Components.revokeInstance(`timePicker`)
+      }
+    }
+  })
+  
