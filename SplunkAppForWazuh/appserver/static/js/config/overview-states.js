f75951fffbab20b82f724ba0d1a74bbd06b839aa
manuasir <manu.asi2@gmail.com>
2018-10-22 16:27:59 +0200

    Refactored angular ui.router states in separated files

---
+++ SplunkAppForWazuh/appserver/static/js/config/overview-states.js
@@ -0,0 +1,124 @@
+define(['./module'], function (module) {
+  'use strict'
+  module.paths = {
+    root: `${window.location.href.split(/\/[a-z][a-z]-[A-Z][A-Z]\//)[0]}/`,
+  }
+  module.constant('BASE_URL', module.paths.root)
+  module.config(['$mdIconProvider', '$locationProvider', '$stateProvider', '$mdThemingProvider', 'BASE_URL', function ($mdIconProvider, $locationProvider, $stateProvider, $mdThemingProvider, BASE_URL) {
+    $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('blue')
+    $locationProvider.html5Mode({
+      'enabled': true,
+      'requirebase': false,
+      'rewriteLinks': false
+    })
+    $stateProvider
+    .state('overview', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-welcome.html',
+      // onEnter: ($navigationService) => { $navigationService.storeRoute('overview') },
+      controller: 'overviewWelcomeCtrl',
+      controllerAs: 'owc',
+      resolve: {
+        agentsInfo: ['$requestService', ($requestService) => {
+          return $requestService.apiReq('/agents/summary')
+          .then(function (response) {
+            return response
+          }, function (response) {
+            return response
+          })
+          .catch(err => {
+            console.error('Error route: ', err)
+          })
+        }]
+      }
+    })
+    // Overview - General
+    .state('ow-general', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-general.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-general') },
+      controller: 'overviewGeneralCtrl',
+      controllerAs: 'ogc',
+      resolve: {
+        pollingState: ['$requestService', ($requestService) => {
+          return $requestService.httpReq(`GET`, `/manager/polling_state`)
+          .then(function (response) {
+            return response
+          }, function (response) {
+            return response
+          })
+          .catch(err => {
+            console.error('Error route: ', err)
+          })
+        }]
+      }
+    })
+    // Overview - policy monitoring
+    .state('ow-pm', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-pm.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-pm') },
+      controller: 'overviewPolicyMonitoringCtrl',
+      controllerAs: 'opm',
+    })
+    // Overview - FIM
+    .state('ow-fim', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-fim.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-fim') },
+      controller: 'overviewFimCtrl',
+      controllerAs: 'ofc',
+    })
+    // Overview - FIM
+    .state('ow-osquery', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/osquery/osquery.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-osquery') },
+      controller: 'osqueryCtrl',
+      controllerAs: 'oqc',
+      resolve: {
+        osquery: ['$requestService', '$stateParams', ($requestService, $stateParams) => {
+          return $requestService.apiReq(`/agents/000/config/wmodules/wmodules`)
+          .then(function (response) {
+            return response
+          }, function (response) {
+            return response
+          })
+          .catch(err => {
+            console.error('Error route: ', err)
+          })
+        }]
+      }
+    })
+    // Overview - audit
+    .state('ow-audit', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-audit.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-audit') },
+      controller: 'overviewAuditCtrl',
+      controllerAs: 'oac',
+    })
+    // Overview - OpenSCAP
+    .state('ow-os', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-openscap.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-os') },
+      controller: 'overviewOpenScapCtrl',
+      controllerAs: 'oos',
+    })
+    // Overview - PCI-DSS
+    .state('ow-pci', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-pci.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-pci') },
+      controller: 'overviewPciCtrl',
+      controllerAs: 'opd',
+    })
+    // Overview - GDPR
+    .state('ow-gdpr', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-gdpr.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-gdpr') },
+      controller: 'overviewGdprCtrl',
+      controllerAs: 'ogdpr',
+    })
+    // Overview - Vulnerabilities
+    .state('ow-vul', {
+      templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/overview/overview-vulnerabilities.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('ow-vul') },
+      controller: 'overviewVulnerabilitiesCtrl',
+      controllerAs: 'ovu',
+    })
+  }])
+})
---
+++ SplunkAppForWazuh/appserver/static/js/config/settings-states.js
@@ -0,0 +1,52 @@
+define(['./module'], function (module) {
+  'use strict'
+  
+  module.config([ '$stateProvider','BASE_URL', function ($stateProvider,BASE_URL) {
+    $stateProvider
+
+    // settings
+    .state('settings', { abstract: true, templateUrl: BASE_URL + 'static/app/SplunkAppForWazuh/views/settings/settings.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.api') } })
+    // settings -> about
+    .state('settings.about', { templateUrl: BASE_URL + '/static/app/SplunkAppForWazuh/views/settings/about.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.about') } })
+    // settings -> api
+    .state('settings.api', {
+      templateUrl: BASE_URL + '/static/app/SplunkAppForWazuh/views/settings/api.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('settings.api') },
+      controller: 'settingsApiCtrl',
+      controllerAs: 'sac',
+      resolve: {
+        apiList: ['$currentDataService', ($currentDataService) => {
+          return $currentDataService.getApiList()
+          .then(function (response) {
+            return response
+          }, function (response) {
+            return response
+          })
+          .catch(err => {
+            console.error('Error route: ', err)
+          })
+        }]
+      }
+    })
+    .state('settings.index', { templateUrl: BASE_URL + '/static/app/SplunkAppForWazuh/views/settings/index.html', onEnter: ($navigationService) => { $navigationService.storeRoute('settings.index') } })
+    .state('settings.logs', {
+      templateUrl: '/static/app/SplunkAppForWazuh/views/settings/logs/logs.html',
+      onEnter: ($navigationService) => { $navigationService.storeRoute('settings.logs') },
+      controller: 'logsCtrl',
+      controllerAs: 'slc',
+      resolve: {
+        logs: ['$requestService', ($requestService) => {
+          return $requestService.httpReq(`GET`,`/manager/get_log_lines`)
+          .then(function (response) {
+            return response
+          }, function (response) {
+            return response
+          })
+          .catch(err => {
+            console.error('Error route: ', err)
+          })
+        }]
+      }
+    })
+  }])
+})
