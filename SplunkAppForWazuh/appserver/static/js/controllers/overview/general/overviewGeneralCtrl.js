9c69513bf4c55698585223359fc351ec8b1399d5
Adri Valle <adri9valle@gmail.com>
2018-10-25 11:43:13 +0200

    Improved overviewGeneral controller

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/general/overviewGeneralCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/general/overviewGeneralCtrl.js
@@ -67,38 +67,29 @@ define([
         agentHistory = new AreaChart(`agentStatusHistory`,`index=wazuh-monitoring-3x status=* | timechart span=1h count by status usenull=f`,`agentStatus`)
       }
       
+      const vizz = [
       /**
       * Metrics
       */
-      const totalAlertsSearch = new SearchHandler(`totalAlerts`,`${filters} | stats count`,`totalAlertsToken`,'$result.count$','totalAlerts',submittedTokenModel,$scope)
-      const level12Search = new SearchHandler(`searchLevel12`,`${filters} sourcetype=wazuh "rule.level">=12 | chart count`,`level12token`,'$result.count$','levelTwelve',submittedTokenModel,$scope)
-      const authFailure = new SearchHandler(`searchAuthFailure`,`${filters} sourcetype=wazuh "rule.groups"="authentication_fail*" | stats count`,`authFailureToken`,'$result.count$','authFailure',submittedTokenModel,$scope)
-      const authSuccess = new SearchHandler(`searchAuthSuccess`,`${filters} sourcetype=wazuh  "rule.groups"="authentication_success" | stats count`,`authSuccessToken`,'$result.count$','authSuccess',submittedTokenModel,$scope)
-      
+      new SearchHandler(`totalAlerts`,`${filters} | stats count`,`totalAlertsToken`,'$result.count$','totalAlerts',submittedTokenModel,$scope),
+      new SearchHandler(`searchLevel12`,`${filters} sourcetype=wazuh "rule.level">=12 | chart count`,`level12token`,'$result.count$','levelTwelve',submittedTokenModel,$scope),
+      new SearchHandler(`searchAuthFailure`,`${filters} sourcetype=wazuh "rule.groups"="authentication_fail*" | stats count`,`authFailureToken`,'$result.count$','authFailure',submittedTokenModel,$scope),
+      new SearchHandler(`searchAuthSuccess`,`${filters} sourcetype=wazuh  "rule.groups"="authentication_success" | stats count`,`authSuccessToken`,'$result.count$','authSuccess',submittedTokenModel,$scope),
       /**
       * Visualizations
       */
-      const totalAlerts = new LinearChart('overviewElement5',`${filters} sourcetype=wazuh rule.level=*| timechart count by rule.level`,'overviewElement5')
-      const totalAlertsColumn = new ColumnChart('overviewElement',`${filters} sourcetype=wazuh | timechart span=2h count`,'overviewElement6')
-      const topAgentName = new PieChart('overviewElement7',`${filters} sourcetype=wazuh | top agent.name`,'overviewElement7')
-      const timechartAgentName = new AreaChart('overviewElement8',`${filters} sourcetype=wazuh | timechart span=1h limit=5 useother=f count by agent.name`,'overviewElement8')
-      const tableSummary = new Table('overviewElement14',`${filters} sourcetype=wazuh |stats count sparkline by rule.id, rule.description, rule.level | sort rule.level DESC | rename rule.id as "Rule ID", rule.description as "Description", rule.level as Level, count as Count`,'overviewElement14')
-      
+      new LinearChart('overviewElement5',`${filters} sourcetype=wazuh rule.level=*| timechart count by rule.level`,'overviewElement5'),
+      new ColumnChart('overviewElement',`${filters} sourcetype=wazuh | timechart span=2h count`,'overviewElement6'),
+      new PieChart('overviewElement7',`${filters} sourcetype=wazuh | top agent.name`,'overviewElement7'),
+      new AreaChart('overviewElement8',`${filters} sourcetype=wazuh | timechart span=1h limit=5 useother=f count by agent.name`,'overviewElement8'),
+      new Table('overviewElement14',`${filters} sourcetype=wazuh |stats count sparkline by rule.id, rule.description, rule.level | sort rule.level DESC | rename rule.id as "Rule ID", rule.description as "Description", rule.level as Level, count as Count`,'overviewElement14')
+      ]
       /**
       * On controller destroy
       */
       $scope.$on('$destroy', () => {
         timePicker.destroy()
-        totalAlerts.destroy()
-        totalAlertsColumn.destroy()
-        topAgentName.destroy()
-        timePicker.destroy()
-        tableSummary.destroy()
-        timechartAgentName.destroy()  
-        totalAlertsSearch.destroy()
-        level12Search.destroy()
-        authFailure.destroy()
-        authSuccess.destroy()
+        vizz.map( (vizz) => vizz.destroy())
         if (agentHistory) {
           agentHistory.destroy()
         }
