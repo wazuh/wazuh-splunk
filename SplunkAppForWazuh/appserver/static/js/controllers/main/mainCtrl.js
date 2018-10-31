0d8d10f470640aa96ce7f7238eae8698ea25bfb1
manuasir <manu.asi2@gmail.com>
2018-10-31 09:52:06 +0100

    Initializing tokens in father controller, also refactoring it

--- SplunkAppForWazuh/appserver/static/js/controllers/main/mainCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/main/mainCtrl.js
@@ -1,46 +1,58 @@
 define([
   '../module',
   'splunkjs/mvc/layoutview',
-  "splunkjs/mvc/simplexml",
+  'splunkjs/mvc/simplexml',
+  '../../services/visualizations/inputs/time-picker'
 ], function (
-  module,
+  app,
   LayoutView,
   DashboardController,
+  TimePicker
   ) {
     'use strict'
-    module.controller('mainCtrl', function ($scope, $urlTokenModel) {
-      
-      $scope.$on('loading', (event, data) => {
-        if (data.status) $scope.loading = true
-        else $scope.loading = false
-        if (!$scope.$$phase) $scope.$digest()
-      })
-      
-      DashboardController.onReady( () => {
-        if (!$urlTokenModel.has('earliest') && !$urlTokenModel.has('latest')) {
-          $urlTokenModel.set({ earliest: '0', latest: '' })
-        }
-      })
-      
-      // Initialize time tokens to default
-      if (!$urlTokenModel.has('earliest') && !$urlTokenModel.has('latest')) {
-        $urlTokenModel.set({ earliest: '0', latest: '' })
+    
+    class MainCtrl{
+      constructor($scope, $urlTokenModel){
+        this.timePicker = new TimePicker('#timePicker',$urlTokenModel.handleValueChange)
+        this.scope = $scope
+        this.dashboardController = DashboardController
+        this.urlTokenModel = $urlTokenModel
+        this.layoutView = new LayoutView({ 'hideFooter': false, 'hideSplunkBar': false, 'hideAppBar': true, 'hideChrome': false })
+        .render()
+        .getContainerElement()
+        .appendChild($('.empty-body-class')[0])
+        
+        this.dashboardController.ready()
+        
       }
       
-      DashboardController.ready()
-      
-      new LayoutView({ "hideFooter": false, "hideSplunkBar": false, "hideAppBar": true, "hideChrome": false })
-      .render()
-      .getContainerElement()
-      .appendChild($('.empty-body-class')[0])
-      
-      /**
-      * On controller destroy
-      */
-      $scope.$on('$destroy', () => {
-        input1.destroy()
-      })
-    })
+      $onInit(){
+        this.scope.$on('loading', (event, data) => {
+          if (data.status) this.scope.loading = true
+          else this.scope.loading = false
+          if (!this.scope.$$phase) this.scope.$digest()
+        })
+        
+        this.dashboardController.onReady( () => {
+          if (!this.urlTokenModel.has('earliest') && !this.urlTokenModel.has('latest')) {
+            this.urlTokenModel.set({ earliest: '0', latest: '' })
+          }
+        })
+        
+        // Initialize time tokens to default
+        if (!this.urlTokenModel.has('earliest') && !this.urlTokenModel.has('latest')) {
+          this.urlTokenModel.set({ earliest: '0', latest: '' })
+        }
+        
+        /**
+        * On controller destroy
+        */
+        this.scope.$on('$destroy', () => {
+          this.timePicker.destroy()
+        })
+      }
+    }
+    app.controller('mainCtrl', MainCtrl)
   })
   
   
