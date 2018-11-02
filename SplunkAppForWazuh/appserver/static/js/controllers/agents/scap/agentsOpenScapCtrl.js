ffa5ae3d42190a8f9afa82805a0a84f42fe5a6d3
Adri Valle <adri9valle@gmail.com>
2018-11-02 16:25:33 +0100

    agentsGdpr agentsOpenScap to classes

--- SplunkAppForWazuh/appserver/static/js/controllers/agents/scap/agentsOpenScapCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/scap/agentsOpenScapCtrl.js
@@ -1,3 +1,15 @@
+/*
+* Wazuh app - Agents controller
+* Copyright (C) 2018 Wazuh, Inc.
+*
+* This program is free software you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation either version 2 of the License, or
+* (at your option) any later version.
+*
+* Find more information about this on the LICENSE file.
+*/
+
 define([
   '../../module',
   '../../../services/visualizations/chart/pie-chart',
@@ -20,89 +32,121 @@ define([
     
     'use strict'
     
-    app.controller('agentsOpenScapCtrl', function ($urlTokenModel, $scope, $currentDataService, agent) {
-      if (!$currentDataService.getCurrentAgent()) { $state.go('overview') }
-      let filters = $currentDataService.getSerializedFilters()
-      const timePicker = new TimePicker('#timePicker',$urlTokenModel.handleValueChange)
-
-      $scope.agent = agent.data.data
+    class AgentsOpenScap {
       
-      const dropdown = new Dropdown(
-        'dropDownInput',
-        `${filters} sourcetype=wazuh  rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=* | stats count by oscap.scan.profile.title | sort oscap.scan.profile.title ASC|fields - count`,
-        'oscap.scan.profile.title',
-        '$form.profile$',
-        'dropDownInput'
-        )
-        const dropdownInstance = dropdown.getElement()
-        const submittedTokenModel = $urlTokenModel.getSubmittedTokenModel()
-        dropdownInstance.on("change", function(newValue){
-          if (newValue && dropdownInstance)
-          $urlTokenModel.handleValueChange(dropdownInstance)
-        })  
-
-        const vizz = [
-          /**
-          * Metrics
-          */
-          new SearchHandler(`lastScapScore`,
-          `${filters} sourcetype=wazuh oscap.scan.score=* | stats latest(oscap.scan.score)`,
-          `latestScapScore`,
-          '$result.latest(oscap.scan.score)$',
-          'scapLastScore',
-          submittedTokenModel,
-          $scope),
-          new SearchHandler(`maxScapScore`,
-          `${filters} sourcetype=wazuh oscap.scan.score=* | stats max(oscap.scan.score)`,
-          `maxScapScore`,
-          '$result.max(oscap.scan.score)$',
-          'scapHighestScore',
-          submittedTokenModel,
-          $scope),
-          new SearchHandler(`scapLowest`,
-          `${filters} sourcetype=wazuh oscap.scan.score=* | stats min(oscap.scan.score)`,
-          `minScapScore`,
-          '$result.min(oscap.scan.score)$',
-          'scapLowestScore',
-          submittedTokenModel,
-          $scope),
+      /**
+      * Class constructor
+      * @param {Object} $urlTokenModel 
+      * @param {Object} $scope 
+      * @param {Object} $currentDataService 
+      * @param {Object} $state 
+      * @param {Object} agent
+      */
+      
+      constructor($urlTokenModel, $scope, $currentDataService, $state, agent){
+        
+        this.urlTokenModel = $urlTokenModel 
+        this.scope = $scope 
+        this.currentDataService = $currentDataService 
+        this.state = $state 
+        this.agent = agent
+        
+        if (!this.currentDataService.getCurrentAgent()) { this.state.go('overview') }
+        
+        this.filters = this.currentDataService.getSerializedFilters()
+        this.timePicker = new TimePicker('#timePicker',this.urlTokenModel.handleValueChange)
+        
+        this.scope.agent = agent.data.data
+        
+        this.dropdown = new Dropdown(
+          'dropDownInput',
+          `${this.filters} sourcetype=wazuh  rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=* | stats count by oscap.scan.profile.title | sort oscap.scan.profile.title ASC|fields - count`,
+          'oscap.scan.profile.title',
+          '$form.profile$',
+          'dropDownInput'
+          )
+          console.log('dropdown ', this.dropdown)
+          this.dropdownInstance = this.dropdown.getElement()
+          console.log('dropdownInstance ', this.dropdownInstance)
+          this.submittedTokenModel = this.urlTokenModel.getSubmittedTokenModel()
+          console.log('submittedTokenModel ', this.submittedTokenModel)
+          this.dropdownInstance.on("change", function(newValue){
+            console.log('onChange dropdownInstance', this.dropdownInstance)
+            console.log('onChange newValue', newValue)
+            if (newValue && this.dropdownInstance){
+              console.log('CHANGED')
+              console.log('CHANGED dropdownInstance', this.dropdownInstance)
+              this.urlTokenModel.handleValueChange(this.dropdownInstance)
+            }
+          })  
+          
+          this.vizz = [
+            /**
+            * Metrics
+            */
+            new SearchHandler(`lastScapScore`,
+            `${this.filters} sourcetype=wazuh oscap.scan.score=* | stats latest(oscap.scan.score)`,
+            `latestScapScore`,
+            '$result.latest(oscap.scan.score)$',
+            'scapLastScore',
+            this.submittedTokenModel,
+            this.scope),
+            new SearchHandler(`maxScapScore`,
+            `${this.filters} sourcetype=wazuh oscap.scan.score=* | stats max(oscap.scan.score)`,
+            `maxScapScore`,
+            '$result.max(oscap.scan.score)$',
+            'scapHighestScore',
+            this.submittedTokenModel,
+            this.scope),
+            new SearchHandler(`scapLowest`,
+            `${this.filters} sourcetype=wazuh oscap.scan.score=* | stats min(oscap.scan.score)`,
+            `minScapScore`,
+            '$result.min(oscap.scan.score)$',
+            'scapLowestScore',
+            this.submittedTokenModel,
+            this.scope),
+            
+            /**
+            * Visualizations
+            */
+            new PieChart('agentsVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top agent.name`,
+            'agentsVizz'),
+            new PieChart('profilesVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top oscap.scan.profile.title`,
+            'profilesVizz'),
+            new BarChart('contentVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top oscap.scan.content`,
+            'contentVizz'),
+            new PieChart('severityVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top oscap.check.severity`,
+            'severityVizz'),
+            new AreaChart('top5AgentsSHVizz',
+            `${this.filters} sourcetype=wazuh rule.groups=\"oscap\" oscap.scan.profile.title=\"$profile$\" oscap.check.severity=\"high\" | chart count by agent.name`,
+            'top5AgentsSHVizz'),
+            new PieChart('top10AleertsVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups=\"oscap-result\" oscap.scan.profile.title=\"$profile$\" | top oscap.check.title`,
+            'top10AleertsVizz'),
+            new PieChart('top10HRAlertsVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups=\"oscap-result\"  oscap.check.severity=\"high\" oscap.scan.profile.title=\"$profile$\" | top oscap.check.title`,
+            'top10HRAlertsVizz'),
+            new Table('alertsSummaryVizz',
+            `${this.filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" oscap.scan.profile.title=\"$profile$\" | stats count by agent.name, oscap.check.title, oscap.scan.profile.title, oscap.scan.id, oscap.scan.content | sort count DESC | rename agent.name as \"Agent name\", oscap.check.title as Title, oscap.scan.profile.title as Profile, oscap.scan.id as \"Scan ID\", oscap.scan.content as Content`,
+            'alertsSummaryVizz')
+          ]
           
           /**
-          * Visualizations
+          * When controller is destroyed
           */
-          new PieChart('agentsVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top agent.name`,
-          'agentsVizz'),
-          new PieChart('profilesVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top oscap.scan.profile.title`,
-          'profilesVizz'),
-          new BarChart('contentVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top oscap.scan.content`,
-          'contentVizz'),
-          new PieChart('severityVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups!=\"syslog\" oscap.scan.profile.title=\"$profile$\" | top oscap.check.severity`,
-          'severityVizz'),
-          new AreaChart('top5AgentsSHVizz',
-          `${filters} sourcetype=wazuh rule.groups=\"oscap\" oscap.scan.profile.title=\"$profile$\" oscap.check.severity=\"high\" | chart count by agent.name`,
-          'top5AgentsSHVizz'),
-          new PieChart('top10AleertsVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups=\"oscap-result\" oscap.scan.profile.title=\"$profile$\" | top oscap.check.title`,
-          'top10AleertsVizz'),
-          new PieChart('top10HRAlertsVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" rule.groups=\"oscap-result\"  oscap.check.severity=\"high\" oscap.scan.profile.title=\"$profile$\" | top oscap.check.title`,
-          'top10HRAlertsVizz'),
-          new Table('alertsSummaryVizz',
-          `${filters} sourcetype=wazuh oscap.check.result=\"fail\" rule.groups=\"oscap\" oscap.scan.profile.title=\"$profile$\" | stats count by agent.name, oscap.check.title, oscap.scan.profile.title, oscap.scan.id, oscap.scan.content | sort count DESC | rename agent.name as \"Agent name\", oscap.check.title as Title, oscap.scan.profile.title as Profile, oscap.scan.id as \"Scan ID\", oscap.scan.content as Content`,
-          'alertsSummaryVizz')
-        ]
+          this.scope.$on('$destroy', () => {
+            this.timePicker.destroy()
+            this.dropdown.destroy()
+            this.vizz.map( (vizz) => vizz.destroy())
+          })
+          
+        }
         
-        /**
-        * When controller is destroyed
-        */
-        $scope.$on('$destroy', () => {
-          timePicker.destroy()
-          dropdown.destroy()
-          vizz.map( (vizz) => vizz.destroy())
-        })
-      })
+      }
+      
+      app.controller('agentsOpenScapCtrl', AgentsOpenScap) 
     })  
