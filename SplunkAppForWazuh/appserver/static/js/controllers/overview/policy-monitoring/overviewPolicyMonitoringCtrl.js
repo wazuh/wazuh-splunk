d6f9aae19491e00e1993ecd00411c42aa77729fa
manuasir <manu.asi2@gmail.com>
2018-11-05 12:48:36 +0100

    Refactored PM controller

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/policy-monitoring/overviewPolicyMonitoringCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/policy-monitoring/overviewPolicyMonitoringCtrl.js
@@ -13,41 +13,49 @@ define([
   ) {
     
     'use strict'
-    
-    app.controller('overviewPolicyMonitoringCtrl', function ($urlTokenModel, $scope, $currentDataService, $state) {
-      let filters = $currentDataService.getSerializedFilters()
-      const timePicker = new TimePicker('#timePicker',$urlTokenModel.handleValueChange)
-      
-      const launchSearches = () => {
-        filters = $currentDataService.getSerializedFilters()
-        $state.reload();
+    class PM{
+      constructor($urlTokenModel, $scope, $currentDataService, $state){
+        this.scope = $scope
+        this.urlTokenModel = $urlTokenModel
+        this.state = $state
+        this.getFilters = $currentDataService.getSerializedFilters
+        this.filters = this.getFilters()
+        this.timePicker = new TimePicker('#timePicker',this.urlTokenModel.handleValueChange)
+        this.vizz = [
+          /**
+          * Visualizations
+          */
+          new AreaChart('elementOverTime',`${this.filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" rule.description=* | timechart span=1h count by rule.description`,'elementOverTime'),
+          new PieChart('cisRequirements',`${this.filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" rule.cis{}=* | top  rule.cis{}`,'cisRequirements'),
+          new PieChart('topPciDss',`${this.filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" rule.pci_dss{}=* | top  rule.pci_dss{}`,'topPciDss'),
+          new AreaChart('eventsPerAgent',`${this.filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" | timechart span=2h count by agent.name`,'eventsPerAgent'),
+          new Table('alertsSummary',`${this.filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" |stats count sparkline by agent.name, rule.description, title | sort count DESC | rename rule.description as \"Rule description\", agent.name as Agent, title as Control`,'alertsSummary')
+        ]
+        
+        
+        this.scope.$on('deletedFilter', () => {
+          this.launchSearches()
+        })
+        
+        this.scope.$on('barFilter', () => {
+          this.launchSearches()
+        })
+        /**
+        * On controller destroy
+        */
+        this.scope.$on('$destroy', () => {
+          this.timePicker.destroy()
+          this.vizz.map( (vizz) => vizz.destroy())
+        })
+        
       }
       
-      $scope.$on('deletedFilter', () => {
-        launchSearches()
-      })
+      launchSearches() {
+        this.filters = this.getFilters()
+        this.state.reload()
+      }
       
-      $scope.$on('barFilter', () => {
-        launchSearches()
-      })
       
-      const vizz = [
-      /**
-      * Visualizations
-      */
-      new AreaChart('elementOverTime',`${filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" rule.description=* | timechart span=1h count by rule.description`,'elementOverTime'),
-      new PieChart('cisRequirements',`${filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" rule.cis{}=* | top  rule.cis{}`,'cisRequirements'),
-      new PieChart('topPciDss',`${filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" rule.pci_dss{}=* | top  rule.pci_dss{}`,'topPciDss'),
-      new AreaChart('eventsPerAgent',`${filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" | timechart span=2h count by agent.name`,'eventsPerAgent'),
-      new Table('alertsSummary',`${filters} sourcetype=wazuh \"rule.groups\"=\"rootcheck\" |stats count sparkline by agent.name, rule.description, title | sort count DESC | rename rule.description as \"Rule description\", agent.name as Agent, title as Control`,'alertsSummary')
-      ]
-
-      /**
-      * On controller destroy
-      */
-      $scope.$on('$destroy', () => {
-        timePicker.destroy()
-        vizz.map( (vizz) => vizz.destroy())
-      })
-    })
+    }
+    app.controller('overviewPolicyMonitoringCtrl', PM)
   })
