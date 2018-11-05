f7f61e5d4f9061e69e9cd42ddfa2e356c8bed7e0
manuasir <manu.asi2@gmail.com>
2018-11-05 09:51:38 +0100

    Refactored overview PCI-DSS controller

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/pci/overviewPciCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/pci/overviewPciCtrl.js
@@ -16,65 +16,58 @@ define([
     
     'use strict'
     
-    app.controller('overviewPciCtrl', function ($urlTokenModel, $scope, $currentDataService, $state) {
-      let filters = $currentDataService.getSerializedFilters()
-      const timePicker = new TimePicker('#timePicker',$urlTokenModel.handleValueChange)
-
-      const dropdown = new Dropdown(
-        'dropDownInput',
-        `${filters} sourcetype=wazuh rule.pci_dss{}=\"*\"| stats count by \"rule.pci_dss{}\" | sort \"rule.pci_dss{}\" ASC | fields - count`,
-        'rule.pci_dss{}',
-        '$form.pci$',
-        'dropDownInput'
-        )
-        const dropdownInstance = dropdown.getElement()
-        dropdownInstance.on("change", function(newValue){
-          if (newValue && dropdownInstance)
-          $urlTokenModel.handleValueChange(dropdownInstance)
-        })  
+    class PCI{
+      constructor($urlTokenModel, $scope, $currentDataService, $state) {
+        this.scope = $scope
+        this.state = $state
+        this.getFilters = $currentDataService.getSerializedFilters
+        this.filters = this.getFilters()
+        this.submittedTokenModel = $urlTokenModel.getSubmittedTokenModel()
         
-        const launchSearches = () => {
-          filters = $currentDataService.getSerializedFilters()
-          $state.reload();
-        }
-        
-        $scope.$on('deletedFilter', () => {
-          launchSearches()
+        this.$on('deletedFilter', () => {
+          this.launchSearches()
         })
         
-        $scope.$on('barFilter', () => {
-          launchSearches()
+        this.$on('barFilter', () => {
+          this.launchSearches()
         })
         
-        
-        const vizz = [
+        this.scope.$on('$destroy', () => {
+          this.timePicker.destroy()
+          this.vizz.map( (vizz) => vizz.destroy())
+        })
+        this.timePicker = new TimePicker('#timePicker',$urlTokenModel.handleValueChange)
+        this.dropdown = new Dropdown('dropDownInput',`${this.filters} sourcetype=wazuh rule.pci_dss{}=\"*\"| stats count by \"rule.pci_dss{}\" | sort \"rule.pci_dss{}\" ASC | fields - count`,'rule.pci_dss{}','$form.pci$','dropDownInput')
+        this.dropdownInstance = this.dropdown.getElement()
+        this.vizz = [
           new ColumnChart('pciReqVizz',
-          `${filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\"  | stats count by rule.pci_dss{}`,
+          `${this.filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\"  | stats count by rule.pci_dss{}`,
           'pciReqVizz'
           ),
           new PieChart('groupsVizz',
-          `${filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" | stats count by rule.groups`,
+          `${this.filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" | stats count by rule.groups`,
           'groupsVizz'
           ),
           new PieChart('agentsVizz',
-          `${filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" | stats count by agent.name`,
+          `${this.filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" | stats count by agent.name`,
           'agentsVizz'),
           new ColumnChart('requirementsByAgentVizz',
-          `${filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" agent.name=*| chart  count(rule.pci_dss{}) by rule.pci_dss{},agent.name`,
+          `${this.filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" agent.name=*| chart  count(rule.pci_dss{}) by rule.pci_dss{},agent.name`,
           'requirementsByAgentVizz'),
           new Table('alertsSummaryViz',
-          `${filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" | stats count sparkline by agent.name, rule.pci_dss{}, rule.description | sort count DESC | rename agent.name as \"Agent Name\", rule.pci_dss{} as Requirement, rule.description as \"Rule description\", count as Count`,
+          `${this.filters} sourcetype=wazuh rule.pci_dss{}=\"$pci$\" | stats count sparkline by agent.name, rule.pci_dss{}, rule.description | sort count DESC | rename agent.name as \"Agent Name\", rule.pci_dss{} as Requirement, rule.description as \"Rule description\", count as Count`,
           'alertsSummaryViz')
         ]
-        
-        /**
-        * When controller is destroyed
-        */
-        $scope.$on('$destroy', () => {
-          timePicker.destroy()
-          dropdown.destroy()
-          vizz.map( (vizz) => vizz.destroy())
+        this.dropdownInstance.on("change", function(newValue){
+          if (newValue && this.dropdownInstance)
+          $urlTokenModel.handleValueChange(this.dropdownInstance)
         })
-      })
-    })
-    
+      }
+      launchSearches(){
+        this.filters = this.getFilters()
+        this.state.reload()
+      }
+    }
+    app.controller('overviewPciCtrl', PCI)
+  })
+  
