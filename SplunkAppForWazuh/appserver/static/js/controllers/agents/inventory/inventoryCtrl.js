a211aa4f1357a2b1bfe1562fbd5f0db0cb4ba61c
manuasir <manu.asi2@gmail.com>
2018-11-07 13:27:25 +0100

    Finished table

--- SplunkAppForWazuh/appserver/static/js/controllers/agents/inventory/inventoryCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/inventory/inventoryCtrl.js
@@ -1,14 +1,14 @@
 /*
- * Wazuh app - Dev tools controller
- * Copyright (C) 2018 Wazuh, Inc.
- *
- * This program is free software you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation either version 2 of the License, or
- * (at your option) any later version.
- *
- * Find more information about this on the LICENSE file.
- */
+* Wazuh app - Dev tools controller
+* Copyright (C) 2018 Wazuh, Inc.
+*
+* This program is free software you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation either version 2 of the License, or
+* (at your option) any later version.
+*
+* Find more information about this on the LICENSE file.
+*/
 
 define(['../../module'], function (module) {
   'use strict'
@@ -20,20 +20,22 @@ define(['../../module'], function (module) {
       this.root = $rootScope
       this.toast = $notificationService.showSimpleToast
     }
-
+    
     /**
-     * Filters by a term in table
-     * @param {String} term 
-     * @param {String} specificPath 
-     */
-    search(term, specificPath) { this.$scope.$broadcast('wazuhSearch', { term, specificPath }) }
-
+    * Filters by a term in table
+    * @param {String} term 
+    * @param {String} specificPath 
+    */
+    search(term, specificPath) { 
+      this.scope.$broadcast('wazuhSearch', { term, specificPath })
+    }
+    
     /**
-     * Initialize
-     */
+    * Initialize
+    */
     $onInit() {
       try {
-        this.scope.search = this.search
+        this.scope.search = (term, specificPath) => { this.search(term, specificPath) }
         this.scope.agent = this.data[5].data.data
         this.scope.getAgentStatusClass = agentStatus => agentStatus === "Active" ? "teal" : "red";
         this.scope.formatAgentStatus = agentStatus => {
@@ -50,35 +52,35 @@ define(['../../module'], function (module) {
           !this.data[1].data.data ||
           typeof this.data[1].data.data !== 'object' ||
           !Object.keys(this.data[1].data.data).length
-        ) {
-          this.scope.syscollector = null
-        } else {
-          const netiface = {}
-          const ports = {}
-          const packagesDate = {}
-          if (this.data[2] && this.data[2].data && this.data[2].data.data)
+          ) {
+            this.scope.syscollector = null
+          } else {
+            const netiface = {}
+            const ports = {}
+            const packagesDate = {}
+            if (this.data[2] && this.data[2].data && this.data[2].data.data)
             Object.assign(netiface, this.data[2].data.data)
-          if (this.data[3] && this.data[3].data && this.data[3].data.data)
+            if (this.data[3] && this.data[3].data && this.data[3].data.data)
             Object.assign(ports, this.data[3].data.data)
-          if (this.data[4] && this.data[4].data && this.data[4].data.data)
+            if (this.data[4] && this.data[4].data && this.data[4].data.data)
             Object.assign(packagesDate, this.data[4].data.data)
-          this.scope.syscollector = {
-            hardware: this.data[0].data.data,
-            os: this.data[1].data.data,
-            netiface: netiface,
-            ports: ports,
-            packagesDate:
+            this.scope.syscollector = {
+              hardware: this.data[0].data.data,
+              os: this.data[1].data.data,
+              netiface: netiface,
+              ports: ports,
+              packagesDate:
               packagesDate && packagesDate.items && packagesDate.items.length
-                ? packagesDate.items[0].scan_time
-                : 'Unknown'
+              ? packagesDate.items[0].scan_time
+              : 'Unknown'
+            }
           }
+          return
+        } catch (error) {
+          this.toast(error.message || error)
         }
-        return
-      } catch (error) {
-        this.toast(error.message || error)
       }
     }
-  }
-  // Logs controller
-  module.controller('inventoryCtrl', Inventory)
-})
+    // Logs controller
+    module.controller('inventoryCtrl', Inventory)
+  })
