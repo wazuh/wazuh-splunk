2d306868875f7e63803270a0db18b69bd439d348
manuasir <manu.asi2@gmail.com>
2018-11-16 11:57:08 +0100

    Included some tests

---
+++ tests/ofuscate.js
@@ -0,0 +1,40 @@
+const chai = require('chai')
+const needle = require('needle')
+chai.should()
+const url = process.env.ofuscateurl || ''
+const s_url = `${url}/custom/SplunkAppForWazuh/api/ofuscate_test`
+const hide = '********'
+const responses = require('./external_files/jsonOfuscatedResponses.js')
+describe('get_request_to_api', () => {
+
+  it('Checks that cluster config is returned whith an ofuscated key', async () => {
+    const payload = responses.clus_conf
+    const pay = JSON.stringify(payload)
+    const res = await needle('post', s_url, pay, {})
+    const resBodyJson = JSON.parse(res.body)
+    resBodyJson.data.key.should.equal(hide)
+  })
+
+  it('Checks that wmodlues are returned whith an ofuscated key', async () => {
+    const payload = responses.wmodules
+    const pay = JSON.stringify(payload)
+    const res = await needle('post', s_url, pay, {})
+    const resBodyJson = JSON.parse(res.body)
+    resBodyJson.data.wmodules.map(item => {
+      if (item['aws-s3'] && item['aws-s3'].buckets) {
+        item['aws-s3'].buckets.map(item => {
+          item.access_key.should.equal(hide)
+          item.secret_key.should.equal(hide)
+        });
+      }
+    })
+  })
+  it('Checks that integrations are returned whith an ofuscated key', async () => {
+    const payload = responses.integrations
+    const pay = JSON.stringify(payload)
+    const res = await needle('post', s_url, pay, {})
+    const resBodyJson = JSON.parse(res.body)
+    resBodyJson.data.integration.map(item => (item.api_key.should.equal(hide)));
+  })
+
+})
