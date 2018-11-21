a68786cd5957a742bc49b5d2728442d0e0104511
manuasir <manu.asi2@gmail.com>
2018-11-21 17:32:23 +0100

    Debugging new report endpoint

--- SplunkAppForWazuh/appserver/controllers/report.py
+++ SplunkAppForWazuh/appserver/controllers/report.py
@@ -19,11 +19,14 @@ import splunk.appserver.mrsparkle.lib.util as util
 from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
 from splunk.appserver.mrsparkle.lib.decorators import expose_page
 from log import log
+from pyPdf import PdfFileWriter
 
 class report(controllers.BaseController):
     def __init__(self):
         self.logger = log()
         try:
+            #self.path = os.path.dirname(os.path.abspath(__file__))
+            self.path = '/opt/splunk/etc/apps/SplunkAppForWazuh/bin/'
             controllers.BaseController.__init__(self)
         except Exception as e:
             self.logger.error("Error in report module constructor: %s" % (e))
@@ -31,10 +34,28 @@ class report(controllers.BaseController):
     @expose_page(must_login=False, methods=['POST'])
     def generate(self, **kwargs):
         try:
-            self.logger.error("Start generating report")
-            self.logger.error("Arguments received: "+str(kwargs))
+            self.logger.info("Start generating report")
+            #self.logger.info("Arguments received: "+str(kwargs))
+            self.logger.info("Path file: "+str(self.path))
+
+            output = PdfFileWriter()
+            outputStream = file(str(self.path)+"salida.pdf", "wb")
+            output.write(outputStream)
+            outputStream.close()
             parsed_data = json.dumps({'data': 'success'})
         except Exception as e:
             self.logger.error("Error generating report: %s" % (e))
+            return json.dumps({"error":str(e)})
+        return parsed_data
+
+    # Returns a list with all PDF files in the bin directory
+    @expose_page(must_login=False, methods=['GET'])
+    def reports(self, **kwargs):
+        try:
+            self.logger.info("Returning list of PDF files")
+            pdf_files = [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, f))]
+            parsed_data = json.dumps({'data': pdf_files})
+        except Exception as e:
+            self.logger.error("Error getting PDF files: %s" % (e))
             return json.dumps({"error":str(e)})
         return parsed_data
