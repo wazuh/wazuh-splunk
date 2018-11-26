c0aeb29750843c6e1f2a881e804c5e1bcf939ba6
Adri Valle <adri9valle@gmail.com>
2018-11-26 11:25:41 +0100

    Solved secondary states without ng-click(select(...)), fixes #338

--- SplunkAppForWazuh/appserver/static/js/run/run.js
+++ SplunkAppForWazuh/appserver/static/js/run/run.js
@@ -3,51 +3,55 @@ define(['./module'], function (module) {
   module.run([
     '$rootScope', '$state', '$transitions', '$navigationService', '$currentDataService',
     function ($rootScope, $state, $transitions, $navigationService, $currentDataService) {
-    
+
       //Go to last state at login
       $navigationService.goToLastState()
 
-      async function checkBeforeTransition(state){
-        try{
+      async function checkBeforeTransition(state) {
+        try {
           const { api, selectedIndex } = await $currentDataService.checkSelectedApiConnection()
           $currentDataService.setApi(api)
           $currentDataService.cleanFilters()
           $navigationService.storeRoute(state.label)
           $currentDataService.addFilter(`{"${api.filterType}":"${api.filterName}", "implicit":true}`)
           $currentDataService.addFilter(`{"index":"${$currentDataService.getIndex().index}", "implicit":true}`)
-        }catch (err){
+        } catch (err) {
           $rootScope.$broadcast('loading', { status: false })
           if (state.label != 'settings.api')
-          $rootScope.$broadcast('stateChanged', 'settings')
+            $rootScope.$broadcast('stateChanged', 'settings')
           $state.go('settings.api')
         }
       }
 
       $transitions.onBefore({}, async (trans) => {
-        $rootScope.$broadcast('loading: ', { status: true })        
+        $rootScope.$broadcast('loading: ', { status: true })
       })
 
       $transitions.onStart({}, async (trans) => {
         $rootScope.$broadcast('loading', { status: true })
         const to = trans.to().name
         //Primary states
-        if ( to === 'dev-tools' ) { await checkBeforeTransition({label: 'dev-tools', state: 'dev-tools'}) }
-        if ( to === 'settings.api' ) { await checkBeforeTransition({label: 'settings.api', state: 'settings'}) }
-        if ( to === 'agents' ) { await checkBeforeTransition({label: 'agents', state: 'agents'}) }
-        if ( to === 'overview' ) { await checkBeforeTransition({label: 'overview', state: 'overview'}) }
-        if ( to === 'manager' ) { await checkBeforeTransition({label: 'manager', state: 'manager'}) }
-        //Secondary states
-        if (to !== 'agents' && to.includes('agent') || to.includes('ag-') ) {
-          $rootScope.$broadcast('stateChanged', 'agents')
-        } else if ( to.includes('ow-') ) {
-          $rootScope.$broadcast('stateChanged', 'overview') 
-        } else if ( to.includes('mg-') ) {
-          $rootScope.$broadcast('stateChanged', 'manager')
-        }
-      })  
+        if (to === 'dev-tools') { await checkBeforeTransition({ label: 'dev-tools', state: 'dev-tools' }) }
+        if (to === 'settings.api') { await checkBeforeTransition({ label: 'settings.api', state: 'settings' }) }
+        if (to === 'agents') { await checkBeforeTransition({ label: 'agents', state: 'agents' }) }
+        if (to === 'overview') { await checkBeforeTransition({ label: 'overview', state: 'overview' }) }
+        if (to === 'manager') { await checkBeforeTransition({ label: 'manager', state: 'manager' }) }
+      })
 
       $transitions.onSuccess({}, async (trans) => {
         $rootScope.$broadcast('loading', { status: false })
+        const to = trans.to().name
+        $rootScope.$broadcast('stateChanged', trans.to().name)
+        //Secondary states
+        if (to !== 'agents' && to.includes('agent') || to.includes('ag-')) {
+          $rootScope.$broadcast('stateChanged', 'agents')
+        } else if (to.includes('ow-')) {
+          $rootScope.$broadcast('stateChanged', 'overview')
+        } else if (to.includes('mg-')) {
+          $rootScope.$broadcast('stateChanged', 'manager')
+        } else if (to.includes('settings')) {
+          $rootScope.$broadcast('stateChanged', 'settings')
+        }
       })
 
       $transitions.onError({}, async (trans) => {
@@ -59,4 +63,4 @@ define(['./module'], function (module) {
       })
 
     }])
-  })
+})
