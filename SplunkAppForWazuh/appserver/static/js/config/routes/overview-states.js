17e357053851b03a0712572b1e2b66e116956b49
Manuel J. Bernal <manu.asi2@gmail.com>
2018-11-27 20:50:35 +0100

    Handling errors in settings-states

--- SplunkAppForWazuh/appserver/static/js/config/routes/overview-states.js
+++ SplunkAppForWazuh/appserver/static/js/config/routes/overview-states.js
@@ -1,4 +1,4 @@
-define(['../module'], function(module) {
+define(['../module'], function (module) {
   'use strict'
   module.paths = {
     root: `${window.location.href.split(/\/[a-z][a-z]-[A-Z][A-Z]\//)[0]}/`
@@ -10,7 +10,7 @@ define(['../module'], function(module) {
     '$stateProvider',
     '$mdThemingProvider',
     'BASE_URL',
-    function(
+    function (
       $mdIconProvider,
       $locationProvider,
       $stateProvider,
@@ -36,24 +36,16 @@ define(['../module'], function(module) {
             agentsInfo: [
               '$requestService',
               '$state',
-              ($requestService, $state) => {
-                return $requestService
-                  .apiReq('/agents/summary')
-                  .then(
-                    function(response) {
-                      return response
-                    },
-                    function(response) {
-                      return response
-                    }
-                  )
-                  .catch(err => {
-                    $state.go('settings.api')
-                  })
+              async ($requestService, $state) => {
+                try {
+                  const result = await $requestService.apiReq('/agents/summary')
+                  return result
+                } catch (err) {
+                  $state.go('settings.api')
+                }
               }
             ],
             extensions: [
-              '$requestService',
               '$currentDataService',
               async ($currentDataService) => {
                 try {
@@ -61,6 +53,7 @@ define(['../module'], function(module) {
                   const result = $currentDataService.getExtensionsById(id)
                   return result
                 } catch (err) {
+                  console.error('err ', err)
                   return false
                 }
               }
@@ -81,20 +74,13 @@ define(['../module'], function(module) {
             pollingState: [
               '$requestService',
               '$state',
-              ($requestService, $state) => {
-                return $requestService
-                  .httpReq(`GET`, `/manager/polling_state`)
-                  .then(
-                    response => {
-                      return response
-                    },
-                    response => {
-                      return response
-                    }
-                  )
-                  .catch(err => {
-                    $state.go('settings.api')
-                  })
+              async ($requestService, $state) => {
+                try {
+                  const result = await $requestService.httpReq(`GET`, `/manager/polling_state`)
+                  return result
+                } catch (err) {
+                  $state.go('settings.api')
+                }
               }
             ]
           }
@@ -132,20 +118,13 @@ define(['../module'], function(module) {
             osquery: [
               '$requestService',
               '$state',
-              ($requestService, $state) => {
-                return $requestService
-                  .apiReq(`/agents/000/config/wmodules/wmodules`)
-                  .then(
-                    function(response) {
-                      return response
-                    },
-                    function(response) {
-                      return response
-                    }
-                  )
-                  .catch(err => {
-                    $state.go('settings.api')
-                  })
+              async ($requestService, $state) => {
+                try {
+                  const result = await $requestService.apiReq(`/agents/000/config/wmodules/wmodules`)
+                  return result
+                } catch (err) {
+                  $state.go('settings.api')
+                }
               }
             ]
           }
