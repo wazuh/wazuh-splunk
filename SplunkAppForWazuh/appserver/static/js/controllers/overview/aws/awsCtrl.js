5e2ee93200604b21d19fbb35b670fc5900f74ff4
Adri Valle <adri9valle@gmail.com>
2018-12-05 09:11:56 +0100

    Added dropdown to filter by service name

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/aws/awsCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/aws/awsCtrl.js
@@ -5,8 +5,9 @@ define([
   '../../../services/visualizations/chart/column-chart',
   '../../../services/visualizations/table/table',
   '../../../services/visualizations/map/map',
-  '../../../services/visualizations/inputs/time-picker'
-], function(app, PieChart, AreaChart, ColumnChart, Table, Map, TimePicker) {
+  '../../../services/visualizations/inputs/time-picker',
+  '../../../services/visualizations/inputs/dropdown-input'
+], function(app, PieChart, AreaChart, ColumnChart, Table, Map, TimePicker, Dropdown) {
   'use strict'
 
   class AWS {
@@ -22,55 +23,69 @@ define([
         '#timePicker',
         $urlTokenModel.handleValueChange
       )
+
+      this.dropdown = new Dropdown(
+        'dropDownInput',
+        `${this.filters} sourcetype=wazuh data.aws.source=* | stats by data.aws.source | fields data.aws.source | sort data.aws.source ASC`,
+        'data.aws.source',
+        '$form.awsSource$',
+        'dropDownInput'
+      )
+      this.dropdownInstance = this.dropdown.getElement()
+      this.dropdownInstance.on('change', newValue => {
+        if (newValue && this.dropdownInstance)
+          $urlTokenModel.handleValueChange(this.dropdownInstance)
+      })
+
       this.vizz = [
         /**
          * Visualizations
          */
         new AreaChart(
           'eventsByIdOverTime',
-          `${this.filters} sourcetype=wazuh | timechart count by data.aws.resource.instanceDetails.instanceId usenull=f`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | timechart count by data.aws.resource.instanceDetails.instanceId usenull=f`,
           'eventsByIdOverTime'
         ),
         new ColumnChart(
           'eventsByRegionOverTime',
-          `${this.filters} sourcetype=wazuh | timechart count by data.aws.awsRegion usenull=f`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | timechart count by data.aws.awsRegion usenull=f`,
           'eventsByRegionOverTime'
         ),
         new PieChart(
           'topEventsByServiceName',
-          `${this.filters} sourcetype=wazuh | stats count BY data.aws.source`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | stats count BY data.aws.source`,
           'topEventsByServiceName'
         ),
         new PieChart(
           'topEventsByInstanceId',
-          `${this.filters} sourcetype=wazuh | top data.aws.resource.instanceDetails.instanceId limit=5`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | top data.aws.resource.instanceDetails.instanceId limit=5`,
           'topEventsByInstanceId'
         ),
         new PieChart(
           'topEventsByResourceType',
-          `${this.filters} sourcetype=wazuh | top data.aws.resource.resourceType limit=5`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | top data.aws.resource.resourceType limit=5`,
           'topEventsByResourceType'
         ),
         new PieChart(
           'topEventsByRegion',
-          `${this.filters} sourcetype=wazuh | top data.aws.awsRegion limit=5`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | top data.aws.awsRegion limit=5`,
           'topEventsByRegion'
         ),
         new Map(
           'map',
           `${
             this.filters
-          } sourcetype=wazuh | geostats latfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lat" longfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lon" count`,
+          } sourcetype=wazuh data.aws.source=$awsSource$ | geostats latfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lat" longfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lon" count`,
           'map'
         ),
         new Table(
           'top5Buckets',
-          `${this.filters} sourcetype=wazuh | top data.aws.source limit=5`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | top data.aws.source limit=5`,
           'top5Buckets'
         ),
         new Table(
           'top5Rules',
-          `${this.filters} sourcetype=wazuh | top rule.id, rule.description limit=5`,
+          `${this.filters} sourcetype=wazuh data.aws.source=$awsSource$ | top rule.id, rule.description limit=5`,
           'top5Rules'
         )
       ]
@@ -87,6 +102,7 @@ define([
        */
       this.scope.$on('$destroy', () => {
         this.timePicker.destroy()
+        this.dropdown.destroy()
         this.vizz.map(vizz => vizz.destroy())
       })
     }
