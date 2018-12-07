32b3e1217bb50fc581774853af29ec6afe5e79fe
manuasir <manu.asi2@gmail.com>
2018-12-07 16:39:34 +0100

    Implemented table fed by arrays

--- SplunkAppForWazuh/appserver/static/js/directives/index.js
+++ SplunkAppForWazuh/appserver/static/js/directives/index.js
@@ -1,10 +1,12 @@
 define([
   './wz-menu/wz-menu',
   './wz-table/index',
+  './wz-data-table/wz-data-table',
   './wz-enter/wz-enter',
   './wz-dynamic/wz-dynamic',
   './wz-welcome-card/wz-welcome-card',
   './wz-bar/wz-bar',
   './wz-no-config/wz-no-config',
+  './wz-data-table/wz-data-table',
   './wz-config-item/wz-config-item'
 ], function() {})
