423689315540c3cd9e1ed2ad6734627037e93a9e
Adri Valle <adri9valle@gmail.com>
2018-12-14 17:02:09 +0100

    Change if condition

--- SplunkAppForWazuh/appserver/static/js/controllers/agents/overview/agentsOverviewCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/overview/agentsOverviewCtrl.js
@@ -36,7 +36,9 @@ define(['../../module'], function (app) {
       if (this.agent.length && typeof this.agent[0] === 'object' && this.agent[0].data && typeof this.agent[0].data.data === 'object') {
         this.scope.agent = this.agent[0].data.data
         //Check OS type
-        this.scope.isLinux = this.agent[0].data.data.os.uname.includes("Linux") ? true : false
+        if (this.agent[0].data.data) {
+          this.scope.isLinux = this.agent[0].data.data.os.uname.includes("Linux")
+        }
         if (this.scope.agent.status == 'Never connected') {
           this.scope.agent.os = { name: 'Unknown', codename: 'Unknown', version: 'Unknown' }
           this.scope.agent.group = null
