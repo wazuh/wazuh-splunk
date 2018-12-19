31eb125e9e2f8d198f14e31ad53fcd29b01d9061
Adri Valle <adri9valle@gmail.com>
2018-12-19 10:22:19 +0100

    Added reporting to overview FIM

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/fim/overviewFimCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/fim/overviewFimCtrl.js
@@ -15,10 +15,12 @@ define([
      * @param {*} $scope 
      * @param {*} $currentDataService 
      * @param {*} $state 
+     * @param {*} $reportingService 
      */
-    constructor($urlTokenModel, $scope, $currentDataService, $state) {
+    constructor($urlTokenModel, $scope, $currentDataService, $state, $reportingService) {
       this.scope = $scope
       this.state = $state
+      this.reportingService = $reportingService
       this.getFilters = $currentDataService.getSerializedFilters
       this.filters = this.getFilters()
       this.submittedTokenModel = $urlTokenModel.getSubmittedTokenModel()
@@ -103,11 +105,30 @@ define([
       })
 
       /**
+       * Generates report
+       */
+      this.scope.startVis2Png = () =>
+      this.reportingService.startVis2Png('overview-fim', [
+        'deletedFiles',
+        'newFiles',
+        'modifiedFiles',
+        'alertsVolume',
+        'eventsSummary',
+        'topRules',
+        'whodataUsage',
+        'topUsers'
+      ])
+
+      /**
        * On controller destroy
        */
       this.scope.$on('$destroy', () => {
         this.timePicker.destroy()
         this.vizz.map(vizz => vizz.destroy())
+      })
+
+      this.scope.$on('loadingReporting', (event, data) => {
+        this.scope.loadingReporting = data.status
       })
     }
 
