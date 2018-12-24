56f84b8ddbe1ce8bb5ec197eef94e2f7dbe275be
Adri Valle <adri9valle@gmail.com>
2018-12-24 14:39:21 +0100

    Fix typo

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/scap/overviewOpenScapCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/scap/overviewOpenScapCtrl.js
@@ -165,7 +165,7 @@ define([
        * Generates report
        */
       this.scope.startVis2Png = () =>
-      this.reportingService.startVis2Png('overview-fim', 'Open SCAP', this.filters, [
+      this.reportingService.startVis2Png('overview-oscap', 'Open SCAP', this.filters, [
         'agentsVizz',
         'profilesVizz',
         'contentVizz',
