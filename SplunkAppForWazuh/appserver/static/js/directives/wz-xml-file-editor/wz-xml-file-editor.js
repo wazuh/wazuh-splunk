1100095f4a8456963552f6294c2eef52dfd39755
manuasir <manu.asi2@gmail.com>
2019-01-09 15:07:52 +0100

    Added missing toast and deleted semicolons

--- SplunkAppForWazuh/appserver/static/js/directives/wz-xml-file-editor/wz-xml-file-editor.js
+++ SplunkAppForWazuh/appserver/static/js/directives/wz-xml-file-editor/wz-xml-file-editor.js
@@ -44,37 +44,39 @@ define([
         ) {
           const checkXmlParseError = () => {
             try {
-              const parser = new DOMParser(); // eslint-disable-line
-              const xml = $scope.xmlCodeBox.getValue();
-              const xmlDoc = parser.parseFromString(xml, 'text/xml');
+              const parser = new DOMParser() // eslint-disable-line
+              const xml = $scope.xmlCodeBox.getValue()
+              const xmlDoc = parser.parseFromString(xml, 'text/xml')
               $scope.validFn({
                 valid: !!xmlDoc.getElementsByTagName('parsererror').length
-              });
+              })
             } catch (error) {
-              $notificationService.showSimpleToast(error, 'Error validating XML');
+              $notificationService.showSimpleToast(error, 'Error validating XML')
             }
-            if (!$scope.$$phase) $scope.$digest();
-            return;
-          };
+            if (!$scope.$$phase) $scope.$digest()
+            return
+          }
     
           const autoFormat = () => {
-            const totalLines = $scope.xmlCodeBox.lineCount();
+            const totalLines = $scope.xmlCodeBox.lineCount()
             $scope.xmlCodeBox.autoFormatRange(
               { line: 0, ch: 0 },
               { line: totalLines - 1 }
-            );
-            $scope.xmlCodeBox.setCursor(0);
-          };
+            )
+            $scope.xmlCodeBox.setCursor(0)
+          }
     
           const saveFile = async params => {
             try {
-              const content = $scope.xmlCodeBox.getValue().trim();
-              await $groupHandler.sendConfiguration(params.group, content);
+              const content = $scope.xmlCodeBox.getValue().trim()
+              await $groupHandler.sendConfiguration(params.group, content)
+              $notificationService.showSimpleToast('Success. Group has been updated')
+
             } catch (error) {
-              $notificationService.showSimpleToast(error, 'Send file error');
+              $notificationService.showSimpleToast(error.message || error, 'Send file error')
             }
-            return;
-          };
+            return
+          }
           $scope.xmlCodeBox = CodeMirror.fromTextArea(
             $document[0].getElementById('xml_box'),
             {
@@ -87,20 +89,20 @@ define([
               styleSelectedText: true,
               gutters: ['CodeMirror-foldgutter']
             }
-          );
+          )
           try {
-            $scope.xmlCodeBox.setValue($scope.data);
-            $scope.xmlCodeBox.refresh();
-            autoFormat();
+            $scope.xmlCodeBox.setValue($scope.data)
+            $scope.xmlCodeBox.refresh()
+            autoFormat()
           } catch (error) {
-            $notificationService.showSimpleToast(error, 'Fetching original file');
+            $notificationService.showSimpleToast(error, 'Fetching original file')
           }
     
           $scope.xmlCodeBox.on('change', () => {
-            checkXmlParseError();
-          });
+            checkXmlParseError()
+          })
     
-          $scope.$on('saveXmlFile', (ev, params) => saveFile(params));
+          $scope.$on('saveXmlFile', (ev, params) => saveFile(params))
         },
         templateUrl: BASE_URL +
           '/static/app/SplunkAppForWazuh/js/directives/wz-xml-file-editor/wz-xml-file-editor.html'
