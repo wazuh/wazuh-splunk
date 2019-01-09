3fe5f9a917c561c9d25852e4da06bd22f8c88250
Adri Valle <adri9valle@gmail.com>
2019-01-09 12:53:01 +0100

    Removed metrics, dropdown and custom filters

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/aws/awsCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/aws/awsCtrl.js
@@ -13,19 +13,16 @@ define([
   class AWS {
     /**
      * Class constructor
-     * @param {*} $urlTokenModel 
      * @param {*} $rootScope 
      * @param {*} $scope 
      * @param {*} $currentDataService 
      * @param {*} $state 
-     * @param {Object} awsMetrics 
      * @param {*} $notificationService 
      */
-    constructor($urlTokenModel, $rootScope, $scope, $currentDataService, $state, awsMetrics, $notificationService) {
-      this.rootScope = $rootScope
+    constructor($urlTokenModel, $scope, $currentDataService, $state, $notificationService) {
       this.scope = $scope
+      this.urlTokenModel = $urlTokenModel
       this.state = $state
-      this.awsMetrics = awsMetrics
       this.toast = $notificationService.showSimpleToast
       this.currentDataService = $currentDataService
       this.currentDataService.addFilter(
@@ -33,11 +30,64 @@ define([
       )
       this.getFilters = this.currentDataService.getSerializedFilters
       this.filters = this.getFilters()
-      this.submittedTokenModel = $urlTokenModel.getSubmittedTokenModel()
+      this.submittedTokenModel = this.urlTokenModel.getSubmittedTokenModel()
       this.timePicker = new TimePicker(
         '#timePicker',
-        $urlTokenModel.handleValueChange
+        this.urlTokenModel.handleValueChange
       )
+      
+      this.vizz = [
+        /**
+         * Visualizations
+         */
+        new AreaChart(
+          'eventsByIdOverTime',
+          `${this.filters} sourcetype=wazuh | timechart count by data.aws.responseElements.instancesSet.items.instanceId usenull=f`,
+          'eventsByIdOverTime'
+        ),
+        new ColumnChart(
+          'eventsByRegionOverTime',
+          `${this.filters} sourcetype=wazuh | timechart count by data.aws.awsRegion usenull=f`,
+          'eventsByRegionOverTime'
+        ),
+        new PieChart(
+          'topEventsByServiceName',
+          `${this.filters} sourcetype=wazuh | stats count BY data.aws.source`,
+          'topEventsByServiceName'
+        ),
+        new PieChart(
+          'topEventsByInstanceId',
+          `${this.filters} sourcetype=wazuh | top data.aws.responseElements.instancesSet.items.instanceId limit=5`,
+          'topEventsByInstanceId'
+        ),
+        new PieChart(
+          'topEventsByResourceType',
+          `${this.filters} sourcetype=wazuh | top data.aws.resourceType limit=5`,
+          'topEventsByResourceType'
+        ),
+        new PieChart(
+          'topEventsByRegion',
+          `${this.filters} sourcetype=wazuh | top data.aws.awsRegion limit=5`,
+          'topEventsByRegion'
+        ),
+        new Map(
+          'map',
+          `${
+          this.filters
+          } sourcetype=wazuh | geostats latfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lat" longfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lon" count`,
+          'map'
+        ),
+        new Table(
+          'top5Buckets',
+          `${this.filters} sourcetype=wazuh | top data.aws.source limit=5`,
+          'top5Buckets'
+        ),
+        new Table(
+          'top5Rules',
+          `${this.filters} sourcetype=wazuh | top rule.id, rule.description limit=5`,
+          'top5Rules'
+        )
+      ]
 
       this.scope.$on('deletedFilter', () => {
         this.launchSearches()
@@ -55,141 +105,7 @@ define([
         this.dropdown.destroy()
         this.vizz.map(vizz => vizz.destroy())
       })
-    }
 
-    /**
-     * On controller loads
-     */
-    $onInit() {
-      try {
-        this.amazonFilters = this.buildAmazonFilter()
-        if (!this.awsMetrics) {
-          this.scope.awsMetrics = {
-            enabled: "N/D",
-            scanInterval: "N/D",
-            runOnStart: "N/D",
-            skipOnError: "N/D",
-            buckets: "N/D",
-            accountInUse: "N/D",
-            regionsInUse: "N/D"
-          }
-        } else {
-          this.awsMetrics = this.awsMetrics[0]['aws-s3']
-          this.scope.awsMetrics = {
-            enabled: "",
-            scanInterval: this.awsMetrics.interval,
-            runOnStart: this.awsMetrics.run_on_start,
-            skipOnError: this.awsMetrics.skip_on_error,
-            buckets: [],
-            accountInUse: [],
-            regionsInUse: []
-          }
-
-          if (this.awsMetrics.disabled == "no") {
-            this.scope.awsMetrics.enabled = "yes"
-          } else {
-            this.scope.awsMetrics.enabled = "no"
-          }
-
-          this.scope.awsMetrics.buckets = this.awsMetrics.buckets
-
-          this.serializedAwsMetrics = this.serializedMetrics(this.awsMetrics.buckets)
-          this.scope.awsMetrics.regionsInUse = this.serializedAwsMetrics.regions
-          this.scope.awsMetrics.accountInUse = this.serializedAwsMetrics.accounts
-        }
-
-        this.dropdown = new Dropdown(
-          'dropDownInput',
-          `${this.filters} sourcetype=wazuh data.aws.source=* | stats by data.aws.source | fields data.aws.source | sort data.aws.source ASC`,
-          'data.aws.source',
-          '$form.awsSource$',
-          'dropDownInput'
-        )
-        this.dropdownInstance = this.dropdown.getElement()
-        this.dropdownInstance.on('change', (newValue) => {
-          this.oldValue = window.localStorage.getItem('lastAwsSourceFilter')
-          if (!this.oldValue || (this.oldValue && this.oldValue != newValue)) {
-            this.awsCurrentFilters = []
-            if (JSON.parse(window.localStorage.getItem('awsSourceFilters')))
-              this.awsCurrentFilters = JSON.parse(window.localStorage.getItem('awsSourceFilters'))
-            this.sourceValues = this.getAwsFiltersValue()
-            this.newKey = 'data.aws.source'
-            this.newFilter = {}
-            this.newFilter[this.newKey] = newValue
-            this.oldValue = window.localStorage.getItem('lastAwsFilter')
-            if (newValue === '*') {
-              this.newAwsFilters = this.awsCurrentFilters.filter(item => !item['data.aws.source'])
-              window.localStorage.setItem('awsSourceFilters', JSON.stringify(this.newAwsFilters))
-              window.localStorage.setItem('lastAwsSourceFilter', newValue)
-              this.rootScope.$broadcast('applyFiltersAws')
-              this.state.reload()
-            } else {
-              if (!this.sourceValues.includes(newValue)) {
-                this.awsCurrentFilters.push(this.newFilter)
-                window.localStorage.setItem('awsSourceFilters', JSON.stringify(this.awsCurrentFilters))
-                window.localStorage.setItem('lastAwsSourceFilter', newValue)
-                this.rootScope.$broadcast('applyFiltersAws')
-                this.state.reload()
-              }
-            }
-          }
-        })
-
-        this.vizz = [
-          /**
-           * Visualizations
-           */
-          new AreaChart(
-            'eventsByIdOverTime',
-            `${this.amazonFilters} sourcetype=wazuh | timechart count by data.aws.responseElements.instancesSet.items.instanceId usenull=f`,
-            'eventsByIdOverTime'
-          ),
-          new ColumnChart(
-            'eventsByRegionOverTime',
-            `${this.amazonFilters} sourcetype=wazuh | timechart count by data.aws.awsRegion usenull=f`,
-            'eventsByRegionOverTime'
-          ),
-          new PieChart(
-            'topEventsByServiceName',
-            `${this.amazonFilters} sourcetype=wazuh | stats count BY data.aws.source`,
-            'topEventsByServiceName'
-          ),
-          new PieChart(
-            'topEventsByInstanceId',
-            `${this.amazonFilters} sourcetype=wazuh | top data.aws.responseElements.instancesSet.items.instanceId limit=5`,
-            'topEventsByInstanceId'
-          ),
-          new PieChart(
-            'topEventsByResourceType',
-            `${this.amazonFilters} sourcetype=wazuh | top data.aws.resourceType limit=5`,
-            'topEventsByResourceType'
-          ),
-          new PieChart(
-            'topEventsByRegion',
-            `${this.amazonFilters} sourcetype=wazuh | top data.aws.awsRegion limit=5`,
-            'topEventsByRegion'
-          ),
-          new Map(
-            'map',
-            `${
-            this.amazonFilters
-            } sourcetype=wazuh | geostats latfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lat" longfield="data.aws.service.action.portProbeAction.portProbeDetails.remoteIpDetails.geoLocation.lon" count`,
-            'map'
-          ),
-          new Table(
-            'top5Buckets',
-            `${this.amazonFilters} sourcetype=wazuh | top data.aws.source limit=5`,
-            'top5Buckets'
-          ),
-          new Table(
-            'top5Rules',
-            `${this.amazonFilters} sourcetype=wazuh | top rule.id, rule.description limit=5`,
-            'top5Rules'
-          )
-        ]
-      } catch (err) {
-        this.toast(err.message || err)
-      }
     }
 
     /**
@@ -198,102 +114,6 @@ define([
     launchSearches() {
       this.filters = this.getFilters()
       this.state.reload()
-    }
-
-    /**
-     * Returns regions and accounts without duplicates
-     * @param {Objects} buckets 
-     */
-    serializedMetrics(buckets) {
-      try {
-        let regions = []
-        let accounts = []
-        buckets.map(bucket => {
-          if (!regions.includes(bucket.regions))
-            regions.push(bucket.regions)
-          if (!accounts.includes(bucket.aws_account_alias))
-            accounts.push(bucket.aws_account_alias)
-        })
-        return { "regions": regions, "accounts": accounts }
-      } catch (err) {
-        throw new Error('Cannot gets metrics')
-      }
-
-    }
-
-    /**
-     * Gets amazon filters
-     */
-    getAwsFiltersValue() {
-      try {
-        let sourceValues = []
-        let awsCurrentFilters = []
-        if (JSON.parse(window.localStorage.getItem('awsSourceFilters'))) {
-          awsCurrentFilters = JSON.parse(window.localStorage.getItem('awsSourceFilters'))
-        }
-        awsCurrentFilters = awsCurrentFilters.filter(item => item['data.aws.source'])
-        awsCurrentFilters.map(filter => {
-          sourceValues.push(filter['data.aws.source'])
-        })
-        return sourceValues
-      } catch (err) {
-        throw new Error('Cannot get AWS filters')
-      }
-    }
-
-    /**
-     * Serializes data.aws.source filters adding OR between filters
-     * @param {Object} filters 
-     */
-    serializedSourceAwsFilters(filters) {
-      try {
-        let sourceFil = ""
-        filters.map(fil => {
-          sourceFil = `${sourceFil} data.aws.source=${fil['data.aws.source']} OR`
-        })
-        sourceFil = sourceFil.substring(0, sourceFil.length - 2);
-        return sourceFil
-      } catch (err) {
-        throw new Error('Cannot serialize AWS source filters')
-      }
-    }
-
-    /**
-     * Serializes the rest of amazon filters
-     * @param {Object} filters 
-     */
-    serializedRestAwsFilters(filters) {
-      try {
-        let restFil = ""
-        filters.map(fil => {
-          const key = Object.keys(fil)[0]
-          const value = fil[key]
-          const filter = `${key}=${value}`
-          restFil = `${restFil} ${filter}`
-        })
-        return restFil
-      } catch (err) {
-        throw new Error('Cannot serialize AWS filters')
-      }
-    }
-
-    /**
-     * Gets and builds amazon filters
-     */
-    buildAmazonFilter() {
-      try {
-        let filters = this.getFilters()
-        if (JSON.parse(window.localStorage.getItem('awsSourceFilters'))) {
-          const awsCurrentFilters = JSON.parse(window.localStorage.getItem('awsSourceFilters'))
-          const sourceFilters = this.serializedSourceAwsFilters(awsCurrentFilters.filter(item => item['data.aws.source']))
-          const restFilters = this.serializedRestAwsFilters(awsCurrentFilters.filter(item => !item['data.aws.source']))
-          filters = `${filters} ${sourceFilters}`
-          filters = `${filters} ${restFilters}`
-        }
-        return filters
-      } catch (err) {
-        throw new Error('Cannot build AWS filters')
-      }
     }
   }
 
