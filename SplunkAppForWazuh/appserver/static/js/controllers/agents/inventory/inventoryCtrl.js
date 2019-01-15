ec18f096c9e8aacb5e7a7b6429375cc16b8a24e0
manuasir <manu.asi2@gmail.com>
2019-01-15 12:45:28 +0100

    Changes for syscollector

--- SplunkAppForWazuh/appserver/static/js/controllers/agents/inventory/inventoryCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/inventory/inventoryCtrl.js
@@ -31,8 +31,14 @@ define(['../../module'], function (module) {
       this.scope = $scope
       this.data = syscollector
       this.httpReq = $requestService.httpReq
+      this.apiReq = $requestService.apiReq
       this.root = $rootScope
       this.toast = $notificationService.showSimpleToast
+      this.netifaceResponse = false
+      this.ports = {}
+      this.packagesDate = {}
+      this.processesDate = {}
+      this.netaddrResponse = false
     }
 
     /**
@@ -49,15 +55,17 @@ define(['../../module'], function (module) {
      */
     $onInit() {
       try {
+        this.scope.hasSize = obj =>
+          obj && typeof obj === 'object' && Object.keys(obj).length
+
         this.scope.agent =
           this.data.length &&
             this.data.length > 4 &&
-            typeof this.data[5] === 'object' &&
-            this.data[5].data &&
-            this.data[5].data.data
-            ? this.data[5].data.data
+            typeof this.data[4] === 'object' &&
+            this.data[4].data &&
+            this.data[4].data.data
+            ? this.data[4].data.data
             : { error: true }
-
         this.scope.search = (term, specificPath) => {
           this.search(term, specificPath)
         }
@@ -82,34 +90,61 @@ define(['../../module'], function (module) {
         ) {
           this.scope.syscollector = null
         } else {
-          const netiface = {}
-          const ports = {}
-          const packagesDate = {}
-          const netaddrResponse = {}
-
           if (this.data[2] && this.data[2].data && this.data[2].data.data)
-            Object.assign(netiface, this.data[2].data.data)
+            Object.assign(this.ports, this.data[3].data.data)
           if (this.data[3] && this.data[3].data && this.data[3].data.data)
-            Object.assign(ports, this.data[3].data.data)
-          if (this.data[4] && this.data[4].data && this.data[4].data.data)
-            Object.assign(packagesDate, this.data[4].data.data)
-          if (this.data[6] && this.data[6].data && this.data[6].data.data)
-            Object.assign(netaddrResponse, this.data[6].data.data)
-          this.scope.syscollector = {
-            hardware: this.data[0].data.data,
-            os: this.data[1].data.data,
-            netiface: netiface,
-            ports: ports,
-            netaddr:  netaddrResponse ? { ...netaddrResponse } : false,
-            packagesDate:
-              packagesDate && packagesDate.items && packagesDate.items.length
-                ? packagesDate.items[0].scan_time
-                : 'Unknown'
-          }
+            Object.assign(this.packagesDate, this.data[3].data.data)
+          if (this.data[5] && this.data[5].data && this.data[5].data.data)
+            Object.assign(this.processesDate, this.data[5].data.data)
         }
+        this.init()
         return
       } catch (error) {
         this.toast(error.message || error)
+      }
+    }
+
+    /**
+     * Initializes the syscollector data
+     */
+    async init() {
+      try {
+        try {
+          const resultNetiface = await this.apiReq(
+            `/syscollector/${this.scope.agent.id}/netiface`,
+            {}
+          )
+          this.netifaceResponse = ((resultNetiface || {}).data || {}).data || false
+        } catch (error) { } // eslint-disable-line
+
+        // This API call may fail so we put it out of Promise.all
+        try {
+          const resultNetaddrResponse = await this.apiReq(
+            `/syscollector/${this.scope.agent.id}/netaddr`,
+            { limit: 1 }
+          )
+          this.netaddrResponse =
+            ((resultNetaddrResponse || {}).data || {}).data || false
+        } catch (error) { } // eslint-disable-line
+
+        this.scope.syscollector = {
+          hardware: this.data[0].data.data,
+          os: this.data[1].data.data,
+          netiface: this.netifaceResponse,
+          ports: this.ports,
+          netaddr: this.netaddrResponse,
+          packagesDate:
+            this.packagesDate && this.packagesDate.items && this.packagesDate.items.length
+              ? this.packagesDate.items[0].scan_time
+              : 'Unknown',
+          processesDate: ((this.processesDate || {}).items || []).length
+          ? this.processesDate.items[0].scan_time
+          : 'Unknown'
+        }
+        if (!this.scope.$$phase) this.scope.$digest()
+        return
+      } catch (error) {
+        throw new Error(error.message || error)
       }
     }
   }
