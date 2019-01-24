4adb0b24165a32375f7f6248bb382d22d0c3d6a1
Adri Valle <adri9valle@gmail.com>
2019-01-24 16:43:03 +0100

    Added xml directive for edit ossec.conf

--- SplunkAppForWazuh/appserver/static/js/controllers/management/edition/editionCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/management/edition/editionCtrl.js
@@ -1,4 +1,4 @@
-define(['../../module'], function(controllers) {
+define(['../../module'], function (controllers) {
   'use strict'
 
   class Edition {
@@ -8,19 +8,56 @@ define(['../../module'], function(controllers) {
      * @param {Array} nodes
      * @param {Boolean} isAdmin
      */
-    constructor($scope, nodes, isAdmin) {
+    constructor($scope, nodes, isAdmin, $notificationService) {
       this.scope = $scope
       this.nodes = nodes
-      console.log('is admin ', isAdmin)
+      this.isAdmin = isAdmin
+      this.toast = $notificationService.showSimpleToast
     }
     /**
      * On controller loads
      */
     $onInit() {
-      if (this.nodes && Array.isArray(this.nodes)) {
-        this.scope.nodes = this.nodes.items.filter(node => node.name)
+      try {
+        this.scope.editingNode = false
+        this.scope.editNode = (nodeName) => this.editNode(nodeName)
+        this.scope.cancelEditNode = () => this.cancelEditNode()
+        this.scope.saveOssecConfig = () => this.saveOssecConfig()
+
+        if (
+          this.nodes &&
+          this.nodes.data &&
+          this.nodes.data.data
+        ) {
+          this.scope.nodes = this.nodes.data.data.items
+        }
+
+        this.scope.isAdmin = this.isAdmin
+      } catch (error) {
+        console.error(error)
       }
     }
+
+    editNode(nodeName) {
+      this.scope.editingNode = nodeName
+      //replace for the api call
+      this.scope.fetchedXML = '<ossec_config>  <global>    <jsonout_output>yes</jsonout_output>    <alerts_log>yes</alerts_log>    <logall>no</logall>    <logall_json>no</logall_json>    <email_notification>no</email_notification>    <smtp_server>smtp.example.wazuh.com</smtp_server>    <email_from>ossecm@example.wazuh.com</email_from>    <email_to>recipient@example.wazuh.com</email_to>    <email_maxperhour>12</email_maxperhour>    <email_log_source>alerts.log</email_log_source>    <queue_size>131072</queue_size>  </global></ossec_config>'
+    }
+
+    cancelEditNode() {
+      this.scope.editingNode = false
+      this.scope.$broadcast('closeEditXmlFile', {})
+    }
+
+    saveOssecConfig() {
+      this.scope.editingNode = false
+      /*this.scope.$broadcast('saveXmlFile', {
+        ruleset: fileName,
+        dir: 'rules'
+      })*/
+      this.toast("Manager configuration saved.")
+    }
+
   }
 
   controllers.controller('editionCtrl', Edition)
