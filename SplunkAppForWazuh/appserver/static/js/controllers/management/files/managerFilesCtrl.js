6f72f5bf11f6c4cb5c94eadfc28d7aa2de0e51f0
Adri Valle <adri9valle@gmail.com>
2019-02-27 12:48:35 +0100

    Added files tab, list rules,decodes and cdb lists

---
+++ SplunkAppForWazuh/appserver/static/js/controllers/management/files/managerFilesCtrl.js
@@ -0,0 +1,52 @@
+define([
+  '../../module',
+], function (
+  controllers,
+) {
+    'use strict'
+    class Files {
+      /**
+       * Class Files
+       * @param {*} $scope
+       * @param {*} $notificationService
+       * @param {*} $currentDataService
+       * @param {*} isAdmin
+       * @param {*} localRules
+       * @param {*} localDecoders
+       * @param {*} cdbLists
+       */
+      constructor(
+        $scope,
+        $notificationService,
+        $currentDataService,
+        isAdmin,
+        $restartService,
+      ) {
+        this.scope = $scope
+        this.notification = $notificationService
+        this.currentDataService = $currentDataService
+        this.isAdmin = isAdmin
+        this.restartService = $restartService
+      }
+
+      /**
+       * On controller load
+       */
+      $onInit() {
+        this.scope.$broadcast('wazuhSearch', { term: '', removeFilters: true })
+        this.scope.selectedNavTab = 'files'
+        this.scope.selectedSubNavTab = 'rules'
+        this.scope.adminMode = this.isAdmin
+
+        this.scope.switchSubTab = (tab) => this.switchSubTab(tab)
+
+      }
+
+      switchSubTab(tab) {
+        this.scope.selectedSubNavTab = tab
+      }
+
+    }
+    controllers.controller('managerFilesCtrl', Files)
+    return Files
+  })
