652843912c63c54a21c6fd8db4a577c8664372b3
Pablo <pablotr9@correo.ugr.es>
2019-03-05 13:21:06 +0100

    Allow navigation through url & back button

--- SplunkAppForWazuh/appserver/static/js/services/navigation/navigationService.js
+++ SplunkAppForWazuh/appserver/static/js/services/navigation/navigationService.js
@@ -1,20 +1,85 @@
-define(['../module'], function(module) {
+define(['../module'], function (module) {
   'use strict'
 
   class navigationService {
-    constructor($state, $window) {
+    constructor($state, $window, $location) {
       this.$state = $state
       this.$window = $window
+      this.$location = $location
+      
+      this.$window.onpopstate = function (event) {
+        try {
+          var lastState = sessionStorage.history.split(',')
+
+          var newHistory = lastState[0];
+          if (lastState.length > 1) { // if there are previous states
+            for (var i = 1; i < lastState.length - 2; i++) {
+              newHistory += ',' + lastState[i]
+            }
+            sessionStorage.history = newHistory
+            lastState = lastState[lastState.length - 2]
+            $state.go(
+              lastState,
+              {}, //routeParams
+              { reload: true, notify: false }
+            );
+          }
+
+        } catch (error) {
+          sessionStorage.params
+            ? $state.go(sessionStorage.params)
+            : $state.go('settings.api')
+        }
+      }
     }
 
     storeRoute(params) {
       sessionStorage.params = params
+      this.addToHistory(params)
+
+      this.$location.search('currentTab', params); //set currentTab to the new state
+    }
+
+    addToHistory(url) {
+      if (!sessionStorage.history) {
+        sessionStorage.history = url;
+      } else {
+        var history = sessionStorage.history.split(',');
+        history.push(url);
+        var newHistory = history[0];
+        if (history.length < 20) {
+          for (var i = 1; i < history.length; i++) {
+            if (history[i - 1] !== history[i])
+              newHistory += ',' + history[i];
+          }
+        } else {
+          for (var i = 11; i < history.length; i++) {
+            if (history[i - 11] !== history[i])
+              newHistory += ',' + history[i];
+          }
+        }
+        sessionStorage.history = newHistory;
+
+      }
     }
 
     goToLastState() {
       sessionStorage.params
-        ? this.$state.go(sessionStorage.params)
-        : this.$state.go('settings.api')
+      ? this.$state.go(sessionStorage.params)
+      : this.$state.go('settings.api') // redirects to settings.api if no previous states were visited
+    }
+
+    /* *
+    * Redirects the user to the tab specified in the url (index?currentTab=)
+    * if no tab is specified it redirects to the last state visited 
+    * */
+    manageState(){
+      var url_params = this.$location.search();
+      if(url_params.currentTab){
+        this.$state.go(url_params.currentTab)
+      } else{
+        this.goToLastState()
+      }
     }
 
     getLastState() {
