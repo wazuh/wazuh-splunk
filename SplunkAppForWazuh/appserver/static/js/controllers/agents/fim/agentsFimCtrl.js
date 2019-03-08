b33a47109890c0400d11632475914896e80452f4
Pablo <pablotr9@correo.ugr.es>
2019-03-08 10:37:53 +0100

    Double click to minimize vis

--- SplunkAppForWazuh/appserver/static/js/controllers/agents/fim/agentsFimCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/agents/fim/agentsFimCtrl.js
@@ -331,6 +331,17 @@ define([
       this.scope.expandArray[i] = !this.scope.expandArray[i];
       let vis = $('#' + id + ' .panel-body .splunk-view .shared-reportvisualizer')
       this.scope.expandArray[i] ? vis.css('height', 'calc(100vh - 200px)') : vis.css('height', '250px')
+
+      let vis_header = $('.wz-headline-title')
+      vis_header.dblclick((e) => {
+        if(this.scope.expandArray[i]){
+          this.scope.expandArray[i] = !this.scope.expandArray[i];
+          this.scope.expandArray[i] ? vis.css('height', 'calc(100vh - 200px)') : vis.css('height', '250px')
+          this.scope.$applyAsync()
+        }else{
+          e.preventDefault();
+        }
+      });
     }
 
 
