c4db7399d75ab7317b8c818ab4428a376a22f3ae
Pablo Torres <pablotr9@correo.ugr.es>
2019-03-11 10:37:14 +0100

    Allow navigation through url & back button (#596)
    * Allow navigation through url & back button
    
    * Minor fix
    
    * Update navigationService.js

--- SplunkAppForWazuh/appserver/static/js/services/navigation/navigationService.js
+++ SplunkAppForWazuh/appserver/static/js/services/navigation/navigationService.js
@@ -1,24 +1,98 @@
-define(['../module'], function(module) {
+define(['../module'], function (module) {
   'use strict'
 
   class navigationService {
-    constructor($state, $window) {
+    constructor($state, $window, $location) {
       this.$state = $state
       this.$window = $window
+      this.$location = $location
+      
+      this.$window.onpopstate = function (event) {
+        try {
+          let lastState = sessionStorage.history.split(',')
+
+          let newHistory = lastState[0]
+          if (lastState.length > 1) { // if there are previous states
+            for (var i = 1; i < lastState.length - 2; i++) {
+              newHistory += ',' + lastState[i]
+            }
+            sessionStorage.history = newHistory
+            lastState = lastState[lastState.length - 2]
+            $state.go(
+              lastState,
+              {}, //routeParams
+              { reload: true, notify: false }
+            )
+          }
+
+        } catch (error) {
+          sessionStorage.params
+            ? $state.go(sessionStorage.params)
+            : $state.go('settings.api')
+        }
+      }
     }
 
     storeRoute(params) {
       sessionStorage.params = params
+      this.addToHistory(params)
+
+      this.$location.search('currentTab', params) //set currentTab to the new state
+    }
+
+    addToHistory(url) {
+      try{
+        if (!sessionStorage.history) {
+          sessionStorage.history = url
+        } else {
+          const history = sessionStorage.history.split(',')
+          history.push(url)
+          let newHistory = history[0]
+          const len = history.length
+          if (history.length < 20) {
+            for (let i = 1; i < len; i++) {
+              if (history[i - 1] !== history[i])
+                newHistory += ',' + history[i]
+            }
+          } else {
+            for (let i = 11; i < len; i++) {
+              if (history[i - 11] !== history[i])
+                newHistory += ',' + history[i]
+            }
+          }
+          sessionStorage.history = newHistory
+  
+        }
+      }catch(error){
+        this.goToLastState()
+      }
     }
 
     goToLastState() {
       sessionStorage.params
-        ? this.$state.go(sessionStorage.params)
-        : this.$state.go('settings.api')
+      ? this.$state.go(sessionStorage.params)
+      : this.$state.go('settings.api') // redirects to settings.api if no previous states were visited
+    }
+
+    /* *
+    * Redirects the user to the tab specified in the url (index?currentTab=)
+    * if no tab is specified it redirects to the last state visited 
+    * */
+    manageState(){
+      try{
+        const url_params = this.$location.search()
+        if(url_params.currentTab){
+          this.$state.go(url_params.currentTab)
+        } else{
+          this.goToLastState()
+        }
+      }catch(error){
+        this.$state.go('settings.api')
+      }
     }
 
     getLastState() {
-      if (sessionStorage.params) return sessionStorage.params
+      return sessionStorage.params || undefined
     }
 
     setCurrentAgent(currentAgentId) {
@@ -26,7 +100,7 @@ define(['../module'], function(module) {
     }
 
     getCurrentAgent() {
-      if (sessionStorage.currentAgent) return sessionStorage.currentAgent
+      return sessionStorage.currentAgent || undefined
     }
 
     setCurrentDevTools(current) {
