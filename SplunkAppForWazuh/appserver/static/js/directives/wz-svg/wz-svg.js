4fe85d0f61cf644698b48c9e3f79a76673ff6d62
Adri Valle <adri9valle@gmail.com>
2019-04-17 09:11:56 +0200

    Add directive to use eui svgs

---
+++ SplunkAppForWazuh/appserver/static/js/directives/wz-svg/wz-svg.js
@@ -0,0 +1,28 @@
+/*
+ * Wazuh app - Wazuh no configuration card directive
+ * Copyright (C) 2015-2019 Wazuh, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Find more information about this on the LICENSE file.
+ */
+
+define(['../module'], function (directives) {
+  'use strict'
+  directives.directive('wzSvg', function (BASE_URL) {
+    return {
+      restrict: 'E',
+      scope: {
+        icon: '@',
+        color: '@'
+      },
+      controller(){},
+      templateUrl:
+        BASE_URL +
+        '/static/app/SplunkAppForWazuh/js/directives/wz-svg/wz-svg.html'
+    }
+  })
+})
