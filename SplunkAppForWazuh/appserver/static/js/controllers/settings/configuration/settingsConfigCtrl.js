66b54498a2f099bd677b0078924998d85a29bd54
Adri Valle <adri9valle@gmail.com>
2019-05-03 14:07:23 +0200

    Edit data(todo send the data and overwrite)

--- SplunkAppForWazuh/appserver/static/js/controllers/settings/configuration/settingsConfigCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/settings/configuration/settingsConfigCtrl.js
@@ -7,6 +7,7 @@ define(['../../module'], function (controllers) {
       this.scope.extensions = {}
       this.notification = $notificationService
       this.currentApi = $currentDataService.getApi()
+      this.getCurrentConfiguration = $currentDataService.getCurrentConfiguration
       this.configuration = configuration
     }
 
@@ -16,6 +17,10 @@ define(['../../module'], function (controllers) {
         this.scope.configuration = this.configuration.data
 
         this.scope.getDescription = key => this.getDescription(key)
+        this.scope.switchEdit = (key, value) => this.switchEdit(key, value)
+        this.scope.cancelEdition =  () => this.cancelEdition()
+        this.scope.setValue = value => this.setValue(value)
+        
 
       } catch (error) { }
 
@@ -28,13 +33,63 @@ define(['../../module'], function (controllers) {
     getDescription(key) {
       try {
         const description = {
-          'log.level': 'Set the app loggin level, allowed values are info and debug.'
+          'log.level': 'Set the app loggin level, allowed values are info and debug.',
+          'timeout': 'Define the maximun time in seconds the app will wait for an API reponse when making request to it.'
         }
         return description[key]
       } catch (error) {
         return false
       }
     }
+
+    /**
+     * Enable edition
+     * @param {String} key 
+     */
+    switchEdit(key, value) {
+      try {
+        this.scope.editingKey = key
+        this.scope.editingNewValue = value
+      } catch (error) {
+        return false
+      }
+    }
+
+    /**
+     * Cancel the edition
+     */
+    cancelEdition() {
+      this.scope.editingKey = false
+      this.scope.editingNewValue = false
+    }
+
+    /**
+     * Edit the value
+     */
+    async setValue() {
+      try {
+        console.log(this.scope.editingNewValue)
+        /** TODO: call to an endpoint to overwritte the value */
+        this.cancelEdition()
+        this.refreshConfig()
+      } catch (error) {
+        return Promise.reject(error) 
+      }
+    }
+
+    /**
+     * Refresh the configuration
+     */
+    async refreshConfig() {
+      try {
+        const config = await this.getCurrentConfiguration()
+        this.scope.configuration = config.data
+        this.scope.$applyAsync()  
+      } catch (error) {
+        return Promise.reject(error) 
+      }
+    }
+
 
   }
   controllers.controller('settingsConfigCtrl', Configuration)
