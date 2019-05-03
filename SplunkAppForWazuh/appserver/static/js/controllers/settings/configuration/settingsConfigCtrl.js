414d94bc094fa3db69f7e1fe61fb24e469a40b5e
Adri Valle <adri9valle@gmail.com>
2019-05-03 11:18:34 +0200

    List configuration

--- SplunkAppForWazuh/appserver/static/js/controllers/settings/configuration/settingsConfigCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/settings/configuration/settingsConfigCtrl.js
@@ -1,21 +1,41 @@
-define(['../../module'], function(controllers) {
-    'use strict'
-  
-    class Configuration {
-      constructor($scope, $currentDataService, $notificationService, configuration) {
-        this.scope = $scope
-        this.scope.extensions = {}
-        this.notification = $notificationService
-        this.currentApi = $currentDataService.getApi()
-        this.configuration = configuration
-      }
-  
-      $onInit() {
-        const id = this.currentApi['_key']
-        this.scope.configuration = this.configuration
-      }
-  
+define(['../../module'], function (controllers) {
+  'use strict'
+
+  class Configuration {
+    constructor($scope, $currentDataService, $notificationService, configuration) {
+      this.scope = $scope
+      this.scope.extensions = {}
+      this.notification = $notificationService
+      this.currentApi = $currentDataService.getApi()
+      this.configuration = configuration
     }
-    controllers.controller('settingsConfigCtrl', Configuration)
-  })
-  
+
+    $onInit() {
+      try {
+        const id = this.currentApi['_key']
+        this.scope.configuration = this.configuration.data
+
+        this.scope.getDescription = key => this.getDescription(key)
+
+      } catch (error) { }
+
+    }
+
+    /**
+     * Returns the description from a key
+     * @param {String} key 
+     */
+    getDescription(key) {
+      try {
+        const description = {
+          log: 'Set the app loggin level, allowed values are info and debug.'
+        }
+        return description[key]
+      } catch (error) {
+        return false
+      }
+    }
+
+  }
+  controllers.controller('settingsConfigCtrl', Configuration)
+})
