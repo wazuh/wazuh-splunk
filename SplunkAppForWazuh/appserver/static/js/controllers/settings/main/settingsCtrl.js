bc197284035fcd4f1e8094578b2b428b2275686d
Adri Valle <adri9valle@gmail.com>
2019-05-14 11:51:51 +0200

    Add loading effect between settings

--- SplunkAppForWazuh/appserver/static/js/controllers/settings/main/settingsCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/settings/main/settingsCtrl.js
@@ -3,34 +3,39 @@ define(['../../module'], function(controllers) {
 
   class Main {
     constructor($scope, $navigationService) {
-      this.$navigationService = $navigationService
-      this.$scope = $scope
-      this.$scope.message = 'Settings'
-      this.$scope.tabName = ''
+      this.navigationService = $navigationService
+      this.scope = $scope
+      this.scope.message = 'Settings'
+      this.scope.tabName = ''
     }
 
     $onInit() {
-      const lastState = this.$navigationService.getLastState()
+      const lastState = this.navigationService.getLastState()
       switch (lastState) {
         case 'settings.api':
-          this.$scope.tabName = 'api'
+          this.scope.tabName = 'api'
           break
         case 'settings.extensions':
-          this.$scope.tabName = 'extensions'
+          this.scope.tabName = 'extensions'
           break
         case 'settings.index':
-          this.$scope.tabName = 'index'
+          this.scope.tabName = 'index'
           break
         case 'settings.about':
-          this.$scope.tabName = 'about'
+          this.scope.tabName = 'about'
           break
         case 'settings.logs':
-          this.$scope.tabName = 'logs'
+          this.scope.tabName = 'logs'
           break
       }
-      this.$scope.switchTab = name => {
-        this.$scope.tabName = name
+
+      this.scope.switchTab = name => {
+        this.scope.tabName = name
       }
+
+      this.scope.$on('loadingContent', (event, data) => {
+        this.scope.loadingContent = data.status
+      })
     }
   }
   controllers.controller('settingsCtrl', Main)
