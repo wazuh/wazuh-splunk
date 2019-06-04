67a3861586f8c5d18aeefec40f65ab10b10888a6
Pablo <pablotr9@correo.ugr.es>
2019-06-04 13:46:51 +0200

    test-babel-rollup

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  input: 'SplunkAppForWazuh/appserver/static/js/main.js',
+  output: {
+    file: 'SplunkAppForWazuh/appserver/static/js/build/main.min.js',
+    format: 'iife',
+    sourceMap: 'inline',
+  },
+};
