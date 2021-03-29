de14eca024eac6277e4ded2f24c8c2d8f1792020
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-03-29 15:54:16 -0300

    Changed equal operator to 'like' operator (#1034)

--- SplunkAppForWazuh/appserver/static/js/directives/wz-tag-filter/wz-tag-filter.js
+++ SplunkAppForWazuh/appserver/static/js/directives/wz-tag-filter/wz-tag-filter.js
@@ -120,7 +120,7 @@ define(['../module'], function(app) {
                   .filter(x => x.type === 'filter')
                   .forEach((tag, idx2) => {
                     const value = tag.value.value === 'unknown' ? 'null' : tag.value.value;
-                    queryObj.query += tag.key + '=' + value;
+                    queryObj.query += tag.key + '~' + value;
                     if (idx2 != group.length - 1) {
                       queryObj.query +=
                         $scope.connectors[idx].subgroup[idx2].value
