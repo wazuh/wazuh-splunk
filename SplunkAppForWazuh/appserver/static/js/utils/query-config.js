ee311037052fba07f6d3882d4e7e8b18b8574778
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-04-14 11:40:18 +0200

    Improved code readability

--- SplunkAppForWazuh/appserver/static/js/utils/query-config.js
+++ SplunkAppForWazuh/appserver/static/js/utils/query-config.js
@@ -61,8 +61,10 @@ define([], function() {
           : partialResult.data.data;
         } 
         else if (partialResult.data.error) {
+          const isModuleConnectionError = typeof partialResult.data.error == 'string' && partialResult.data.error.includes("Error connecting with socket");
+          
           if ((((partialResult.data.data || {}).failed_items || []).length && partialResult.data.data.failed_items[0].error.code == 1116) ||
-            typeof partialResult.data.error == 'string' && partialResult.data.error.includes("Error connecting with socket")) //Error 1121 - Module not configured in ossec.conf
+            isModuleConnectionError) //Error 1121 - Module not configured in ossec.conf
             result[`${component}-${configuration}`] = 'not-present';
           else if(partialResult.data.detail || partialResult.data.message)
             result[`${component}-${configuration}`] = partialResult.data.detail || partialResult.data.message;
