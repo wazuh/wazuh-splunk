5bf1ae514ffd6301e82a20c60cbae787795b789a
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-05-14 12:53:32 +0200

    Fix technique event details table

--- SplunkAppForWazuh/appserver/static/js/controllers/overview/mitre-ids/overviewMitreIdsCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/overview/mitre-ids/overviewMitreIdsCtrl.js
@@ -56,9 +56,10 @@ define([
         $urlTokenModel
       )
       this.scope = $scope
-      this.submittedTokenModel = $urlTokenModel.getSubmittedTokenModel()
-      this.submittedTokenModel.set('activeAgentToken', '-')
       this.currentDataService = $currentDataService
+      this.currentDataService.addFilter(
+        `{"rule.mitre.id{}":"*", "implicit":true, "onlyShow":true}`
+      )
       this.api = this.currentDataService.getApi()
       this.apiReq = $requestService.apiReq
       this.state = $state
@@ -71,9 +72,9 @@ define([
       this.groupHandler = $groupHandler
       this.setBrowserOffset = $dateDiffService.setBrowserOffset
       this.onDataModalEvents = this.onDataModalEvents.bind(this)
-      try {
+      this.vizz = []
 
-        this.scope.modalData = []
+      try {
         this.scope.loadingModalData = false
         this.scope.tactics = { ...mitre_tactics }
         this.scope.sortedTactics = Object.entries(this.scope.tactics)
@@ -86,15 +87,7 @@ define([
         }
       } catch (error) { } //eslint-disable-line
 
-      //Splunk table
-      this.vizz = [
-        new Table(
-          'agentsSummaryVizz',
-          `${this.filters} sourcetype=wazuh |stats count sparkline by rule.id, rule.description, rule.level | sort count DESC  | rename rule.id as "Rule ID", rule.description as "Description", rule.level as Level, count as Count`,
-          'agentsSummaryVizz',
-          this.scope
-        ),
-      ];
+
 
       this.tacticsSearch = new SearchHelper({
         id: 'tacticsCount',
@@ -120,97 +113,14 @@ define([
       });
       this.scope.$applyAsync();
     }
-    
-    async onDataModalEvents(rows) {      
+
+    async onDataModalEvents(rows) {
       const modalData = rows.map((value) => {
         return JSON.parse(value[3])
       });
       const modalSearch = this.modalSearch;
 
-      const newRequestMitre = await this.apiReq(`/mitre`, { q: `id=${this.scope.selectedItem[0]}` })
-      const mitreData = newRequestMitre.data.data.affected_items[0];
 
-      var parentEl = angular.element(document.body);
-
-      //hide spinner when the informations has been loaded
-      this.scope.loadingModalData = false;
-
-      this.$mdDialog.show({
-
-        parent: parentEl,
-        template:
-          `<md-dialog aria-label="List dialog" style="max-width: 75%;">
-          <h3 class="wz-headline-title boldText">Technique ${this.scope.selectedItem[1].name}</h3>
-          <md-divider class="wz-margin-top-10"></md-divider>
-          <md-dialog-content class="_md flex wazuh-column wz-margin-top-10">
-            <div class="modalFlexData">            
-              <p><b>Id: </b>${mitreData.id}</p>  
-              <p><b>Tactic: </b>${mitreData.phase_name}</p>
-            </div>
-            <div class="modalFlexData">
-              <p><b>Platform: </b>${mitreData.platform_name}</p>  
-              <p><b>Version: </b>${mitreData.json.x_mitre_version}</p>
-            </div>
-            
-            <p><b>Data sources: </b>${mitreData.json.x_mitre_data_sources}</p>
-
-            <p><b>Description: </b>${mitreData.json.description}</p>
-
-            <md-card flex="100" class="wz-md-card" ng-class="{'fullscreen': expandArray[5]}">
-              <md-card-content class="wazuh-column">
-                <span class="wz-headline-title">Events</span>
-                <md-divider class="wz-margin-top-10"></md-divider>
-                <div id='agentsSummaryVizz'></div>
-              </md-card-content>
-            </md-card>
-          </md-dialog-content>
-          <md-dialog-actions>
-            <md-button ng-click="ctrl.closeDialog()" class="splButton-primary">
-              Close
-            </md-button>
-          </md-dialog-actions>
-        </md-dialog>;`,
-        locals: {
-          items: this.scope.selectedItem
-        },
-      //   <table>
-                           
-      //   <tr>                             
-      //     <th>Time</th>
-      //     <th>Agent</th>
-      //     <th>Agent name</th>
-      //     <th>Technique(s)</th>
-      //     <th>Tactic(s)</th>
-      //     <th>Level</th>
-      //     <th>Rule ID</th>
-      //     <th>Description</th>
-      //   </tr>   
-      //   <tr ng-repeat="(index, row) in ctrl.rows">                             
-      //     <td>{{row.timestamp}}</td>
-      //     <td>{{row.agent.id}}</td>
-      //     <td>{{row.agent.name}}</td>
-      //     <td>{{row.rule.mitre.id.toString()}}</td>
-      //     <td>{{row.rule.mitre.tactic.toString()}}</td>
-      //     <td>{{row.rule.level}}</td>
-      //     <td>{{row.rule.id}}</td>
-      //     <td>{{row.rule.description}}</td>
-      //   </tr>   
-
-      // </table>
-        controller: DialogController,
-        controllerAs: 'ctrl'
-      })
-      function DialogController($mdDialog) {
-
-        this.rows = modalData
-
-        this.closeDialog = () => {
-          modalSearch.destroy()
-          $mdDialog.hide()
-        }
-      }
-
-      this.scope.$applyAsync();
     }
 
     onDataTechniques(rows) {
@@ -238,7 +148,7 @@ define([
       this.scope.node_name = 'all'
       this.scope.versionModel = 'all'
       this.scope.downloadCsv = () => this.downloadCsv()
-      this.scope.$on('$destroy', () => {this.destroy()})
+      this.scope.$on('$destroy', () => { this.destroy() })
       this.scope.reloadList = () => this.reloadList()
       this.scope.offsetTimestamp = (text, time) => {
         try {
@@ -246,18 +156,18 @@ define([
         } catch (error) {
           return ''
         }
-      }        
+      }
       this.scope.startVis2Png = () =>
-      this.reportingService.startVis2Png(
-        'overview-general',
-        'Security events',
-        this.filters,
-        [
-          'agentsSummaryVizz'
-        ],
-        this.reportMetrics,
-        this.tableResults
-      )
+        this.reportingService.startVis2Png(
+          'overview-general',
+          'Security events',
+          this.filters,
+          [
+            'agentsSummaryVizz'
+          ],
+          this.reportMetrics,
+          this.tableResults
+        )
     }
 
     destroy() {
@@ -272,83 +182,75 @@ define([
       this.scope.selectedItem = item;
 
       try {
-        this.modalSearch = new SearchHelper({
-          id: 'tacticsCountTable',
-          // search: `index=wazuh ${this.filters} "rule.mitre.id{}"="${item[0]}" | fields + rule.description, rule.mitre.id{}, rule.mitre.tactic{}, rule.mitre.technique{}, timestamp`,
-          search: `index=wazuh ${this.filters} "rule.mitre.id{}"="${item[0]}"`,
-          onData: this.onDataModalEvents,
-          scope: this.scope
+        const newRequestMitre = await this.apiReq(`/mitre`, { q: `id=${this.scope.selectedItem[0]}` })
+        const mitreData = newRequestMitre.data.data.affected_items[0];
+        var parentEl = angular.element(document.body);
+        const ParentCtrl = this;
+        //hide spinner when the informations has been loaded
+        this.scope.loadingModalData = false;
+
+        this.$mdDialog.show({
+
+          parent: parentEl,
+          scope: this.scope,
+          preserveScope: true,
+          template:
+            `<md-dialog aria-label="List dialog" style="max-width: 75%;">
+          <h3 class="wz-headline-title boldText">Technique ${this.scope.selectedItem[1].name}</h3>
+          <md-divider class="wz-margin-top-10"></md-divider>
+          <md-dialog-content class="_md flex wazuh-column wz-margin-top-10">
+            <div class="modalFlexData">            
+              <p><b>Id: </b>${mitreData.id}</p>  
+              <p><b>Tactic: </b>${mitreData.phase_name}</p>
+            </div>
+            <div class="modalFlexData">
+              <p><b>Platform: </b>${mitreData.platform_name}</p>  
+              <p><b>Version: </b>${mitreData.json.x_mitre_version}</p>
+            </div>
+            
+            <p><b>Data sources: </b>${mitreData.json.x_mitre_data_sources}</p>
+
+            <p><b>Description: </b>${mitreData.json.description}</p>
+              <h6 class="wz-headline-title">Events</h6>
+              <md-divider class="wz-margin-top-10"></md-divider>
+              <wazuh-bar></wazuh-bar>
+              <md-divider class="wz-margin-top-10"></md-divider>
+              <div id='mitre-technique-details-vizz'></div>
+          </md-dialog-content>
+          <md-dialog-actions>
+            <md-button ng-click="ctrl.closeDialog()" class="splButton-primary">
+              Close
+            </md-button>
+          </md-dialog-actions>
+        </md-dialog>;`,
+          locals: {
+            items: this.scope.selectedItem
+          },
+          onComplete: () => {
+            this.vizz.push(
+              new Table(
+                'mitre-technique-details-vizz',
+                `${this.filters} sourcetype=wazuh |stats count sparkline by rule.id, rule.description, rule.level | sort count DESC  | rename rule.id as "Rule ID", rule.description as "Description", rule.level as Level, count as Count`,
+                'mitre-technique-details-vizz',
+                this.scope
+              )
+            )
+          },
+          controller: DialogController,
+          controllerAs: 'ctrl'
         })
+        function DialogController($mdDialog, $scope) {
 
-        console.log(this.scope.modalData);
+          this.$scope = $scope
+          this.closeDialog = () => {
+            ParentCtrl.vizz.map(vizz => vizz.destroy())
+            ParentCtrl.vizz.pop()
+            $mdDialog.hide()
+          }
+        }
 
-        // const newRequestMitre = await this.apiReq(`/mitre`, { q: `id=${item[0]}` })
-        // const mitreData = newRequestMitre.data.data.affected_items[0];
+        this.scope.$applyAsync();
 
-        // var parentEl = angular.element(document.body);
-        // this.$mdDialog.show({
-        //   parent: parentEl,
-        //   template:
-        //     `<md-dialog aria-label="List dialog" style="max-width: 75%;">
-        //     <h3 class="wz-headline-title boldText">Technique ${item[1].name}</h3>
-        //     <md-divider class="wz-margin-top-10"></md-divider>
-        //     <md-dialog-content class="_md flex wazuh-column wz-margin-top-10">
-        //       <div class="modalFlexData">            
-        //         <p><b>Id: </b>${mitreData.id}</p>  
-        //         <p><b>Tactic: </b>${mitreData.phase_name}</p>
-        //       </div>
-        //       <div class="modalFlexData">
-        //         <p><b>Platform: </b>${mitreData.platform_name}</p>  
-        //         <p><b>Version: </b>${mitreData.json.x_mitre_version}</p>
-        //       </div>
-              
-        //       <p><b>Data sources: </b>${mitreData.json.x_mitre_data_sources}</p>
-
-        //       <p><b>Description: </b>${mitreData.json.description}</p>
-
-        //       <table>
-        //         <tr>
-        //           <th>Time</th>
-        //           <th>Agent</th>
-        //           <th>Agent name</th>
-        //           <th>Technique(s)</th>
-        //           <th>Tactic(s)</th>
-        //           <th>Level</th>
-        //           <th>Rule ID</th>
-        //           <th>Description</th>
-        //         </tr>
-        //         <tr>
-        //           <td>May 10, 2021 @ 17:03:12.919</td>
-        //           <td>005</td>
-        //           <td>RHEL7</td>
-        //           <td>T1110</td>
-        //           <td>Credential Access</td>
-        //           <td>10</td>
-        //           <td>5720</td>
-        //           <td>	
-        //           syslog: User missed the password more than one time</td>          
-        //         </tr>                             
-        //       </table>
-
-        //     </md-dialog-content>
-        //     <md-dialog-actions>
-        //       <md-button ng-click="ctrl.closeDialog()" class="splButton-primary">
-        //         Close
-        //       </md-button>
-        //     </md-dialog-actions>
-        //   </md-dialog>;`,
-        //   locals: {
-        //     items: item
-        //   },
-        //   controller: DialogController,
-        //   controllerAs: 'ctrl'
-        // })
-        // function DialogController($mdDialog) {
-        //   this.closeDialog = () => {
-        //     modalSearch.destroy()
-        //     $mdDialog.hide()
-        //   }
-        // }
       } catch (err) {
         console.error(err);
       }
