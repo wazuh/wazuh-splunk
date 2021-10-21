56866a316861261fcf7b56074d20cfd9d7bfc15b
gabiwassan <gabriel.wassan@wazuh.com>
2021-10-21 14:18:14 -0300

    feat(security-role): Added new security service for handler all request.

---
+++ SplunkAppForWazuh/appserver/static/js/services/security/securityService.js
@@ -0,0 +1,99 @@
+define(["../module"], function(module) {
+  "use strict";
+
+  module.service("$securityService", function($requestService, $state) {
+    const getRoleData = async () => {
+      try {
+        return await $requestService.apiReq("/security/roles");
+      } catch (error) {
+        $state.go("settings.api");
+      }
+    };
+
+    const getPoliciesData = async () => {
+      try {
+        return await $requestService.apiReq("/security/policies");
+      } catch (error) {
+        $state.go("settings.api");
+      }
+    };
+
+    const saveRole = async (roleName, policies) => {
+      try {
+        const result = await $requestService.apiReq(
+          "/security/roles",
+          {
+            content: JSON.stringify({ name: roleName }),
+            origin: "json"
+          },
+          "POST"
+        );
+
+        const data = (result.data || {}).data;
+        if (data.failed_items && data.failed_items.length) {
+          return;
+        }
+
+        let roleId = "";
+        if (data.affected_items && data.affected_items) {
+          roleId = data.affected_items[0].id;
+        }
+
+        // const policiesId = policies.map(policy => {
+        //   return policy.id;
+        // });
+
+        const policyResult = await $requestService.apiReq(
+          `/security/roles/${roleId}/policies`,
+          {
+            content: JSON.stringify({
+              params: {
+                policy_ids: "1"
+              }
+            }),
+            origin: "json"
+          },
+          "POST"
+        );
+
+        const policiesData = (policyResult.data || {}).data;
+        if (policiesData.failed_items && policiesData.failed_items.length) {
+          return policiesData;
+        }
+
+        if (result.data.error === 1905) {
+          return result;
+        } else {
+          throw new Error(
+            result.data.message || result.data.error || "Cannot save Role."
+          );
+        }
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    };
+
+    const removeRole = async role => {
+      try {
+        const result = await $requestService.apiReq(
+          `/security/roles?role_ids=${role}`,
+          {},
+          "DELETE"
+        );
+        if (result.data.error !== 0) {
+          throw new Error(result.data.message);
+        }
+        return result;
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    };
+
+    return {
+      getRoleData: getRoleData,
+      getPoliciesData: getPoliciesData,
+      saveRole: saveRole,
+      removeRole: removeRole
+    };
+  });
+});
