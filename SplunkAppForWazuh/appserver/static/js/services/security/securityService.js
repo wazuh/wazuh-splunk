1b7cb337701386b7a0bd43565dba8bf020a2d6f3
sortiz <sortiz@owlh.net>
2021-10-28 10:03:31 +0200

    solving problems

--- SplunkAppForWazuh/appserver/static/js/services/security/securityService.js
+++ SplunkAppForWazuh/appserver/static/js/services/security/securityService.js
@@ -4,8 +4,6 @@ define(["../module"], function(module) {
   "use strict";
 
   module.service("$securityService", function($requestService, $state) {
-<<<<<<< HEAD
-    
     const removeUser = async user => {
       try {
         const result = await $requestService.apiReq(
@@ -17,74 +15,6 @@ define(["../module"], function(module) {
           throw new Error(result.data.message);
         }
         return result;
-=======
-    const getRoleData = async () => {
-      try {
-        return await $requestService.apiReq("/security/roles");
-      } catch (error) {
-        $state.go("settings.api");
-      }
-    };
-
-    const getPolicyData = async () => {
-      try {
-        return await $requestService.apiReq("/security/policies");
-      } catch (error) {
-        $state.go("settings.api");
-      }
-    };
-
-    const fetchNewRole = async (roleName) => {
-      return await $requestService.apiReq(
-        "/security/roles",
-        {
-          content: JSON.stringify({ name: roleName }),
-          origin: "json"
-        },
-        "POST"
-      );
-    };
-
-    const saveRole = async (role, policies) => {
-      try {
-        let roleId = "";
-
-        if (!role.id) {
-          const result = await fetchNewRole(role.name);
-
-          const data = (result.data || {}).data;
-          if (data.failed_items && data.failed_items.length) {
-            return data;
-          }
-
-          if (data.affected_items && data.affected_items) {
-            roleId = data.affected_items[0].id;
-          }
-
-          if (result.data.error === 1905) {
-            return result;
-          } else if (result.data.error) {
-            throw new Error(
-              result.data.message || result.data.error || "Cannot save Role."
-            );
-          }
-        } else {
-          roleId = role.id;
-        }
-
-        const policyResult = await $requestService.apiReq(
-          `/security/roles/${roleId}/policies`,
-          {
-            policy_ids: policies.toString()
-          },
-          "POST"
-        );
-
-        const policiesData = (policyResult.data || {}).data;
-        if (policiesData.failed_items && policiesData.failed_items.length) {
-          return policiesData;
-        }
->>>>>>> 48ef8b6c... feat(security-policies): Fixed handler errors
       } catch (error) {
         return Promise.reject(error);
       }
