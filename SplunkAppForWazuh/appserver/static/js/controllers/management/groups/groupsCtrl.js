c98a272af64fcddf3b5c10ca984898b9b5d1f39e
Alex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>
2022-01-31 19:58:50 +0100

    Small fix

--- SplunkAppForWazuh/appserver/static/js/controllers/management/groups/groupsCtrl.js
+++ SplunkAppForWazuh/appserver/static/js/controllers/management/groups/groupsCtrl.js
@@ -506,8 +506,8 @@ define(["../../module", "FileSaver"], function (controllers) {
             await this.scope.loadSelectedAgents()
             this.scope.selectedAgents.offset += 499
           }
+          // eslint-disable-next-line
           this.scope.firstSelectedList = angular.copy(
-            // eslint-disable-line
             this.scope.selectedAgents.data
           )
           await this.scope.loadAllAgents()
