5d1af0f9721dbf667ec293b2fa6b929d5af602e1
Alex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>
2022-09-07 11:10:38 +0200

    Translate constant name to english

--- SplunkAppForWazuh/appserver/static/js/directives/wz-tag-filter/wz-tag-filter.js
+++ SplunkAppForWazuh/appserver/static/js/directives/wz-tag-filter/wz-tag-filter.js
@@ -120,10 +120,10 @@ define(['../module'], function (app) {
                   .filter((x) => x.type === 'filter')
                   .forEach((tag, idx2) => {
                     const [dataModelTag] = $scope.dataModel.filter((c) => c.key === tag.key)
-                    const operador = dataModelTag.list.includes(tag.value.value) ? '=' : '~'
+                    const operator = dataModelTag.list.includes(tag.value.value) ? '=' : '~'
                     const value =
                       tag.value.value === 'unknown' ? 'null' : tag.value.value
-                    queryObj.query += tag.key + operador + value
+                    queryObj.query += tag.key + operator + value
                     if (idx2 != group.length - 1) {
                       queryObj.query +=
                         $scope.connectors[idx].subgroup[idx2].value
