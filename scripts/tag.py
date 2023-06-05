2b269049be4fc6179c4430d8c48e86ce99cd438b
yenienserrano <ian.serrano@wazuh.com>
2023-06-05 11:40:59 +0200

    Bump: 4.4.4

--- scripts/tag.py
+++ scripts/tag.py
@@ -17,11 +17,11 @@ import os
 # Splunk versions
 supported_versions = ['8.1', '8.2']
 # Wazuh version
-version = '4.4.3'
+version = '4.4.4'
 # RC number(optional, set to 0 or None to disable)
 rc_number = 1
 # App's revision number (previous rev + 1)
-revision = '4403'
+revision = '4404'
 # Debug mode
 dry_run = False
 
